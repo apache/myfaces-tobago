54aa340358b33267c238c654b00b849e1ddf3f8c
Udo Schnurpfeil <lofwyr@apache.org>
2005-01-17 09:54:44 +0000

    imports

--- src/main/com/atanion/tobago/component/UIColumnSelector.java
+++ src/main/com/atanion/tobago/component/UIColumnSelector.java
@@ -5,20 +5,9 @@
   */
 package com.atanion.tobago.component;
 
-import com.atanion.tobago.TobagoConstants;
-import com.atanion.tobago.model.SheetState;
-import com.atanion.tobago.renderkit.html.scarborough.standard.tag.SheetRenderer;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import javax.faces.context.FacesContext;
-import javax.faces.el.ValueBinding;
 import javax.faces.component.UIColumn;
 
 public class UIColumnSelector extends UIColumn{
-
-  private static final Log LOG = LogFactory.getLog(UIColumnSelector.class);
 
   public static final String COMPONENT_TYPE = "com.atanion.tobago.ColumnSelector";
 
