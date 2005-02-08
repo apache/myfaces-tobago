00e176f48cf8dce647a1d1c496dad6352e63216d
Udo Schnurpfeil <lofwyr@apache.org>
2005-02-08 11:24:36 +0000

    WML

--- src/main/com/atanion/tobago/renderkit/wml/scarborough/standard/tag/InRenderer.java
+++ src/main/com/atanion/tobago/renderkit/wml/scarborough/standard/tag/InRenderer.java
@@ -6,6 +6,12 @@
 package com.atanion.tobago.renderkit.wml.scarborough.standard.tag;
 
 import com.atanion.tobago.renderkit.RendererBase;
+import com.atanion.tobago.renderkit.RenderUtil;
+import com.atanion.tobago.webapp.TobagoResponseWriter;
+import com.atanion.tobago.component.ComponentUtil;
+import com.atanion.tobago.component.UIPage;
+import com.atanion.tobago.TobagoConstants;
+import com.atanion.util.KeyValuePair;
 
 import javax.faces.context.FacesContext;
 import javax.faces.component.UIComponent;
@@ -19,7 +25,36 @@ public class InRenderer extends RendererBase {
 
   public void encodeEndTobago(FacesContext facesContext, UIComponent component)
       throws IOException {
-    // todo
+
+    TobagoResponseWriter writer
+        = (TobagoResponseWriter) facesContext.getResponseWriter();
+
+    String clientId = component.getClientId(facesContext);
+
+    UIPage uiPage = ComponentUtil.findPage(component);
+
+    if (uiPage != null){
+      uiPage.getPostfields().add(new KeyValuePair(clientId, clientId));
+    }
+
+    UIComponent label = component.getFacet(TobagoConstants.FACET_LABEL);
+    if (label != null) {
+      RenderUtil.encode(facesContext, label);
+    }
+
+    String currentValue = ComponentUtil.currentValue(component);
+
+    String type = ComponentUtil.getBooleanAttribute(
+        component, TobagoConstants.ATTR_PASSWORD) ? "password" : "text";
+
+    writer.startElement("input", component);
+    writer.writeAttribute("name", clientId, null);
+    writer.writeAttribute("id", clientId, null);
+    writer.writeAttribute("value", currentValue, null);
+    writer.writeAttribute("type", type, null);
+    writer.endElement("input");
+
+
   }
 }
 
--- src/main/com/atanion/tobago/renderkit/wml/scarborough/standard/tag/PageRenderer.java
+++ src/main/com/atanion/tobago/renderkit/wml/scarborough/standard/tag/PageRenderer.java
@@ -5,18 +5,53 @@
  */
 package com.atanion.tobago.renderkit.wml.scarborough.standard.tag;
 
+import com.atanion.tobago.component.UIPage;
 import com.atanion.tobago.renderkit.PageRendererBase;
+import com.atanion.tobago.renderkit.RenderUtil;
+import com.atanion.tobago.webapp.TobagoResponseWriter;
 
-import javax.faces.context.FacesContext;
 import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 
 public class PageRenderer extends PageRendererBase {
 
+  private static final String DOCTYPE =
+      "<?xml version='1.0'?>\n" +
+      "<!DOCTYPE wml PUBLIC '-//WAPFORUM//DTD WML 1.1//EN'\n" +
+      " 'http://www.wapforum.org/DTD/wml_1.1.xml'>";
+
   public void encodeEndTobago(FacesContext facesContext, UIComponent component)
       throws IOException {
-    // todo
-    
+
+    UIPage page = (UIPage) component;
+
+    TobagoResponseWriter writer
+        = (TobagoResponseWriter) facesContext.getResponseWriter();
+
+    // replace responseWriter and render page content
+    StringWriter content = new StringWriter();
+    ResponseWriter contentWriter = new TobagoResponseWriter(
+        content, writer.getContentType(), writer.getCharacterEncoding());
+    facesContext.setResponseWriter(contentWriter);
+
+    RenderUtil.encodeChildren(facesContext, page);
+
+    // reset responseWriter and render page
+    facesContext.setResponseWriter(writer);
+
+    writer.write(DOCTYPE);
+    writer.write('\n');
+
+    writer.startElement("wml", page);
+    writer.startElement("card", page);
+
+    writer.write(content.toString());
+
+    writer.endElement("card");
+    writer.endElement("wml");
   }
 }
 
