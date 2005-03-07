f1dcca569d3415c169b7418e7fabc411d0854537
Bernd Bohmann <bommel@apache.org>
2005-03-07 08:10:47 +0000

    Annotation Processor Task

--- src/main/com/atanion/tobago/taglib/ValueBinding.java
+++ src/main/com/atanion/tobago/taglib/ValueBinding.java
@@ -1,11 +1,17 @@
 package com.atanion.tobago.taglib;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
 /*
  * Copyright (c) 2005 Atanion GmbH, Germany
  * All rights reserved. Created 05.03.2005 16:13:42.
  * $Id$
  */
-
+@Retention(value=RetentionPolicy.SOURCE)
+@Target(value=ElementType.METHOD)
 public @interface ValueBinding {
 
   Class type();
