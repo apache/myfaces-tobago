3d7a9520c908fe4729166a121bef322f8c6233f8
Volker Weber <weber@apache.org>
2005-03-08 15:54:04 +0000

    TobagoMultipartFormdataRequest extends HtpServletRequestWrapper

--- src/main/com/atanion/tobago/webapp/TobagoMultipartFormdataRequest.java
+++ src/main/com/atanion/tobago/webapp/TobagoMultipartFormdataRequest.java
@@ -12,33 +12,23 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import javax.faces.FacesException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.Principal;
+import javax.servlet.http.HttpServletRequestWrapper;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
-public class TobagoMultipartFormdataRequest implements HttpServletRequest {
+public class TobagoMultipartFormdataRequest extends HttpServletRequestWrapper {
 
 // ///////////////////////////////////////////// constant
 
-  private static final Log LOG = LogFactory.getLog(
-      TobagoMultipartFormdataRequest.class);
+  private static final Log LOG
+      = LogFactory.getLog(TobagoMultipartFormdataRequest.class);
 
 // ///////////////////////////////////////////// attribute
-
-  private HttpServletRequest request;
 
   private Map parameters;
 
@@ -47,7 +37,7 @@ public class TobagoMultipartFormdataRequest implements HttpServletRequest {
 // ///////////////////////////////////////////// constructor
 
   TobagoMultipartFormdataRequest(HttpServletRequest request) {
-    this.request = request;
+    super(request);
     String contentType = request.getContentType();
     if (contentType == null ||
         !contentType.toLowerCase().startsWith("multipart/form-data")) {
@@ -121,12 +111,6 @@ public class TobagoMultipartFormdataRequest implements HttpServletRequest {
     return null;
   }
 
-// ////////////////////////////////////////////// getter
-
-  public HttpServletRequest getRequest() {
-    return request;
-  }
-  
 // ////////////////////////////////////////////// overwriten
 
 
@@ -152,198 +136,4 @@ public class TobagoMultipartFormdataRequest implements HttpServletRequest {
   public Map getParameterMap() {
     return parameters;
   }
-
-// ////////////////////////////////////////////// deligates
-
-  public String getAuthType() {
-    return request.getAuthType();
-  }
-
-  public Cookie[] getCookies() {
-    return request.getCookies();
-  }
-
-  public long getDateHeader(String s) {
-    return request.getDateHeader(s);
-  }
-
-  public String getHeader(String s) {
-    return request.getHeader(s);
-  }
-
-  public Enumeration getHeaders(String s) {
-    return request.getHeaders(s);
-  }
-
-  public Enumeration getHeaderNames() {
-    return request.getHeaderNames();
-  }
-
-  public int getIntHeader(String s) {
-    return request.getIntHeader(s);
-  }
-
-  public String getMethod() {
-    return request.getMethod();
-  }
-
-  public String getPathInfo() {
-    return request.getPathInfo();
-  }
-
-  public String getPathTranslated() {
-    return request.getPathTranslated();
-  }
-
-  public String getContextPath() {
-    return request.getContextPath();
-  }
-
-  public String getQueryString() {
-    return request.getQueryString();
-  }
-
-  public String getRemoteUser() {
-    return request.getRemoteUser();
-  }
-
-  public boolean isUserInRole(String s) {
-    return request.isUserInRole(s);
-  }
-
-  public Principal getUserPrincipal() {
-    return request.getUserPrincipal();
-  }
-
-  public String getRequestedSessionId() {
-    return request.getRequestedSessionId();
-  }
-
-  public String getRequestURI() {
-    return request.getRequestURI();
-  }
-
-  public StringBuffer getRequestURL() {
-    return request.getRequestURL();
-  }
-
-  public String getServletPath() {
-    return request.getServletPath();
-  }
-
-  public HttpSession getSession(boolean b) {
-    return request.getSession(b);
-  }
-
-  public HttpSession getSession() {
-    return request.getSession();
-  }
-
-  public boolean isRequestedSessionIdValid() {
-    return request.isRequestedSessionIdValid();
-  }
-
-  public boolean isRequestedSessionIdFromCookie() {
-    return request.isRequestedSessionIdFromCookie();
-  }
-
-  public boolean isRequestedSessionIdFromURL() {
-    return request.isRequestedSessionIdFromURL();
-  }
-
-  /**
-   * @deprecated
-   */
-  public boolean isRequestedSessionIdFromUrl() {
-    return request.isRequestedSessionIdFromUrl();
-  }
-
-  public Object getAttribute(String s) {
-    return request.getAttribute(s);
-  }
-
-  public Enumeration getAttributeNames() {
-    return request.getAttributeNames();
-  }
-
-  public String getCharacterEncoding() {
-    return request.getCharacterEncoding();
-  }
-
-  public void setCharacterEncoding(String s)
-      throws UnsupportedEncodingException {
-    request.setCharacterEncoding(s);
-  }
-
-  public int getContentLength() {
-    return request.getContentLength();
-  }
-
-  public String getContentType() {
-    return request.getContentType();
-  }
-
-  public ServletInputStream getInputStream() throws IOException {
-    return request.getInputStream();
-  }
-
-  public String getProtocol() {
-    return request.getProtocol();
-  }
-
-  public String getScheme() {
-    return request.getScheme();
-  }
-
-  public String getServerName() {
-    return request.getServerName();
-  }
-
-  public int getServerPort() {
-    return request.getServerPort();
-  }
-
-  public BufferedReader getReader() throws IOException {
-    return request.getReader();
-  }
-
-  public String getRemoteAddr() {
-    return request.getRemoteAddr();
-  }
-
-  public String getRemoteHost() {
-    return request.getRemoteHost();
-  }
-
-  public void setAttribute(String s, Object o) {
-    request.setAttribute(s, o);
-  }
-
-  public void removeAttribute(String s) {
-    request.removeAttribute(s);
-  }
-
-  public Locale getLocale() {
-    return request.getLocale();
-  }
-
-  public Enumeration getLocales() {
-    return request.getLocales();
-  }
-
-  public boolean isSecure() {
-    return request.isSecure();
-  }
-
-  public RequestDispatcher getRequestDispatcher(String s) {
-    return request.getRequestDispatcher(s);
-  }
-
-  /**
-   * @deprecated
-   */
-  public String getRealPath(String s) {
-    return request.getRealPath(s);
-  }
-
 }
