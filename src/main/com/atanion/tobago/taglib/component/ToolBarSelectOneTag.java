447d0bba0de38ec282fe1d034d061e6799450fc7
Volker Weber <weber@apache.org>
2005-04-11 14:04:10 +0000

    class for toolbarSelectOneTag

---
+++ src/main/com/atanion/tobago/taglib/component/ToolBarSelectOneTag.java
@@ -0,0 +1,12 @@
+package com.atanion.tobago.taglib.component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: weber
+ * Date: Apr 11, 2005
+ * Time: 2:21:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ToolBarSelectOneTag extends MenuSelectOneTag {
+  
+}
