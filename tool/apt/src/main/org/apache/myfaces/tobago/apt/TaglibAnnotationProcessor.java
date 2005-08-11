e7c7566c75d10d01bddbe0a3ee4330d7e69be8b6
Udo Schnurpfeil <lofwyr@apache.org>
2005-08-11 09:59:23 +0000

    move from apt -> tool/apt

---
+++ tool/apt/src/main/org/apache/myfaces/tobago/apt/TaglibAnnotationProcessor.java
@@ -0,0 +1,95 @@
+package org.apache.myfaces.tobago.apt;
+
+import com.sun.mirror.apt.AnnotationProcessor;
+import com.sun.mirror.apt.AnnotationProcessorEnvironment;
+import com.sun.mirror.apt.Filer;
+import com.sun.mirror.declaration.AnnotationTypeDeclaration;
+import com.sun.mirror.declaration.Declaration;
+import com.sun.mirror.util.DeclarationVisitors;
+import org.apache.commons.io.IOUtils;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Copyright (c) 2003 Atanion GmbH, Germany. All rights reserved.
+ * Created: Mar 22, 2005 8:14:29 PM
+ * User: bommel
+ * $Id: TaglibAnnotationProcessor.java,v 1.6 2005/05/11 15:20:34 bommel Exp $
+ */
+public class TaglibAnnotationProcessor implements AnnotationProcessor {
+
+  protected final AnnotationProcessorEnvironment env;
+  protected final Set<AnnotationTypeDeclaration> atds;
+  protected String packageName = "org.apache.myfaces.tobago.taglib.component";
+  protected String fileName = "tobago3.tld";
+  protected String packageKey = "-Apackage=";
+  protected String fileKey = "-Afile=";
+  public TaglibAnnotationProcessor(Set<AnnotationTypeDeclaration> atds,
+      AnnotationProcessorEnvironment env) {
+    this.atds = atds;
+    this.env = env;
+    for(Map.Entry<String,String> entry: env.getOptions().entrySet()) {
+      if (entry.getKey().startsWith(packageKey)) {
+        packageName = entry.getKey().substring(packageKey.length());
+      } else if (entry.getKey().startsWith(fileKey)) {
+        fileName = entry.getKey().substring(fileKey.length());
+      }
+
+    }
+
+    this.env.getMessager().printNotice("Starting annotation process");
+
+  }
+
+  public void process() {
+    TaglibAnnotationVisitor visitor = new TaglibAnnotationVisitor();
+    
+    for (AnnotationTypeDeclaration atd : atds) {
+      env.getMessager().printNotice("Collecting annotation "+atd);
+      Collection<Declaration> decls = env.getDeclarationsAnnotatedWith(atd);
+      for (Declaration decl : decls) {
+        decl.accept(DeclarationVisitors.getDeclarationScanner(visitor, DeclarationVisitors.NO_OP));
+      }
+    }
+    PrintWriter writer = null;
+    try {
+      env.getMessager().printNotice("Create DOM");
+      Document document = visitor.createDom();
+
+      writer = env.getFiler().createTextFile(Filer.Location.SOURCE_TREE, packageName,
+          new File(fileName), null);
+      TransformerFactory transFactory = TransformerFactory.newInstance();
+      Transformer transformer = transFactory.newTransformer();
+      transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC,
+          "-//Sun Microsystems, Inc.//DTD JSP Tag Library 1.2//EN");
+      transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM,
+          "http://java.sun.com/dtd/web-jsptaglibrary_1_2.dtd");
+      transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+      transformer.transform(new DOMSource(document), new StreamResult(writer));
+      env.getMessager().printNotice("Write to file " +packageName+ "."+fileName);
+    } catch (ParserConfigurationException e) {
+      // TODO
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    } catch (TransformerException e) {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    } catch (IOException e) {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    } finally {
+      IOUtils.closeQuietly(writer);
+    }
+  }
+}
---
+++ tool/apt/src/main/org/apache/myfaces/tobago/apt/ant/compiler/AptCompilerAdapter.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2003 Atanion GmbH, Germany. All rights reserved.
+ * Created: Feb 8, 2005 7:11:30 PM
+ * User: bommel
+ * $Id: AptCompilerAdapter.java,v 1.1 2005/03/05 15:00:36 bommel Exp $
+ */
+package org.apache.myfaces.tobago.apt.ant.compiler;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.compilers.DefaultCompilerAdapter;
+import org.apache.tools.ant.types.Commandline;
+import org.apache.tools.ant.types.Path;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import org.apache.myfaces.tobago.apt.ant.Apt;
+
+/**
+ * The implementation of the apt compiler for JDK 1.5
+ *
+ * @since Ant 1.7
+ */
+public class AptCompilerAdapter extends DefaultCompilerAdapter {
+
+    /**
+     * Integer returned by the "Modern" jdk1.3 compiler to indicate success.
+     */
+    private static final int APT_COMPILER_SUCCESS = 0;
+    public static final String APT_ENTRY_POINT = "com.sun.tools.apt.Main";
+    public static final String APT_METHOD_NAME = "compile";
+
+    protected Apt getApt() {
+        return (Apt) getJavac();
+    }
+
+    static void setAptCommandlineSwitches(Apt apt, Commandline cmd) {
+
+        if (!apt.isCompile()) {
+            cmd.createArgument().setValue("-nocompile");
+        }
+
+        // Process the factory class
+        String factory = apt.getFactory();
+        if (factory != null) {
+            cmd.createArgument().setValue("-factory");
+            cmd.createArgument().setValue(factory);
+        }
+
+        // Process the factory path
+        Path factoryPath = apt.getFactoryPath();
+        if (factoryPath != null) {
+            cmd.createArgument().setValue("-factorypath");
+            cmd.createArgument().setPath(factoryPath);
+        }
+
+        File preprocessDir = apt.getPreprocessDir();
+        if (preprocessDir != null) {
+            cmd.createArgument().setValue("-s");
+            cmd.createArgument().setFile(preprocessDir);
+        }
+
+        // Process the processor options
+        Vector options = apt.getOptions();
+        Enumeration elements = options.elements();
+        Apt.Option opt;
+        StringBuffer arg;
+        while (elements.hasMoreElements()) {
+            opt = (Apt.Option) elements.nextElement();
+            arg = new StringBuffer();
+            arg.append("-A").append(opt.getName());
+            if (opt.getValue() != null) {
+                arg.append("=").append(opt.getValue());
+            }
+            cmd.createArgument().setValue(arg.toString());
+        }
+    }
+
+    protected void setAptCommandlineSwitches(Commandline cmd) {
+        // Process the nocompile flag
+        Apt apt = getApt();
+        setAptCommandlineSwitches(apt, cmd);
+    }
+
+    /**
+     * Run the compilation.
+     *
+     * @throws BuildException if the compilation has problems.
+     */
+    public boolean execute() throws BuildException {
+        attributes.log("Using apt compiler", Project.MSG_VERBOSE);
+        Commandline cmd = setupModernJavacCommand();
+        setAptCommandlineSwitches(cmd);
+
+        // Use reflection to be able to build on all JDKs:
+        try {
+            Class c = Class.forName(APT_ENTRY_POINT);
+            Object compiler = c.newInstance();
+            Method compile = c.getMethod(APT_METHOD_NAME,
+                    new Class[]{(new String[]{}).getClass()});
+            int result = ((Integer) compile.invoke
+                    (compiler, new Object[]{cmd.getArguments()}))
+                    .intValue();
+            return (result == APT_COMPILER_SUCCESS);
+        } catch (BuildException be) {
+            throw be;
+        } catch (Exception ex) {
+            throw new BuildException("Error starting apt compiler",
+                    ex, location);
+        }
+    }
+}
