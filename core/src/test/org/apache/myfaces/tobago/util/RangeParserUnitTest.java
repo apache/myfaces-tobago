78e7a01336d2babe5d147723427047721c96a02c
Udo Schnurpfeil <lofwyr@apache.org>
2005-08-12 13:33:52 +0000

    add apache license

--- core/src/test/org/apache/myfaces/tobago/util/RangeParserUnitTest.java
+++ core/src/test/org/apache/myfaces/tobago/util/RangeParserUnitTest.java
@@ -1,4 +1,19 @@
 /*
+ * Copyright 2002-2005 atanion GmbH.
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/*
  * Copyright (c) 2003 Atanion GmbH, Germany
  * All rights reserved. Created 17.03.2004 11:16:26.
  * $Id:RangeParserUnitTest.java 1300 2005-08-10 16:40:23 +0200 (Mi, 10 Aug 2005) lofwyr $
