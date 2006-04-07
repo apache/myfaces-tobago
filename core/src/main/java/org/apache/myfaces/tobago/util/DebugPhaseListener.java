394b281bfe78e4f4a806e889f19c7aea35105beb
Volker Weber <weber@apache.org>
2006-04-07 08:58:02 +0000

    write response and phase time information to logs

--- core/src/main/java/org/apache/myfaces/tobago/util/DebugPhaseListener.java
+++ core/src/main/java/org/apache/myfaces/tobago/util/DebugPhaseListener.java
@@ -19,9 +19,12 @@ package org.apache.myfaces.tobago.util;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
+import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,12 +35,64 @@ import javax.faces.event.PhaseListener;
  */
 public class DebugPhaseListener implements PhaseListener {
   private static final Log LOG = LogFactory.getLog(DebugPhaseListener.class);
+  private static final String KEY = DebugPhaseListener.class.getName() + "_ID_";
   public void afterPhase(PhaseEvent phaseEvent) {
-    LOG.debug("After Phase :" + phaseEvent.getPhaseId());
+    if (LOG.isInfoEnabled()) {
+      Date end = new Date();
+      Map map = FacesContext.getCurrentInstance().getExternalContext()
+          .getRequestMap();
+      map.put(KEY + phaseEvent.getPhaseId().getOrdinal() + "E", end);
+
+      if (LOG.isTraceEnabled()) {
+        LOG.trace("After Phase :" + phaseEvent.getPhaseId()
+            + " Time=" + end.getTime());
+      }
+
+      if (LOG.isDebugEnabled()) {
+        Date start
+            = (Date) map.get(KEY + phaseEvent.getPhaseId().getOrdinal() + "S");
+        LOG.debug("Phase " + phaseEvent.getPhaseId() + " needs "
+            + (end.getTime() - start.getTime() + " milliseconds"));
+      }
+
+      if (phaseEvent.getPhaseId().getOrdinal() == 6) {
+        Date start = (Date) map.get(KEY + "1S");
+        if (start != null) {
+          LOG.info("Total response time : "
+              + (end.getTime() - start.getTime() + " milliseconds"));
+        }
+      }
+    }
+
   }
 
   public void beforePhase(PhaseEvent phaseEvent) {
-    LOG.debug("Before Phase :" + phaseEvent.getPhaseId());
+    if (LOG.isInfoEnabled()) {
+      Date start = null;
+      Map map = null;
+      if (LOG.isDebugEnabled() || phaseEvent.getPhaseId().getOrdinal() == 1) {
+        start = new Date();
+        map = FacesContext.getCurrentInstance().getExternalContext()
+            .getRequestMap();
+        map.put(KEY + phaseEvent.getPhaseId().getOrdinal() + "S", start);
+      }
+
+      if (LOG.isDebugEnabled()) {
+        Date end = null;
+        int ordinal = phaseEvent.getPhaseId().getOrdinal();
+        while (end == null && ordinal > 0) {
+          end = (Date) map.get(KEY + --ordinal + "E");
+        }
+        if (end != null) {
+          LOG.debug("Time between phases " + ordinal + " and " + phaseEvent.getPhaseId().getOrdinal() + ": "
+              + (start.getTime() - end.getTime()) + " milliseconds");
+        }
+      }
+      if (LOG.isTraceEnabled()) {
+        LOG.trace("Before Phase :" + phaseEvent.getPhaseId()
+            + " Time=" + start.getTime());
+      }
+    }
   }
 
   public PhaseId getPhaseId() {
