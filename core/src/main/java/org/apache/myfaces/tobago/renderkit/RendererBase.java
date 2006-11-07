a9a480f0af894e00bbbb1f9927127e700aadcfe3
Bernd Bohmann <bommel@apache.org>
2006-11-07 20:06:24 +0000

    checkstyle

--- core/src/main/java/org/apache/myfaces/tobago/renderkit/RendererBase.java
+++ core/src/main/java/org/apache/myfaces/tobago/renderkit/RendererBase.java
@@ -43,7 +43,6 @@ import javax.faces.FacesException;
 import java.awt.Dimension;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Arrays;
