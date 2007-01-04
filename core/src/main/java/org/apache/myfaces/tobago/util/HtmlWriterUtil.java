da5a85cefb0ca6634b400e218098c11faeb42168
Volker Weber <weber@apache.org>
2007-01-04 13:39:41 +0000

    TOBAGO-218 (tobago throws an exception after validation form in not English locale) TOBAGO-230 (Exception when using umlauts in confirm component)

--- core/src/main/java/org/apache/myfaces/tobago/util/HtmlWriterUtil.java
+++ core/src/main/java/org/apache/myfaces/tobago/util/HtmlWriterUtil.java
@@ -175,10 +175,15 @@ public final class HtmlWriterUtil {
    */
   private void writeDecRef(final char ch) throws IOException {
     if (ch == '\u20ac') {
-      out.write("&euro;");
+      out.write('&');
+      out.write('e');
+      out.write('u');
+      out.write('r');
+      out.write('o');
       return;
     }
-    out.write("&#");
+    out.write('&');
+    out.write('#');
     // Formerly used String.valueOf().  This version tests out
     // about 40% faster in a microbenchmark (and on systems where GC is
     // going gonzo, it should be even better)
