2799a697c819d1a6fa7866011b3be3cf1515520f
Udo Schnurpfeil <lofwyr@apache.org>
2007-01-26 20:01:23 +0000

    SychronizeNavigationPhaseListener is needed to update the Navigation Tree when using direct links.

--- example/demo/src/main/java/org/apache/myfaces/tobago/example/demo/Navigation.java
+++ example/demo/src/main/java/org/apache/myfaces/tobago/example/demo/Navigation.java
@@ -31,6 +31,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.Enumeration;
 
 /**
  * User: lofwyr
@@ -78,6 +79,18 @@ public class Navigation {
   public String navigate() {
     Node selected = (Node) state.getMarker().getUserObject();
     return selected.getOutcome();
+  }
+
+  public void updateMarker(String viewId) {
+    Enumeration enumeration = tree.depthFirstEnumeration();
+    while (enumeration.hasMoreElements()) {
+      DefaultMutableTreeNode maybeMarker = ((DefaultMutableTreeNode) enumeration.nextElement());
+      Node node = (Node) maybeMarker.getUserObject();
+      if (viewId.contains(node.getOutcome())) {
+        state.setMarker(maybeMarker);
+        break;
+      }
+    }
   }
 
   public DefaultMutableTreeNode getTree() {
