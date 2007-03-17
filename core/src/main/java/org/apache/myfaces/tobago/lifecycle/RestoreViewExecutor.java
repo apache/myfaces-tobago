32183a9e17575b3f05413ab3bd5de8937d6318b3
Volker Weber <weber@apache.org>
2007-03-17 20:23:29 +0000

    TOBAGO-312 (Integrate handling of ajax requests into standard lifecycle)   reformat code for checkstyle

--- core/src/main/java/org/apache/myfaces/tobago/lifecycle/RestoreViewExecutor.java
+++ core/src/main/java/org/apache/myfaces/tobago/lifecycle/RestoreViewExecutor.java
@@ -1,22 +1,21 @@
+package org.apache.myfaces.tobago.lifecycle;
+
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package org.apache.myfaces.lifecycle;
 
 import java.io.IOException;
 
@@ -27,112 +26,112 @@ import javax.faces.component.UIViewRoot;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseId;
-import javax.portlet.PortletRequest;
+//import javax.portlet.PortletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.myfaces.portlet.MyFacesGenericPortlet;
-import org.apache.myfaces.portlet.PortletUtil;
-import org.apache.myfaces.shared_impl.util.RestoreStateUtils;
-import org.apache.myfaces.util.DebugUtils;
+//import org.apache.myfaces.portlet.MyFacesGenericPortlet;
+//import org.apache.myfaces.portlet.PortletUtil;
+//import org.apache.myfaces.shared_impl.util.RestoreStateUtils;
+//import org.apache.myfaces.util.DebugUtils;
 
 /**
  * Implements the lifecycle as described in Spec. 1.0 PFD Chapter 2
- * @author Nikolay Petrov
  *
  * Restore view phase (JSF Spec 2.2.1)
  */
 class RestoreViewExecutor implements PhaseExecutor {
 
-	private static final Log log = LogFactory.getLog(LifecycleImpl.class);
+  private static final Log LOG = LogFactory.getLog(RestoreViewExecutor.class);
 
-	public boolean execute(FacesContext facesContext) {
-		if(facesContext.getViewRoot() != null) {
-			facesContext.getViewRoot().setLocale(facesContext.getExternalContext().getRequestLocale());
-			RestoreStateUtils.recursivelyHandleComponentReferencesAndSetValid(facesContext, facesContext.getViewRoot());
-			return false;
-		}
+  public boolean execute(FacesContext facesContext) {
+    if(facesContext.getViewRoot() != null) {
+      facesContext.getViewRoot().setLocale(facesContext.getExternalContext().getRequestLocale());
+//      RestoreStateUtils.recursivelyHandleComponentReferencesAndSetValid(facesContext, facesContext.getViewRoot());
+      return false;
+    }
 
-		// Derive view identifier
-		String viewId = deriveViewId(facesContext);
+    // Derive view identifier
+    String viewId = deriveViewId(facesContext);
 
-		if (viewId == null) {
-			ExternalContext externalContext = facesContext.getExternalContext();
+    if (viewId == null) {
+      ExternalContext externalContext = facesContext.getExternalContext();
 
-			if(externalContext.getRequestServletPath() == null) {
-				return true;
-			}
-			
-			if (!externalContext.getRequestServletPath().endsWith("/")) {
-				try {
-					externalContext.redirect(externalContext.getRequestServletPath() + "/");
-					facesContext.responseComplete();
-					return true;
-				} catch (IOException e) {
-					throw new FacesException("redirect failed", e);
-				}
-			}
-		}
+      if(externalContext.getRequestServletPath() == null) {
+        return true;
+      }
 
-		Application application = facesContext.getApplication();
-		ViewHandler viewHandler = application.getViewHandler();
+      if (!externalContext.getRequestServletPath().endsWith("/")) {
+        try {
+          externalContext.redirect(externalContext.getRequestServletPath() + "/");
+          facesContext.responseComplete();
+          return true;
+        } catch (IOException e) {
+          throw new FacesException("redirect failed", e);
+        }
+      }
+    }
 
-		// boolean viewCreated = false;
-		UIViewRoot viewRoot = viewHandler.restoreView(facesContext, viewId);
-		if (viewRoot == null) {
-			viewRoot = viewHandler.createView(facesContext, viewId);
-			viewRoot.setViewId(viewId);
-			facesContext.renderResponse();
-			// viewCreated = true;
-		}
+    Application application = facesContext.getApplication();
+    ViewHandler viewHandler = application.getViewHandler();
 
-		facesContext.setViewRoot(viewRoot);
+    // boolean viewCreated = false;
+    UIViewRoot viewRoot = viewHandler.restoreView(facesContext, viewId);
+    if (viewRoot == null) {
+      viewRoot = viewHandler.createView(facesContext, viewId);
+      viewRoot.setViewId(viewId);
+      facesContext.renderResponse();
+      // viewCreated = true;
+    }
 
-		if (facesContext.getExternalContext().getRequestParameterMap().isEmpty()) {
-			// no POST or query parameters --> set render response flag
-			facesContext.renderResponse();
-		}
+    facesContext.setViewRoot(viewRoot);
 
-		RestoreStateUtils.recursivelyHandleComponentReferencesAndSetValid(facesContext, viewRoot);
-		return false;
-	}
+    if (facesContext.getExternalContext().getRequestParameterMap().isEmpty()) {
+      // no POST or query parameters --> set render response flag
+      facesContext.renderResponse();
+    }
 
-	public PhaseId getPhase() {
-		return PhaseId.RESTORE_VIEW;
-	}
+//    RestoreStateUtils.recursivelyHandleComponentReferencesAndSetValid(facesContext, viewRoot);
+    return false;
+  }
 
-	private static String deriveViewId(FacesContext facesContext) {
-		ExternalContext externalContext = facesContext.getExternalContext();
+  public PhaseId getPhase() {
+    return PhaseId.RESTORE_VIEW;
+  }
 
-		if (PortletUtil.isPortletRequest(facesContext)) {
-			PortletRequest request = (PortletRequest) externalContext.getRequest();
-			return request.getParameter(MyFacesGenericPortlet.VIEW_ID);
-		}
+  private static String deriveViewId(FacesContext facesContext) {
+    ExternalContext externalContext = facesContext.getExternalContext();
 
-		String viewId = externalContext.getRequestPathInfo(); // getPathInfo
-		if (viewId == null) {
-			// No extra path info found, so it is propably extension mapping
-			viewId = externalContext.getRequestServletPath(); // getServletPath
-			DebugUtils.assertError(viewId != null, log,
-					"RequestServletPath is null, cannot determine viewId of current page.");
-			if (viewId == null)
-				return null;
+//    if (PortletUtil.isPortletRequest(facesContext)) {
+//      PortletRequest request = (PortletRequest) externalContext.getRequest();
+//      return request.getParameter(MyFacesGenericPortlet.VIEW_ID);
+//    }
 
-			// TODO: JSF Spec 2.2.1 - what do they mean by "if the default
-			// ViewHandler implementation is used..." ?
-			String defaultSuffix = externalContext.getInitParameter(ViewHandler.DEFAULT_SUFFIX_PARAM_NAME);
-			String suffix = defaultSuffix != null ? defaultSuffix : ViewHandler.DEFAULT_SUFFIX;
-			DebugUtils.assertError(suffix.charAt(0) == '.', log, "Default suffix must start with a dot!");
+    String viewId = externalContext.getRequestPathInfo(); // getPathInfo
+    if (viewId == null) {
+      // No extra path info found, so it is propably extension mapping
+      viewId = externalContext.getRequestServletPath(); // getServletPath
+//      DebugUtils.assertError(viewId != null, LOG,
+//          "RequestServletPath is null, cannot determine viewId of current page.");
+      if (viewId == null) {
+        return null;
+      }
 
-			int dot = viewId.lastIndexOf('.');
-			if (dot == -1) {
-				log.error("Assumed extension mapping, but there is no extension in " + viewId);
-				viewId = null;
-			} else {
-				viewId = viewId.substring(0, dot) + suffix;
-			}
-		}
+      // TODO: JSF Spec 2.2.1 - what do they mean by "if the default
+      // ViewHandler implementation is used..." ?
+      String defaultSuffix = externalContext.getInitParameter(ViewHandler.DEFAULT_SUFFIX_PARAM_NAME);
+      String suffix = defaultSuffix != null ? defaultSuffix : ViewHandler.DEFAULT_SUFFIX;
+//      DebugUtils.assertError(suffix.charAt(0) == '.', LOG, "Default suffix must start with a dot!");
 
-		return viewId;
-	}
+      int dot = viewId.lastIndexOf('.');
+      if (dot == -1) {
+        LOG.error("Assumed extension mapping, but there is no extension in " + viewId);
+        viewId = null;
+      } else {
+        viewId = viewId.substring(0, dot) + suffix;
+      }
+    }
+
+    return viewId;
+  }
 }
--- core/src/main/java/org/apache/myfaces/tobago/lifecycle/TobagoLifecycle.java
+++ core/src/main/java/org/apache/myfaces/tobago/lifecycle/TobagoLifecycle.java
@@ -1,22 +1,21 @@
+package org.apache.myfaces.tobago.lifecycle;
+
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package org.apache.myfaces.lifecycle;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,175 +28,175 @@ import javax.faces.lifecycle.Lifecycle;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.myfaces.util.DebugUtils;
+//import org.apache.myfaces.util.DebugUtils;
 
 /**
  * Implements the lifecycle as described in Spec. 1.0 PFD Chapter 2
- * @author Manfred Geiler
- * @author Nikolay Petrov
  */
-public class LifecycleImpl
-        extends Lifecycle
-{
-    private static final Log log = LogFactory.getLog(LifecycleImpl.class);
+public class TobagoLifecycle extends Lifecycle {
 
-    private PhaseExecutor[] lifecycleExecutors;
-    private PhaseExecutor renderExecutor;
+  private static final Log LOG = LogFactory.getLog(TobagoLifecycle.class);
 
-    private final List _phaseListenerList = new ArrayList();
+  private PhaseExecutor[] lifecycleExecutors;
+  private PhaseExecutor renderExecutor;
 
-    /**
-     * Lazy cache for returning _phaseListenerList as an Array.
-     */
-    private PhaseListener[] _phaseListenerArray = null;
+  private final List<PhaseListener> phaseListenerList = new ArrayList<PhaseListener>();
 
-    public LifecycleImpl() {
-        // hide from public access
-    	lifecycleExecutors = new PhaseExecutor[] {
-    			new RestoreViewExecutor(),
-    			new ApplyRequestValuesExecutor(),
-    			new ProcessValidationsExecutor(),
-    			new UpdateModelValuesExecutor(),
-    			new InvokeApplicationExecutor()
-    	};
+  /**
+   * Lazy cache for returning phaseListenerList as an Array.
+   */
+  private PhaseListener[] phaseListenerArray = null;
 
-    	renderExecutor = new RenderResponseExecutor();
+  public TobagoLifecycle() {
+    // hide from public access
+    lifecycleExecutors = new PhaseExecutor[] {
+        new RestoreViewExecutor(),
+        new ApplyRequestValuesExecutor(),
+        new ProcessValidationsExecutor(),
+        new UpdateModelValuesExecutor(),
+        new InvokeApplicationExecutor()
+    };
+
+    renderExecutor = new RenderResponseExecutor();
+  }
+
+  public void execute(FacesContext facesContext) throws FacesException {
+    PhaseListenerManager phaseListenerMgr
+        = new PhaseListenerManager(this, facesContext, getPhaseListeners());
+    for (PhaseExecutor executor : lifecycleExecutors) {
+      if (executePhase(facesContext, executor, phaseListenerMgr)) {
+        return;
+      }
+    }
+  }
+
+  private boolean executePhase(FacesContext facesContext, PhaseExecutor executor,
+                               PhaseListenerManager phaseListenerMgr)
+      throws FacesException {
+
+    boolean skipFurtherProcessing = false;
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("entering " + executor.getPhase() + " in " + TobagoLifecycle.class.getName());
     }
 
-    public void execute(FacesContext facesContext) throws FacesException {
-        PhaseListenerManager phaseListenerMgr = new PhaseListenerManager(this, facesContext, getPhaseListeners());
-        for(int executorIndex = 0;executorIndex < lifecycleExecutors.length;executorIndex++) {
-        	if(executePhase(facesContext, lifecycleExecutors[executorIndex], phaseListenerMgr)) {
-        		return;
-        	}
-        }
+    try {
+      phaseListenerMgr.informPhaseListenersBefore(executor.getPhase());
+
+      if(isResponseComplete(facesContext, executor.getPhase(), true)) {
+        // have to return right away
+        return true;
+      }
+      if(shouldRenderResponse(facesContext, executor.getPhase(), true)) {
+        skipFurtherProcessing = true;
+      }
+
+      if(executor.execute(facesContext)) {
+        return true;
+      }
+    } finally {
+      phaseListenerMgr.informPhaseListenersAfter(executor.getPhase());
     }
 
 
-    private boolean executePhase(FacesContext facesContext, PhaseExecutor executor,
-    		PhaseListenerManager phaseListenerMgr) throws FacesException {
-		boolean skipFurtherProcessing = false;
-        if (log.isTraceEnabled()) {
-        	log.trace("entering " + executor.getPhase() + " in " + LifecycleImpl.class.getName());
-        }
-
-        try {
-            phaseListenerMgr.informPhaseListenersBefore(executor.getPhase());
-
-            if(isResponseComplete(facesContext, executor.getPhase(), true)) {
-                // have to return right away
-            	return true;
-            }
-            if(shouldRenderResponse(facesContext, executor.getPhase(), true)) {
-            	skipFurtherProcessing = true;
-            }
-
-            if(executor.execute(facesContext)) {
-            	return true;
-            }
-        } finally {
-            phaseListenerMgr.informPhaseListenersAfter(executor.getPhase());
-        }
-
-
-        if (isResponseComplete(facesContext, executor.getPhase(), false)
-        		|| shouldRenderResponse(facesContext, executor.getPhase(), false)) {
-        	// since this phase is completed we don't need to return right away even if the response is completed
-        	skipFurtherProcessing = true;
-        }
-
-        if (!skipFurtherProcessing && log.isTraceEnabled()) {
-			log.trace("exiting " + executor.getPhase() + " in " + LifecycleImpl.class.getName());
-        }
-
-        return skipFurtherProcessing;
+    if (isResponseComplete(facesContext, executor.getPhase(), false)
+        || shouldRenderResponse(facesContext, executor.getPhase(), false)) {
+      // since this phase is completed we don't need to return right away even if the response is completed
+      skipFurtherProcessing = true;
     }
 
-    public void render(FacesContext facesContext) throws FacesException {
-    	// if the response is complete we should not be invoking the phase listeners
-        if(isResponseComplete(facesContext, renderExecutor.getPhase(), true)) {
-        	return;
-        }
-        if (log.isTraceEnabled()) log.trace("entering " + renderExecutor.getPhase() + " in " + LifecycleImpl.class.getName());
-
-        PhaseListenerManager phaseListenerMgr = new PhaseListenerManager(this, facesContext, getPhaseListeners());
-
-        try {
-            phaseListenerMgr.informPhaseListenersBefore(renderExecutor.getPhase());
-            // also possible that one of the listeners completed the response
-            if(isResponseComplete(facesContext, renderExecutor.getPhase(), true)) {
-            	return;
-            }
-
-            renderExecutor.execute(facesContext);
-        } finally {
-            phaseListenerMgr.informPhaseListenersAfter(renderExecutor.getPhase());
-        }
-
-        if (log.isTraceEnabled()) {
-            //Note: DebugUtils Logger must also be in trace level
-            DebugUtils.traceView("View after rendering");
-        }
-
-        if (log.isTraceEnabled()) {
-        	log.trace("exiting " + renderExecutor.getPhase() + " in " + LifecycleImpl.class.getName());
-        }
+    if (!skipFurtherProcessing && LOG.isTraceEnabled()) {
+      LOG.trace("exiting " + executor.getPhase() + " in " + TobagoLifecycle.class.getName());
     }
 
-    private boolean isResponseComplete(FacesContext facesContext, PhaseId phase, boolean before) {
-		boolean flag = false;
-	    if (facesContext.getResponseComplete()) {
-	        if (log.isDebugEnabled()) {
-				log.debug("exiting from lifecycle.execute in " + phase
-						+ " because getResponseComplete is true from one of the " +
-						(before ? "before" : "after") + " listeners");
-	        }
-	        flag = true;
-	    }
-	    return flag;
-	}
+    return skipFurtherProcessing;
+  }
 
-	private boolean shouldRenderResponse(FacesContext facesContext, PhaseId phase, boolean before) {
-			boolean flag = false;
-	    if (facesContext.getRenderResponse()) {
-	        if (log.isDebugEnabled()) {
-				log.debug("exiting from lifecycle.execute in " + phase
-						+ " because getRenderResponse is true from one of the " +
-						(before ? "before" : "after") + " listeners");
-	        }
-	        flag = true;
-	    }
-	    return flag;
-	}
+  public void render(FacesContext facesContext) throws FacesException {
+    // if the response is complete we should not be invoking the phase listeners
+    if(isResponseComplete(facesContext, renderExecutor.getPhase(), true)) {
+      return;
+    }
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("entering " + renderExecutor.getPhase() + " in " + TobagoLifecycle.class.getName());
+    }
 
-    public void addPhaseListener(PhaseListener phaseListener) {
-		if (phaseListener == null) {
-			throw new NullPointerException("PhaseListener must not be null.");
-		}
-		synchronized (_phaseListenerList) {
-			_phaseListenerList.add(phaseListener);
-			_phaseListenerArray = null; // reset lazy cache array
-		}
-	}
+    PhaseListenerManager phaseListenerMgr = new PhaseListenerManager(this, facesContext, getPhaseListeners());
 
-	public void removePhaseListener(PhaseListener phaseListener) {
-		if (phaseListener == null) {
-			throw new NullPointerException("PhaseListener must not be null.");
-		}
-		synchronized (_phaseListenerList) {
-			_phaseListenerList.remove(phaseListener);
-			_phaseListenerArray = null; // reset lazy cache array
-		}
-	}
+    try {
+      phaseListenerMgr.informPhaseListenersBefore(renderExecutor.getPhase());
+      // also possible that one of the listeners completed the response
+      if(isResponseComplete(facesContext, renderExecutor.getPhase(), true)) {
+        return;
+      }
 
-	public PhaseListener[] getPhaseListeners() {
-		synchronized (_phaseListenerList) {
-			// (re)build lazy cache array if necessary
-			if (_phaseListenerArray == null) {
-				_phaseListenerArray = (PhaseListener[]) _phaseListenerList.toArray(new PhaseListener[_phaseListenerList
-						.size()]);
-			}
-			return _phaseListenerArray;
-		}
-	}
+      renderExecutor.execute(facesContext);
+    } finally {
+      phaseListenerMgr.informPhaseListenersAfter(renderExecutor.getPhase());
+    }
+
+    if (LOG.isTraceEnabled()) {
+      //Note: DebugUtils Logger must also be in trace level
+//      DebugUtils.traceView("View after rendering");
+    }
+
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("exiting " + renderExecutor.getPhase() + " in " + TobagoLifecycle.class.getName());
+    }
+  }
+
+  private boolean isResponseComplete(FacesContext facesContext, PhaseId phase, boolean before) {
+    boolean flag = false;
+    if (facesContext.getResponseComplete()) {
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("exiting from lifecycle.execute in " + phase
+            + " because getResponseComplete is true from one of the "
+            + (before ? "before" : "after") + " listeners");
+      }
+      flag = true;
+    }
+    return flag;
+  }
+
+  private boolean shouldRenderResponse(FacesContext facesContext, PhaseId phase, boolean before) {
+    boolean flag = false;
+    if (facesContext.getRenderResponse()) {
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("exiting from lifecycle.execute in " + phase
+            + " because getRenderResponse is true from one of the "
+            + (before ? "before" : "after") + " listeners");
+      }
+      flag = true;
+    }
+    return flag;
+  }
+
+  public void addPhaseListener(PhaseListener phaseListener) {
+    if (phaseListener == null) {
+      throw new NullPointerException("PhaseListener must not be null.");
+    }
+    synchronized (phaseListenerList) {
+      phaseListenerList.add(phaseListener);
+      phaseListenerArray = null; // reset lazy cache array
+    }
+  }
+
+  public void removePhaseListener(PhaseListener phaseListener) {
+    if (phaseListener == null) {
+      throw new NullPointerException("PhaseListener must not be null.");
+    }
+    synchronized (phaseListenerList) {
+      phaseListenerList.remove(phaseListener);
+      phaseListenerArray = null; // reset lazy cache array
+    }
+  }
+
+  public PhaseListener[] getPhaseListeners() {
+    synchronized (phaseListenerList) {
+      // (re)build lazy cache array if necessary
+      if (phaseListenerArray == null) {
+        phaseListenerArray = phaseListenerList.toArray(new PhaseListener[phaseListenerList.size()]);
+      }
+      return phaseListenerArray;
+    }
+  }
 }
