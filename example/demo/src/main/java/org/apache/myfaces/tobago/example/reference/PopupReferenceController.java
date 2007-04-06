0683036fffc16aa87d818e82144b1a6fd5b8a750
Volker Weber <weber@apache.org>
2007-04-06 21:52:35 +0000

    checkstyle

--- example/demo/src/main/java/org/apache/myfaces/tobago/example/reference/PopupReferenceController.java
+++ example/demo/src/main/java/org/apache/myfaces/tobago/example/reference/PopupReferenceController.java
@@ -1,3 +1,5 @@
+package org.apache.myfaces.tobago.example.reference;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,8 +18,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-package org.apache.myfaces.tobago.example.reference;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
