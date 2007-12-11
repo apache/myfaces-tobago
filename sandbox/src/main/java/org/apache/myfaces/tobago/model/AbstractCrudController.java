d483e271ace13ebac978bcdb494a789e895baa13
Udo Schnurpfeil <lofwyr@apache.org>
2007-12-11 10:39:13 +0000

    TOBAGO-519: Working on the CRUD stuff

--- sandbox/src/main/java/org/apache/myfaces/tobago/model/AbstractCrudController.java
+++ sandbox/src/main/java/org/apache/myfaces/tobago/model/AbstractCrudController.java
@@ -17,25 +17,17 @@ package org.apache.myfaces.tobago.model;
  * limitations under the License.
  */
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-
 public abstract class AbstractCrudController implements Crud {
-
-  private static final Log LOG = LogFactory.getLog(AbstractCrudController.class);
 
   private String defaultOutcome;
 
   private boolean showDetail;
 
-  private String filterExpression;
-
-  private boolean initialized;
-
   private boolean itemEditable;
+
+  protected AbstractCrudController() {
+    reset();
+  }
 
   /**
    * @see Crud#getShowDetail()
@@ -45,68 +37,28 @@ public abstract class AbstractCrudController implements Crud {
   }
 
   /**
-   * @see org.apache.myfaces.tobago.model.Crud#isInitialized()
-   */
-  public final boolean isInitialized() {
-    return initialized;
-  }
-
-  /**
    * @see Crud#isItemEditable()
    */
-  public boolean isItemEditable() {
+  public final boolean isItemEditable() {
     return itemEditable;
   }
-
-  /**
-   * @see Crud#initialize()
-   */
-  public final String initialize() {
-    if (!initialized) {
-      reset();
-
-      boolean success = doInitialization();
-      if (success) {
-        initialized = true;
-      } else {
-        FacesContext.getCurrentInstance().addMessage("",
-            new FacesMessage(FacesMessage.SEVERITY_ERROR, "CRUD component not successfully initalized.",
-                "Please see log for more details."));
-      }
-    }
-    return getDefaultOutcome();
-  }
-
-  /**
-   * <p>
-   * Hook for the implementation of business logic,
-   * after invoking the action {@link AbstractCrudController#initialize()}.
-   * If the execution of the business logic completed successfully, the method has to return <i>true</i>.
-   * Otherwise the method has to return <i>false</i>.
-   * </p>
-   *
-   * @return true if the method completed sucessfully, false if not
-   */
-  protected abstract boolean doInitialization();
 
   /**
    * @see Crud#deleteItem()
    */
   public final String deleteItem() {
-    if (initialized) {
-      doDeleteItem();
-    }
+    doDeleteItem();
     return getDefaultOutcome();
   }
 
   /**
    * <p>
-   * Hook for the implementation of business logic,
-   * after invoking the action {@link AbstractCrudController#deleteItem()}.
-   * If the execution of the business logic completed successfully, the method has to return <i>true</i>.
+   * Hook for the implementation of business logic, after invoking the action
+   * {@link AbstractCrudController#deleteItem()}. If the execution of the
+   * business logic completed successfully, the method has to return <i>true</i>.
    * Otherwise the method has to return <i>false</i>.
    * </p>
-   *
+   * 
    * @return true if the method completed sucessfully, false if not
    */
   protected abstract boolean doDeleteItem();
@@ -115,10 +67,8 @@ public abstract class AbstractCrudController implements Crud {
    * @see Crud#showItem()
    */
   public final String showItem() {
-    if (initialized) {
-      itemEditable = false;
-      showDetail = doShowItem(itemEditable);
-    }
+    itemEditable = false;
+    showDetail = doShowItem() ? true : false;
     return getDefaultOutcome();
   }
 
@@ -126,45 +76,40 @@ public abstract class AbstractCrudController implements Crud {
    * @see Crud#editItem()
    */
   public final String editItem() {
-    if (initialized) {
-      itemEditable = true;
-      showDetail = doShowItem(itemEditable);
-    }
+    itemEditable = true;
+    showDetail = doShowItem() ? true : false;
     return getDefaultOutcome();
   }
 
   /**
    * <p>
-   * Hook for the implementation of business logic,
-   * after invoking the action {@link AbstractCrudController#showItem()}.
-   * If the execution of the business logic completed successfully, the method has to return <i>true</i>.
+   * Hook for the implementation of business logic, after invoking the action
+   * {@link AbstractCrudController#showItem()}. If the execution of the
+   * business logic completed successfully, the method has to return <i>true</i>.
    * Otherwise the method has to return <i>false</i>.
    * </p>
-   *
-   * @param isEditAble Indicator if the Item is editable
+   * 
    * @return true if the method completed sucessfully, false if not
    */
-  protected abstract boolean doShowItem(boolean isEditAble);
+  protected abstract boolean doShowItem();
 
   /**
    * @see Crud#createItem()
    */
   public final String createItem() {
-    if (initialized) {
-      itemEditable = true;
-      showDetail = doCreateItem();
-    }
+    itemEditable = true;
+    showDetail = doCreateItem() ? true : false;
     return getDefaultOutcome();
   }
 
   /**
    * <p>
-   * Hook for the implementation of business logic,
-   * after invoking the action {@link AbstractCrudController#createItem()}.
-   * If the execution of the business logic completed successfully, the method has to return <i>true</i>.
+   * Hook for the implementation of business logic, after invoking the action
+   * {@link AbstractCrudController#createItem()}. If the execution of the
+   * business logic completed successfully, the method has to return <i>true</i>.
    * Otherwise the method has to return <i>false</i>.
    * </p>
-   *
+   * 
    * @return true if the method completed sucessfully, false if not
    */
   protected abstract boolean doCreateItem();
@@ -173,18 +118,18 @@ public abstract class AbstractCrudController implements Crud {
    * @see Crud#saveItem()
    */
   public final String saveItem() {
-    showDetail = !doSaveItem();
+    showDetail = doSaveItem() ? false : true;
     return getDefaultOutcome();
   }
 
   /**
    * <p>
-   * Hook for the implementation of business logic,
-   * after invoking the action {@link AbstractCrudController#saveItem()}.
-   * If the execution of the business logic completed successfully, the method has to return <i>true</i>.
+   * Hook for the implementation of business logic, after invoking the action
+   * {@link AbstractCrudController#saveItem()}. If the execution of the
+   * business logic completed successfully, the method has to return <i>true</i>.
    * Otherwise the method has to return <i>false</i>.
    * </p>
-   *
+   * 
    * @return true if the method completed sucessfully, false if not
    */
   protected abstract boolean doSaveItem();
@@ -193,70 +138,32 @@ public abstract class AbstractCrudController implements Crud {
    * @see Crud#cancelItem()
    */
   public final String cancelItem() {
-    showDetail = !doCancelItem();
+    // doCancelItem();
+    showDetail = false;
     return getDefaultOutcome();
   }
 
-  /**
-   * <p>
-   * Hook for the implementation of business logic,
-   * after invoking the action {@link AbstractCrudController#cancelItem()}.
-   * If the execution of the business logic completed successfully, the method has to return <i>true</i>.
-   * Otherwise the method has to return <i>false</i>.
-   * </p>
-   *
-   * @return true if the method completed sucessfully, false if not
-   */
-  protected abstract boolean doCancelItem();
-
-  /**
-   * @see Crud#filter()
-   */
-  public String filter() {
-    if (initialized) {
-      doFilter(filterExpression);
-    }
-    return defaultOutcome;
-  }
-
-  /**
-   * <p>
-   * Hook for the implementation of business logic, after invoking the action {@link AbstractCrudController#filter()}.
-   * </p>
-   *
-   * @param filterExpression The expression to use for searching/narrowing the items to show in the crud master view
-   */
-  public abstract void doFilter(String filterExpression);
-
-  /**
-   * @return The filterExpression to use for the action search
-   * @see Crud#getFilterExpression()
-   */
-  public String getFilterExpression() {
-    return this.filterExpression;
-  }
-
-  /**
-   * Set the filter expression to use when executing the action filter in order to narrow
-   * the items to show on the crud master view. @see Crud#setFilterExpression(String)
-   *
-   * @param filterExpression The expression to use for searching
-   */
-  public void setFilterExpression(String filterExpression) {
-    this.filterExpression = filterExpression;
-  }
+  //
+  // /**
+  // * <p>
+  // * Hook for the implementation of business logic, after invoking the action
+  // {@link AbstractCrudController#cancelItem()}.
+  // * </p>
+  // */
+  // public abstract void doCancelItem();
 
   /**
    * @return The outcome for the actions of the crud component
    */
-  private String getDefaultOutcome() {
+  protected final String getDefaultOutcome() {
     return this.defaultOutcome;
   }
 
   /**
    * Set the outcome for all actions of the crud component.
-   *
-   * @param defaultOutcome The outcome for all actions of the crud component
+   * 
+   * @param defaultOutcome
+   *          The outcome for all actions of the crud component
    */
   public final void setDefaultOutcome(String defaultOutcome) {
     this.defaultOutcome = defaultOutcome;
@@ -267,8 +174,6 @@ public abstract class AbstractCrudController implements Crud {
    */
   protected void reset() {
     showDetail = false;
-    filterExpression = null;
-    initialized = false;
     itemEditable = false;
   }
 
--- sandbox/src/main/java/org/apache/myfaces/tobago/model/Crud.java
+++ sandbox/src/main/java/org/apache/myfaces/tobago/model/Crud.java
@@ -19,105 +19,67 @@ package org.apache.myfaces.tobago.model;
 
 public interface Crud {
 
-  //    String getDefaultOutcome();
-
   /**
-   * Indicates if the crud component should show the crud detail view otherwise the crud master view is shown
-   *
-   * @return True if the detail view should be shown, false if the master view should be shown
+   * Indicates if the crud component should show the crud detail view
+     * otherwise the crud master view is shown
+   * 
+   * @return True if the detail view should be shown, false if the master view
+     *         should be shown
    */
   boolean getShowDetail();
 
   /**
-   * Managed bean (controller) method to execute to initialize or set up the CRUD component.
-   *
-   * @return The outcome after the method was executed
-   */
-  String initialize();
-
-  /**
    * Managed bean (controller) method to delete an selected item.
-   *
+   * 
    * @return The outcome after the method was executed
    */
   String deleteItem();
 
   /**
-   * Managed bean (controller) method to show an selected item on the CRUD detail view.
-   *
+   * Managed bean (controller) method to show an selected item on the CRUD
+     * detail view.
+   * 
    * @return The outcome after the method was executed
    */
   String showItem();
 
   /**
-   * Managed bean (controller) method to show and edit an selected item on the CRUD detail view.
-   *
+   * Managed bean (controller) method to show and edit an selected item on the
+     * CRUD detail view.
+   * 
    * @return The outcome after the method was executed
    */
   String editItem();
 
   /**
-   * Managed bean (controller) method to create a new item on the CRUD detail view.
-   *
+   * Managed bean (controller) method to create a new item on the CRUD detail
+     * view.
+   * 
    * @return The outcome after the method was executed
    */
   String createItem();
 
   /**
    * Managed bean (controller) method to save changes to a existing item or
-   * save a newly created item on the crud detail view.
-   *
+     * save a newly created item on the crud detail view.
+   * 
    * @return The outcome after the method was executed
    */
   String saveItem();
 
   /**
-   * Managed bean (controller) method to exit the crud detail view without saving.
-   *
+   * Managed bean (controller) method to exit the crud detail view without
+     * saving.
+   * 
    * @return The outcome after the method was executed
    */
   String cancelItem();
 
   /**
-   * Managed bean (controller) method to apply a filter/narrow the items to show in the CRUD component.
-   *
-   * @return The outcome after the method was executed
-   */
-  String filter();
-
-  /**
-   * @return The filter expression to use for the action filter
-   */
-  String getFilterExpression();
-
-  /**
-   * Set the filter expression to use when executing the action search in order to narrow
-   * the items to show on the CRUD master view.
-   *
-   * @param filterExpression The expression to use for filtering
-   */
-  void setFilterExpression(String filterExpression);
-
-  /**
-   * <p>
-   * Indicates whether the CRUD component is successfully initialized or not.
-   * </p>
-   * <p/>
-   * <b>Note: </b>If the CRUD component is not successfully initialized, all actions
-   * of the CRUD component should be ignored or disabled, expect the initialization action.
-   * <p/>
-   *
-   * @return True if the CRUD component is successfully initialized, otherwise false
-   */
-  boolean isInitialized();
-
-  /**
    * Indicates wether an item shown in the CRUD detail view is editable.
-   *
+   * 
    * @return true if the shown item is editable, false if not
    */
   boolean isItemEditable();
-
-  //    boolean showControllView();
 
 }
