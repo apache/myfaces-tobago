b50da71529f223b97f0f23ab8930db7fbe24615c
Bernd Bohmann <bommel@apache.org>
2008-03-17 15:30:01 +0000

    checkstyle

--- core/src/main/java/org/apache/myfaces/tobago/webapp/TobagoMultipartFormdataFilter.java
+++ core/src/main/java/org/apache/myfaces/tobago/webapp/TobagoMultipartFormdataFilter.java
@@ -27,7 +27,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.File;
 import java.util.Locale;
