a7099d57785c3b16ad9514710b125e3b0b54fc84
Bernd Bohmann <bommel@apache.org>
2008-04-02 19:53:32 +0000

    readded files

---
+++ core/src/main/java/org/apache/myfaces/tobago/renderkit/html/HtmlAttributes.java
@@ -0,0 +1,77 @@
+package org.apache.myfaces.tobago.renderkit.html;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Created by IntelliJ IDEA.
+ * User: bommel
+ * Date: Sep 23, 2006
+ * Time: 10:01:48 AM
+ */
+public final class HtmlAttributes {
+  public static final String STYLE = "style";
+  public static final String TYPE = "type";
+  public static final String NAME = "name";
+  public static final String ID = "id";
+  public static final String VALUE = "value";
+  public static final String FOR = "for";
+  public static final String TITLE = "title";
+  public static final String DISABLED = "disabled";
+  public static final String ONCLICK = "onclick";
+  public static final String SELECTED = "selected";
+  public static final String HREF = "href";
+  public static final String SRC = "src";
+  public static final String ALT = "alt";
+  public static final String WIDTH = "width";
+  public static final String BORDER = "border";
+  public static final String CHECKED = "checked";
+  public static final String SUMMARY = "summary";
+  public static final String HEIGHT = "height";
+  public static final String READONLY = "readonly";
+  public static final String ONCHANGE = "onchange";
+  public static final String CELLPADDING = "cellpadding";
+  public static final String CELLSPACING = "cellspacing";
+  public static final String ALIGN = "align";
+  public static final String VALIGN = "valign";
+  public static final String ROWSPAN = "rowspan";
+  public static final String COLSPAN = "colspan";
+  public static final String ONMOUSEOVER = "onmouseover";
+  public static final String ONMOUSEOUT = "onmouseout";
+  public static final String AUTOCOMPLETE = "autocomplete";
+  public static final String TARGET = "target";
+  public static final String SIZE = "size";
+  public static final String MULTIPLE = "multiple";
+  public static final String ROWS = "rows";
+  public static final String REL = "rel";
+  public static final String MEDIA = "media";
+  public static final String ONLOAD = "onload";
+  public static final String SCROLL = "scroll";
+  public static final String ACTION = "action";
+  public static final String METHOD = "method";
+  public static final String ENCTYPE = "enctype";
+  public static final String ACCEPT_CHARSET = "accept-charset";
+  public static final String ONFOCUS = "onfocus";
+  public static final String ONBLUR = "onblur";
+  public static final String ONKEYDOWN = "onkeydown";
+  public static final String ONKEYPRESS = "onkeypress";
+  public static final String ONKEYUP = "onkeyup";
+  public static final String CLASS = "class";
+  public static final String MAXLENGTH = "maxlength";
+  public static final String LABEL = "label";
+}
+
---
+++ core/src/main/java/org/apache/myfaces/tobago/renderkit/html/StyleClasses.java
@@ -0,0 +1,276 @@
+package org.apache.myfaces.tobago.renderkit.html;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.commons.collections.set.ListOrderedSet;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.lang.StringUtils;
+import static org.apache.myfaces.tobago.TobagoConstants.ATTR_DISABLED;
+import static org.apache.myfaces.tobago.TobagoConstants.ATTR_INLINE;
+import static org.apache.myfaces.tobago.TobagoConstants.ATTR_READONLY;
+import static org.apache.myfaces.tobago.TobagoConstants.ATTR_STYLE_CLASS;
+import org.apache.myfaces.tobago.util.ComponentUtil;
+import org.apache.myfaces.tobago.component.SupportsMarkup;
+import org.apache.myfaces.tobago.context.ClientProperties;
+import org.apache.myfaces.tobago.context.Theme;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+
+/*
+ * Date: 2007-05-01
+ */
+
+public class StyleClasses implements Serializable {
+
+  private static final long serialVersionUID = 3738052927067803517L;
+
+  private static final Log LOG = LogFactory.getLog(StyleClasses.class);
+
+  public static final char SEPERATOR = '-';
+  public static final String PREFIX = "tobago" + SEPERATOR;
+  public static final String MARKUP = SEPERATOR + "markup" + SEPERATOR;
+
+  private ListOrderedSet classes;
+
+  public StyleClasses() {
+    classes = new ListOrderedSet();
+  }
+
+  private StyleClasses(StyleClasses base) {
+    this();
+    classes.addAll(base.classes);
+  }
+
+  public static StyleClasses ensureStyleClasses(UIComponent component) {
+    Map attributes = component.getAttributes();
+    StyleClasses classes = (StyleClasses) attributes.get(ATTR_STYLE_CLASS);
+    if (classes == null) {
+      classes = new StyleClasses();
+      attributes.put(ATTR_STYLE_CLASS, classes);
+    }
+    return classes;
+  }
+
+  public static StyleClasses ensureStyleClassesCopy(UIComponent component) {
+    return new StyleClasses(ensureStyleClasses(component));
+  }
+
+  @Deprecated
+  public void addFullQualifiedClass(String clazz) {
+    classes.add(clazz);
+  }
+
+  @Deprecated
+  public void removeFullQualifiedClass(String clazz) {
+    classes.remove(clazz);
+  }
+
+  public void addClass(String renderer, String sub) {
+    classes.add(nameOfClass(renderer, sub));
+  }
+
+  public void removeClass(String renderer, String sub) {
+    classes.remove(nameOfClass(renderer, sub));
+  }
+
+  public boolean isEmpty() {
+    return classes.isEmpty();
+  }
+
+  private String nameOfClass(String renderer, String sub) {
+    StringBuilder builder = new StringBuilder();
+    builder.append(PREFIX);
+    builder.append(renderer);
+    builder.append(SEPERATOR);
+    builder.append(sub);
+    return builder.toString();
+  }
+
+  public void addMarkupClass(String renderer, String markup) {
+    addMarkupClass(renderer, null, markup);
+  }
+
+  public void removeMarkupClass(String renderer, String markup) {
+    removeMarkupClass(renderer, null, markup);
+  }
+
+  public void addMarkupClass(String renderer, String sub, String markup) {
+    classes.add(nameOfMarkupClass(renderer, sub, markup));
+  }
+
+  public void removeMarkupClass(String renderer, String sub, String markup) {
+    classes.remove(nameOfMarkupClass(renderer, sub, markup));
+  }
+
+  private String nameOfMarkupClass(String renderer, String sub, String markup) {
+    StringBuilder builder = new StringBuilder();
+    builder.append(PREFIX);
+    builder.append(renderer);
+    if (sub != null) {
+      builder.append(SEPERATOR);
+      builder.append(sub);
+    }
+    builder.append(MARKUP);
+    builder.append(markup);
+    return builder.toString();
+  }
+
+  public void addMarkupClass(UIComponent component, String rendererName) {
+    if (component instanceof SupportsMarkup) {
+      addMarkupClass((SupportsMarkup) component, rendererName, null);
+    }
+  }
+
+  public void addMarkupClass(SupportsMarkup supportsMarkup, String rendererName, String sub) {
+    for (String markup : supportsMarkup.getMarkup()) {
+      if (!StringUtils.isBlank(markup)) {
+        Theme theme = ClientProperties.getInstance(FacesContext.getCurrentInstance().getViewRoot()).getTheme();
+        if (theme.getRenderersConfig().isMarkupSupported(rendererName, markup)) {
+          addMarkupClass(rendererName, sub, markup);
+        } else if (!"none".equals(markup)) {
+          LOG.warn("Unknown markup='" + markup + "'");
+        }
+      }
+    }
+  }
+
+  public void addAspectClass(String renderer, Aspect aspect) {
+    classes.add(nameOfAspectClass(renderer, aspect));
+  }
+
+  public void removeAspectClass(String renderer, Aspect aspect) {
+    classes.remove(nameOfAspectClass(renderer, aspect));
+  }
+
+  private String nameOfAspectClass(String renderer, Aspect aspect) {
+    StringBuilder builder = new StringBuilder();
+    builder.append(PREFIX);
+    builder.append(renderer);
+    builder.append(aspect);
+    return builder.toString();
+  }
+
+
+  public void addAspectClass(String renderer, String sub, Aspect aspect) {
+    classes.add(nameOfAspectClass(renderer, sub, aspect));
+  }
+
+  public void removeAspectClass(String renderer, String sub, Aspect aspect) {
+    classes.remove(nameOfAspectClass(renderer, sub, aspect));
+  }
+
+  private String nameOfAspectClass(String renderer, String sub, Aspect aspect) {
+    StringBuilder builder = new StringBuilder();
+    builder.append(PREFIX);
+    builder.append(renderer);
+    builder.append(SEPERATOR);
+    builder.append(sub);
+    builder.append(aspect);
+    return builder.toString();
+  }
+
+  public void addClasses(StyleClasses styleClasses) {
+    for (String clazz : (Iterable<String>) styleClasses.classes) {
+      classes.add(clazz);
+    }
+  }
+
+  public void removeClass(String clazz) {
+    classes.remove(clazz);
+  }
+
+  public void removeTobagoClasses(String rendererName) {
+    for (Iterator i = classes.iterator(); i.hasNext();) {
+      String clazz = (String) i.next();
+      if (clazz.startsWith(PREFIX + rendererName)) {
+        i.remove();
+      }
+    }
+  }
+
+  public void updateClassAttributeAndMarkup(UIComponent component, String rendererName) {
+    updateClassAttribute(component, rendererName);
+    addMarkupClass(component, rendererName);
+  }
+
+  public void updateClassAttribute(UIComponent component, String rendererName) {
+    // first remove old tobago-<rendererName>-<type> classes from class-attribute
+    removeTobagoClasses(rendererName);
+
+    addAspectClass(rendererName, Aspect.DEFAULT);
+    if (ComponentUtil.getBooleanAttribute(component, ATTR_DISABLED)) {
+      addAspectClass(rendererName, Aspect.DISABLED);
+    }
+    if (ComponentUtil.getBooleanAttribute(component, ATTR_READONLY)) {
+      addAspectClass(rendererName, Aspect.READONLY);
+    }
+    if (ComponentUtil.getBooleanAttribute(component, ATTR_INLINE)) {
+      addAspectClass(rendererName, Aspect.INLINE);
+    }
+    if (component instanceof UIInput) {
+      UIInput input = (UIInput) component;
+      if (ComponentUtil.isError(input)) {
+        addAspectClass(rendererName, Aspect.ERROR);
+      }
+      if (input.isRequired()) {
+        addAspectClass(rendererName, Aspect.REQUIRED);
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+    StringBuffer buffer = new StringBuffer();
+    for (Iterator i = classes.iterator(); i.hasNext();) {
+      String clazz = (String) i.next();
+      buffer.append(clazz);
+      if (i.hasNext()) {
+        buffer.append(' ');
+      }
+    }
+    return buffer.toString();
+  }
+
+  public enum Aspect {
+
+    DEFAULT,
+    DISABLED,
+    READONLY,
+    INLINE,
+    ERROR,
+    REQUIRED;
+
+    private String aspect;
+
+    Aspect() {
+      aspect = '-' + name().toLowerCase(Locale.ENGLISH);
+    }
+
+    @Override
+    public String toString() {
+      return aspect;
+    }
+  }
+}
