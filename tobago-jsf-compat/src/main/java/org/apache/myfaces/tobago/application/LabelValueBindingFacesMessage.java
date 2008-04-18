a254a14e373de833bb909d261814a8f9179bbd58
Bernd Bohmann <bommel@apache.org>
2008-04-18 16:24:39 +0000

    (TOBAGO-650) Support for validatorMessage, requiredMessage, converterMessage and label attribute for EditableValueHolder

--- tobago-jsf-compat/src/main/java/org/apache/myfaces/tobago/application/LabelValueBindingFacesMessage.java
+++ tobago-jsf-compat/src/main/java/org/apache/myfaces/tobago/application/LabelValueBindingFacesMessage.java
@@ -14,32 +14,32 @@ public class LabelValueBindingFacesMessage extends FacesMessage {
   private Object[] args;
 
   public LabelValueBindingFacesMessage() {
-		super();
-	}
+    super();
+  }
 
-	public LabelValueBindingFacesMessage(FacesMessage.Severity severity, String summary, String detail,
+  public LabelValueBindingFacesMessage(FacesMessage.Severity severity, String summary, String detail,
       Locale locale, Object... args) {
-		super(severity, summary, detail);
+    super(severity, summary, detail);
     this.locale = locale;
     this.args = args;
   }
 
-	public LabelValueBindingFacesMessage(String summary, String detail) {
-		super(summary, detail);
-	}
+  public LabelValueBindingFacesMessage(String summary, String detail) {
+    super(summary, detail);
+  }
 
-	public LabelValueBindingFacesMessage(String summary) {
-		super(summary);
-	}
+  public LabelValueBindingFacesMessage(String summary) {
+    super(summary);
+  }
 
-	@Override
-	public String getDetail() {
+  @Override
+  public String getDetail() {
     String detail = super.getDetail();
     if (args != null && args.length > 0) {
       if (args.length == 1 && UIComponentTag.isValueReference(args[0].toString())) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         ValueBinding value = facesContext.getApplication().createValueBinding(detail);
-		    return MessageUtils.getFormatedMessage(detail, locale, value.getValue(facesContext));
+        return MessageUtils.getFormatedMessage(detail, locale, value.getValue(facesContext));
       }
       return MessageUtils.getFormatedMessage(detail, locale, args);
     }
@@ -47,13 +47,13 @@ public class LabelValueBindingFacesMessage extends FacesMessage {
   }
 
   @Override
-	public String getSummary() {
+  public String getSummary() {
     String summary = super.getSummary();
     if (args != null && args.length > 0) {
       if (args.length == 1 && UIComponentTag.isValueReference(args[0].toString())) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         ValueBinding value = facesContext.getApplication().createValueBinding(summary);
-		    return MessageUtils.getFormatedMessage(summary, locale, value.getValue(facesContext));
+        return MessageUtils.getFormatedMessage(summary, locale, value.getValue(facesContext));
       }
       return MessageUtils.getFormatedMessage(summary, locale, args);
     }
