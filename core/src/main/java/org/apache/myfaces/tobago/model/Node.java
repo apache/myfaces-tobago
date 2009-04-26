b84f4333fccc9ae2d92d95235ada06f5384d349d
Bernd Bohmann <bommel@apache.org>
2009-04-26 05:13:12 +0000

    checkstyle

--- core/src/main/java/org/apache/myfaces/tobago/model/Node.java
+++ core/src/main/java/org/apache/myfaces/tobago/model/Node.java
@@ -1,9 +1,5 @@
 package org.apache.myfaces.tobago.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -20,6 +16,11 @@ import java.util.List;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class Node {
 
