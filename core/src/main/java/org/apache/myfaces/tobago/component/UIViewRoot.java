2bd2b66099802ab66aabff9a766c46f4e52826bc
Bernd Bohmann <bommel@apache.org>
2009-10-30 13:35:19 +0000

    (TOBAGO-811) Dublicate Id Exception with RI 1.1..

--- core/src/main/java/org/apache/myfaces/tobago/component/UIViewRoot.java
+++ core/src/main/java/org/apache/myfaces/tobago/component/UIViewRoot.java
@@ -19,10 +19,18 @@ package org.apache.myfaces.tobago.component;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.myfaces.tobago.ajax.api.AjaxResponseRenderer;
+import org.apache.myfaces.tobago.ajax.api.AjaxUtils;
 import org.apache.myfaces.tobago.compat.FacesUtils;
 import org.apache.myfaces.tobago.compat.InvokeOnComponent;
 import org.apache.myfaces.tobago.context.ClientProperties;
 import org.apache.myfaces.tobago.context.ResourceManagerImpl;
+import org.apache.myfaces.tobago.context.TobagoFacesContext;
+import org.apache.myfaces.tobago.util.ComponentUtil;
+import org.apache.myfaces.tobago.util.TobagoCallback;
+import org.apache.myfaces.tobago.util.ApplyRequestValuesCallback;
+import org.apache.myfaces.tobago.util.ProcessValidationsCallback;
+import org.apache.myfaces.tobago.util.UpdateModelValuesCallback;
 import org.apache.myfaces.tobago.util.FacesVersion;
 
 import javax.faces.FacesException;
@@ -33,11 +41,13 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.FacesEvent;
 import javax.faces.event.PhaseId;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Locale;
+import java.util.Map;
 
 /*
  * User: weber
@@ -51,7 +61,9 @@ public class UIViewRoot extends javax.faces.component.UIViewRoot implements Invo
   private ResourceManagerImpl.CacheKey rendererCacheKey;
 
   public static final int ANY_PHASE_ORDINAL = PhaseId.ANY_PHASE.getOrdinal();
-
+  private static final TobagoCallback APPLY_REQUEST_VALUES_CALLBACK = new ApplyRequestValuesCallback();
+  private static final ContextCallback PROCESS_VALIDATION_CALLBACK = new ProcessValidationsCallback();
+  private static final ContextCallback UPDATE_MODEL_VALUES_CALLBACK = new UpdateModelValuesCallback();
   private List events = null;
 
   private int nextUniqueId;
@@ -99,7 +111,7 @@ public class UIViewRoot extends javax.faces.component.UIViewRoot implements Invo
     Object[] state = (Object[]) o;
     super.restoreState(facesContext, state[0]);
     rendererCacheKey = (ResourceManagerImpl.CacheKey) state[1];
-    if (FacesVersion.supports12()) {
+    if (!FacesVersion.supports12()) {
       nextUniqueId = (Integer) state[2];
     }
   }
@@ -208,18 +220,74 @@ public class UIViewRoot extends javax.faces.component.UIViewRoot implements Invo
     if (context == null) {
       throw new NullPointerException("context");
     }
-    super.processDecodes(context);
+     Map<String, UIComponent> ajaxComponents = AjaxUtils.parseAndStoreComponents(context);
+    if (ajaxComponents != null) {
+      // first decode the page
+      AbstractUIPage page = ComponentUtil.findPage(context);
+      page.decode(context);
+      page.markSubmittedForm(context);
+      if (context instanceof TobagoFacesContext) {
+        ((TobagoFacesContext) context).setAjax(true);
+      }
+      // decode the action if actionComponent not inside one of the ajaxComponents
+      // otherwise it is decoded there
+      decodeActionComponent(context, page, ajaxComponents);
+
+      // and all ajax components
+      for (Map.Entry<String, UIComponent> entry : ajaxComponents.entrySet()) {
+        if (context instanceof TobagoFacesContext) {
+          ((TobagoFacesContext) context).setAjaxComponentId(entry.getKey());
+        }
+        invokeOnComponent(context, entry.getKey(), APPLY_REQUEST_VALUES_CALLBACK);
+      }
+    } else {
+      super.processDecodes(context);
+    }
     broadcastForPhase(PhaseId.APPLY_REQUEST_VALUES);
     if (context.getRenderResponse() || context.getResponseComplete()) {
       clearEvents();
     }
   }
 
+  private void decodeActionComponent(FacesContext facesContext, AbstractUIPage page, Map<String,
+      UIComponent> ajaxComponents) {
+    String actionId = page.getActionId();
+    UIComponent actionComponent = null;
+    if (actionId != null) {
+      actionComponent = findComponent(actionId);
+    }
+    if (actionComponent == null) {
+      return;
+    }
+    for (UIComponent ajaxComponent : ajaxComponents.values()) {
+      UIComponent component = actionComponent;
+      while (component != null) {
+        if (component == ajaxComponent) {
+          return;
+        }
+        component = component.getParent();
+      }
+    }
+    invokeOnComponent(facesContext, actionId, APPLY_REQUEST_VALUES_CALLBACK);
+  }
+
+
   public void processValidators(FacesContext context) {
     if (context == null) {
       throw new NullPointerException("context");
     }
-    super.processValidators(context);
+
+    Map<String, UIComponent> ajaxComponents = AjaxUtils.getAjaxComponents(context);
+    if (ajaxComponents != null) {
+      for (Map.Entry<String, UIComponent> entry : ajaxComponents.entrySet()) {
+        if (context instanceof TobagoFacesContext) {
+          ((TobagoFacesContext) context).setAjaxComponentId(entry.getKey());
+        }
+        invokeOnComponent(context, entry.getKey(), PROCESS_VALIDATION_CALLBACK);
+      }
+    } else {
+      super.processValidators(context);
+    }
     broadcastForPhase(PhaseId.PROCESS_VALIDATIONS);
     if (context.getRenderResponse() || context.getResponseComplete()) {
       clearEvents();
@@ -230,7 +298,14 @@ public class UIViewRoot extends javax.faces.component.UIViewRoot implements Invo
     if (context == null) {
       throw new NullPointerException("context");
     }
-    super.processUpdates(context);
+     Map<String, UIComponent> ajaxComponents = AjaxUtils.getAjaxComponents(context);
+    if (ajaxComponents != null) {
+      for (Map.Entry<String, UIComponent> entry : ajaxComponents.entrySet()) {
+        invokeOnComponent(context, entry.getKey(), UPDATE_MODEL_VALUES_CALLBACK);
+      }
+    } else {
+      super.processUpdates(context);
+    }
     broadcastForPhase(PhaseId.UPDATE_MODEL_VALUES);
     if (context.getRenderResponse() || context.getResponseComplete()) {
       clearEvents();
@@ -244,6 +319,25 @@ public class UIViewRoot extends javax.faces.component.UIViewRoot implements Invo
     broadcastForPhase(PhaseId.INVOKE_APPLICATION);
     if (context.getRenderResponse() || context.getResponseComplete()) {
       clearEvents();
+    }
+  }
+
+  @Override
+  public boolean getRendersChildren() {
+    if (AjaxUtils.isAjaxRequest(FacesContext.getCurrentInstance())) {
+      return true;
+    } else {
+      return super.getRendersChildren();
+    }
+  }
+
+  @Override
+  public void encodeChildren(FacesContext context) throws IOException {
+    if (AjaxUtils.isAjaxRequest(context)) {
+      new AjaxResponseRenderer().renderResponse(context);
+
+    } else {
+      super.encodeChildren(context);
     }
   }
 
