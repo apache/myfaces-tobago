7600cd70b21e24e03213a8efe2bcaa0e63ca86ed
Udo Schnurpfeil <lofwyr@apache.org>
2009-11-10 09:13:06 +0000

    TOBAGO-821: SelectOneListbox with new layout  - also fixing error style

--- theme/scarborough/src/main/java/org/apache/myfaces/tobago/renderkit/html/scarborough/standard/tag/SelectOneListboxRenderer.java
+++ theme/scarborough/src/main/java/org/apache/myfaces/tobago/renderkit/html/scarborough/standard/tag/SelectOneListboxRenderer.java
@@ -51,42 +51,43 @@ public class SelectOneListboxRenderer extends SelectOneRendererBase {
 
   public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
     if (!(component instanceof UISelectOneListbox)) {
-      LOG.error("Wrong type: Need " + UISelectOneListbox.class.getName() + ", but was "
-          + component.getClass().getName());
+      LOG.error("Wrong type: Need " + UISelectOneListbox.class.getName()
+          + ", but was " + component.getClass().getName());
       return;
     }
 
+    UISelectOneListbox select = (UISelectOneListbox) component;
     TobagoResponseWriter writer = HtmlRendererUtils.getTobagoResponseWriter(facesContext);
-    UISelectOneListbox selectOne = (UISelectOneListbox) component;
-    List<SelectItem> items = RenderUtil.getSelectItems(selectOne);
 
-    writer.startElement(HtmlConstants.SELECT, selectOne);
-    String clientId = selectOne.getClientId(facesContext);
-    writer.writeNameAttribute(clientId);
-    writer.writeIdAttribute(clientId);
-    writer.writeAttribute(HtmlAttributes.DISABLED, ComponentUtils.getBooleanAttribute(selectOne, Attributes.DISABLED));
-    Integer tabIndex = selectOne.getTabIndex();
+    String id = select.getClientId(facesContext);
+    List<SelectItem> items = RenderUtil.getSelectItems(select);
+
+    writer.startElement(HtmlConstants.SELECT, select);
+    writer.writeNameAttribute(id);
+    writer.writeIdAttribute(id);
+    writer.writeAttribute(HtmlAttributes.DISABLED, ComponentUtils.getBooleanAttribute(select, Attributes.DISABLED));
+    Integer tabIndex = select.getTabIndex();
     if (tabIndex != null) {
       writer.writeAttribute(HtmlAttributes.TABINDEX, tabIndex);
     }
-    Style style = new Style(facesContext, selectOne);
+    Style style = new Style(facesContext, select);
     writer.writeStyleAttribute(style);
     writer.writeClassAttribute();
-    HtmlRendererUtils.renderTip(selectOne, writer);
+    HtmlRendererUtils.renderTip(select, writer);
     writer.writeAttribute(HtmlAttributes.SIZE, 2); // should be greater 1
-    if (!ComponentUtils.getBooleanAttribute(selectOne, Attributes.REQUIRED)) {
+    if (!ComponentUtils.getBooleanAttribute(select, Attributes.REQUIRED)) {
       writer.writeAttribute(HtmlAttributes.ONCHANGE, "Tobago.selectOneListboxChange(this)", false);
       writer.writeAttribute(HtmlAttributes.ONCLICK, "Tobago.selectOneListboxClick(this)", false);
     }
 
-    Object[] values = {selectOne.getValue()};
+    Object[] values = {select.getValue()};
 
-    HtmlRendererUtils.renderSelectItems(selectOne, items, values, writer, facesContext);
+    HtmlRendererUtils.renderSelectItems(select, items, values, writer, facesContext);
 
     writer.endElement(HtmlConstants.SELECT);
-    super.encodeEnd(facesContext, selectOne);
-    HtmlRendererUtils.renderFocusId(facesContext, selectOne);
-    HtmlRendererUtils.checkForCommandFacet(selectOne, facesContext, writer);
+    super.encodeEnd(facesContext, select);
+    HtmlRendererUtils.renderFocusId(facesContext, select);
+    HtmlRendererUtils.checkForCommandFacet(select, facesContext, writer);
   }
 
 
