b01c7b56280e06a1dcbc23ef4666db5d5213b9ea
Udo Schnurpfeil <lofwyr@apache.org>
2009-11-17 09:50:27 +0000

    TOBAGO-606: Simplify box rendering, easy way to have boxes without shadow

--- theme/speyside/src/main/java/org/apache/myfaces/tobago/renderkit/html/speyside/standard/tag/BoxRenderer.java
+++ theme/speyside/src/main/java/org/apache/myfaces/tobago/renderkit/html/speyside/standard/tag/BoxRenderer.java
@@ -19,13 +19,15 @@ package org.apache.myfaces.tobago.renderkit.html.speyside.standard.tag;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import static org.apache.myfaces.tobago.TobagoConstants.SUBCOMPONENT_SEP;
 import org.apache.myfaces.tobago.ajax.api.AjaxRenderer;
 import org.apache.myfaces.tobago.ajax.api.AjaxUtils;
 import org.apache.myfaces.tobago.component.Attributes;
 import org.apache.myfaces.tobago.component.Facets;
 import org.apache.myfaces.tobago.component.UIBox;
 import org.apache.myfaces.tobago.component.UIToolBar;
+import org.apache.myfaces.tobago.config.ThemeConfig;
+import org.apache.myfaces.tobago.layout.Measure;
+import org.apache.myfaces.tobago.layout.PixelMeasure;
 import org.apache.myfaces.tobago.renderkit.BoxRendererBase;
 import org.apache.myfaces.tobago.renderkit.css.Style;
 import org.apache.myfaces.tobago.renderkit.html.HtmlConstants;
@@ -40,6 +42,8 @@ import javax.faces.context.FacesContext;
 import java.io.IOException;
 import java.util.Map;
 
+import static org.apache.myfaces.tobago.TobagoConstants.SUBCOMPONENT_SEP;
+
 public class BoxRenderer extends BoxRendererBase implements AjaxRenderer {
 
   private static final Log LOG = LogFactory.getLog(BoxRenderer.class);
@@ -53,17 +57,29 @@ public class BoxRenderer extends BoxRendererBase implements AjaxRenderer {
   }
 
   /*
+  
+with shadow
+  
 <div class="tobago-box-default" style="width: 100px; height: 100px">
   <div class="tobago-box-shadow" style="width: 99px; height: 99px">
     <div class="tobago-box-border" style="width: 97px; height: 97px">
-      <div class="tobago-box-header">
-        <span class="tobago-box-header-label">Label</span>
-      </div>
+      <div class="tobago-box-header">Label</div>
     </div>
   </div>
-            <div style="position: absolute; top: 26px; left: 6px; width: 87px; height: 67px; background-color: blue;">
-            Content
-            </div>
+
+  <div style="position: absolute; top: 26px; left: 6px; width: 87px; height: 67px; background-color: blue;">
+    Content
+  </div>
+</div>
+
+without shadow
+
+<div class="tobago-box-default" style="width: 100px; height: 100px">
+  <span class="tobago-box-header">Label</span>
+
+  <div style="position: absolute; top: 26px; left: 6px; width: 87px; height: 67px; background-color: blue;">
+    Content
+  </div>
 </div>
 
    */
@@ -85,65 +101,58 @@ public class BoxRenderer extends BoxRendererBase implements AjaxRenderer {
 
   private void encodeBox(FacesContext facesContext, TobagoResponseWriter writer, UIBox box) throws IOException {
 
-    // shadow begin
-    writer.startElement(HtmlConstants.DIV, box);
+    // todo: shadow = 0px means, that shadow is disabled, but it may be better, if we can set a boolean in the config.
+    Measure measure = ThemeConfig.getMeasure(facesContext, box.getRendererType(), "shadow");
+    boolean hasShadow = measure.greaterThan(PixelMeasure.ZERO);
 
-    StyleClasses classes = new StyleClasses();
-    classes.addClass("box", "shadow");
-    writer.writeClassAttribute(classes);
+    if (hasShadow) {
+      // shadow begin
+      writer.startElement(HtmlConstants.DIV, box);
 
-    Style shadow = new Style();
-    shadow.setWidth(box.getWidth().subtract(1));
-    shadow.setHeight(box.getHeight().subtract(1));
-    writer.writeStyleAttribute(shadow);
+      StyleClasses classes = new StyleClasses();
+      classes.addClass("box", "shadow");
+      writer.writeClassAttribute(classes);
 
-    // border begin
-    writer.startElement(HtmlConstants.DIV, box);
+      Style shadow = new Style();
+      shadow.setWidth(box.getWidth().subtract(1));
+      shadow.setHeight(box.getHeight().subtract(1));
+      writer.writeStyleAttribute(shadow);
 
-    classes = new StyleClasses();
-    classes.addClass("box", "border");
-    writer.writeClassAttribute(classes);
+      // border begin
+      writer.startElement(HtmlConstants.DIV, box);
 
-    Style border = new Style();
-    border.setWidth(box.getWidth().subtract(3));
-    border.setHeight(box.getHeight().subtract(3));
-    writer.writeStyleAttribute(border);
+      classes = new StyleClasses();
+      classes.addClass("box", "border");
+      writer.writeClassAttribute(classes);
 
-    renderBoxHeader(facesContext, writer, box);
+      Style border = new Style();
+      border.setWidth(box.getWidth().subtract(3));
+      border.setHeight(box.getHeight().subtract(3));
+      writer.writeStyleAttribute(border);
+    }
 
-    // border end
-    writer.endElement(HtmlConstants.DIV);
-    // shadow end
-    writer.endElement(HtmlConstants.DIV);
-  }
-
-  protected void renderBoxHeader(
-      FacesContext facesContext, TobagoResponseWriter writer, UIBox box) throws IOException {
-
-    writer.startElement(HtmlConstants.DIV, box);
-    StyleClasses headerClasses = new StyleClasses();
-    headerClasses.addClass("box", "header");
-    // XXX should this be done everywhere?
-    headerClasses.addMarkupClass(box, "box", "header");
-    writer.writeClassAttribute(headerClasses);
-    String id = box.getClientId(facesContext) + HEADER;
-    writer.writeIdAttribute(id);
     UIComponent label = box.getFacet(Facets.LABEL);
-    writer.startElement(HtmlConstants.SPAN, null);
-    writer.writeClassAttribute("tobago-box-header-label");
+    writer.startElement(HtmlConstants.DIV, null);
+    writer.writeClassAttribute("tobago-box-header");
     String labelString = (String) box.getAttributes().get(Attributes.LABEL);
     if (label != null) {
       RenderUtil.encode(facesContext, label);
     } else if (labelString != null) {
       writer.writeText(labelString);
     }
-    writer.endElement(HtmlConstants.SPAN);
+    writer.endElement(HtmlConstants.DIV);
 
     UIPanel toolbar = (UIPanel) box.getFacet(Facets.TOOL_BAR);
     if (toolbar != null) {
       renderToolbar(facesContext, writer, toolbar);
     }
-    writer.endElement(HtmlConstants.DIV);
+    
+    if (hasShadow) {
+      // border end
+      writer.endElement(HtmlConstants.DIV);
+      // shadow end
+      writer.endElement(HtmlConstants.DIV);
+    }
   }
 
   @Override
