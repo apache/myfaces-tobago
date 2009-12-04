fd5b45978409b910218a13ee794e9715a013135c
Bernd Bohmann <bommel@apache.org>
2009-12-04 15:53:25 +0000

    TOBAGO-834) PopupActionListener has wrong popup reference if Component tree is rebuild before render view

--- core/src/main/java/org/apache/myfaces/tobago/event/PopupActionListener.java
+++ core/src/main/java/org/apache/myfaces/tobago/event/PopupActionListener.java
@@ -17,11 +17,13 @@ package org.apache.myfaces.tobago.event;
  * limitations under the License.
  */
 
+import org.apache.myfaces.tobago.TobagoConstants;
 import org.apache.myfaces.tobago.component.UIPopup;
 import org.apache.myfaces.tobago.component.ComponentUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import javax.faces.component.UIComponent;
 import javax.faces.event.ActionListener;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.AbortProcessingException;
@@ -34,6 +36,7 @@ import java.io.Serializable;
  * Date: Dec 23, 2006
  * Time: 10:59:53 AM
  */
+// TODO implement StateHolder
 public class PopupActionListener implements ActionListener, Serializable {
 
   private static final Log LOG = LogFactory.getLog(PopupActionListener.class);
@@ -41,6 +44,9 @@ public class PopupActionListener implements ActionListener, Serializable {
   private String popupId;
 
   private ValueBinding popupIdBinding;
+
+  private boolean facet;
+
 
   public PopupActionListener() {
   }
@@ -58,34 +64,43 @@ public class PopupActionListener implements ActionListener, Serializable {
   }
 
   public PopupActionListener(UIPopup popup) {
-    this.popupId = ":" + popup.getClientId(FacesContext.getCurrentInstance());
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Add ActionListener: " + popupId);
-    }
+    facet = true;
   }
 
   public void processAction(ActionEvent actionEvent) throws AbortProcessingException {
     FacesContext facesContext = FacesContext.getCurrentInstance();
-    String id = null;
-    if (popupIdBinding != null) {
-      id = (String) popupIdBinding.getValue(facesContext);
+    UIPopup popup = null;
+    if (facet) {
+      UIComponent component = actionEvent.getComponent().getFacet(TobagoConstants.FACET_POPUP);
+      if (component instanceof UIPopup) {
+        popup = (UIPopup) component;
+      }
+      if (popup == null) {
+        LOG.error("Found no popup facet in component "
+              + actionEvent.getComponent().getClientId(facesContext));
+      }
     } else {
-      id = popupId;
+      String id;
+      if (popupIdBinding != null) {
+        id = (String) popupIdBinding.getValue(facesContext);
+      } else {
+        id = popupId;
+      }
+      popup = (UIPopup) ComponentUtil.findComponent(actionEvent.getComponent(), id);
+      if (popup == null) {
+        LOG.error("Found no popup for \""
+          + (popupIdBinding != null ? popupIdBinding.getExpressionString() + "\" := \"" : "")
+          + id + "\"! Search base componentId : "
+              + actionEvent.getComponent().getClientId(facesContext));
+      }
     }
-    UIPopup popup = (UIPopup) ComponentUtil.findComponent(actionEvent.getComponent(), id);
-
     if (popup != null) {
       if (LOG.isDebugEnabled()) {
         LOG.debug("activated "
             + actionEvent.getComponent().getClientId(facesContext));
       }
       popup.setActivated(true);
-    } else {
-      LOG.error("Found no popup for \""
-          + (popupIdBinding != null ? popupIdBinding.getExpressionString() + "\" := \"" : "")
-          + id + "\"! Search base componentId : " 
-              + actionEvent.getComponent().getClientId(facesContext));
-    }
+    } 
   }
 
 }
