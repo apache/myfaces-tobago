613f7f29a34bc80eefe3fa8c5ab04164b711a9ed
Bernd Bohmann <bommel@apache.org>
2009-12-07 17:23:20 +0000

    checkstyle

--- core/src/main/java/org/apache/myfaces/tobago/config/ThemeConfig.java
+++ core/src/main/java/org/apache/myfaces/tobago/config/ThemeConfig.java
@@ -115,8 +115,12 @@ public class ThemeConfig {
 
     @Override
     public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
 
       CacheKey cacheKey = (CacheKey) o;
 
