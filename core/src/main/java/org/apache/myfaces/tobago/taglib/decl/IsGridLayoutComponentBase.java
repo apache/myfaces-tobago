6cc3a364921363ef2b2bae93846f8f90db1c1878
Bernd Bohmann <bommel@apache.org>
2009-12-07 17:35:05 +0000

    checkstyle

--- core/src/main/java/org/apache/myfaces/tobago/taglib/decl/IsGridLayoutComponentBase.java
+++ core/src/main/java/org/apache/myfaces/tobago/taglib/decl/IsGridLayoutComponentBase.java
@@ -1,7 +1,5 @@
 package org.apache.myfaces.tobago.taglib.decl;
 
-import org.apache.myfaces.tobago.apt.annotation.UIComponentTagAttribute;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -18,6 +16,8 @@ import org.apache.myfaces.tobago.apt.annotation.UIComponentTagAttribute;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import org.apache.myfaces.tobago.apt.annotation.UIComponentTagAttribute;
 
 /**
  * @deprecated This class should not be used directly, please use IsGridLayoutComponent. 
