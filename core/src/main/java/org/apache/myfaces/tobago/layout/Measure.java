4c45cc94963fc4f61e9e11d0213073e77de11e63
Udo Schnurpfeil <lofwyr@apache.org>
2009-12-16 09:01:08 +0000

    TOBAGO-815: make Measure.parse() more flexible

--- core/src/main/java/org/apache/myfaces/tobago/layout/Measure.java
+++ core/src/main/java/org/apache/myfaces/tobago/layout/Measure.java
@@ -31,9 +31,20 @@ public abstract class Measure implements Serializable {
 
   // todo: refactor and consolidate with LayoutToken
 
-  public static Measure parse(String value) {
+  public static Measure parse(Object object) {
+    if (object == null) {
+      return PixelMeasure.ZERO; // fixme: may return a "default measure", or is Pixel the default?
+    }
+    if (object instanceof Measure) {
+      return (Measure) object;
+    }
+    if (object instanceof Number) {
+      return new PixelMeasure(((Number) object).intValue());
+    }
+    String value = object instanceof String ? (String) object : object.toString();
+    
     if (StringUtils.isEmpty(value)) {
-      return new PixelMeasure(0); // fixme: may return a "default measure", or is Pixel the default?
+      return PixelMeasure.ZERO; // fixme: may return a "default measure", or is Pixel the default?
     }
     if (value.toLowerCase().matches("\\d+px")) {// XXX no regexp here: user LayoutTokens.parse !!!
       return new PixelMeasure(Integer.parseInt(value.substring(0, value.length() - 2)));
