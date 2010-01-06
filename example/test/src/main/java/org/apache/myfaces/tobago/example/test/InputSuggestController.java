caf0bfe98669d4321f49a43a27324491c9dce171
Udo Schnurpfeil <lofwyr@apache.org>
2010-01-06 13:22:26 +0000

    TOBAGO-843: Change method signature

--- example/test/src/main/java/org/apache/myfaces/tobago/example/test/InputSuggestController.java
+++ example/test/src/main/java/org/apache/myfaces/tobago/example/test/InputSuggestController.java
@@ -3,6 +3,7 @@ package org.apache.myfaces.tobago.example.test;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.myfaces.tobago.component.UIInput;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +13,9 @@ public class InputSuggestController {
 
   private static final Log LOG = LogFactory.getLog(InputSuggestController.class);
 
-  public List<String> getInputSuggestItems(String prefix) {
-    LOG.info("createing items for prefix :\"" + prefix + "\"");
+  public List<String> getInputSuggestItems(UIInput component) {
+    String prefix = (String) component.getSubmittedValue();
+    LOG.info("Creating items for prefix :\"" + prefix + "\"");
     List<String> li = new ArrayList<String>();
     li.add(prefix + 1);
     li.add(prefix + 2);
