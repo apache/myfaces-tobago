e9c542485b3ed4f2f006959283cda4909ae519fa
Bernd Bohmann <bommel@apache.org>
2010-03-29 14:54:53 +0000

    (TOBAGO-866) Add some browers and a unit test

--- core/src/test/java/org/apache/myfaces/tobago/context/UserAgentUnitTest.java
+++ core/src/test/java/org/apache/myfaces/tobago/context/UserAgentUnitTest.java
@@ -34,18 +34,19 @@ public class UserAgentUnitTest {
   private static final String OPERA_10_10_MACOSX
       = "User-Agent: Opera/9.80 (Macintosh; Intel Mac OS X; U; de) Presto/2.2.15 Version/10.10";
 
+  private static final String OPERA_10_51_UBUNTU_9_10
+      = "Opera/9.80 (X11; Linux i686; U; en) Presto/2.5.22 Version/10.51";
 
-  private static final String FIREFOX_3_6_UBUNTU_9_10 =
-      "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.3pre) Gecko/20100328 Ubuntu/9.10 (karmic) Namoroka/3.6.3pre";
+  private static final String FIREFOX_3_6_UBUNTU_9_10
+      = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.3pre) Gecko/20100328 Ubuntu/9.10 (karmic) Namoroka/3.6.3pre";
 
-  private static final String CHROMIUM_5_0_365_UBUNTU_9_10 =
-      "Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/533.3 (KHTML, like Gecko) Chrome/5.0.365.0 Safari/533.3";
+  private static final String CHROMIUM_5_0_365_UBUNTU_9_10
+      = "Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/533.3 (KHTML, like Gecko) Chrome/5.0.365.0 Safari/533.3";
 
-  private static final String CHROME_5_0_360_UBUNTO_9_10 =
-      "Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/533.3 (KHTML, like Gecko) Chrome/5.0.360.0 Safari/533.3";
+  private static final String CHROME_5_0_360_UBUNTO_9_10
+      = "Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/533.3 (KHTML, like Gecko) Chrome/5.0.360.0 Safari/533.3";
 
-  private static final String OPERA_10_51_UBUNTU_9_10 =
-      "Opera/9.80 (X11; Linux i686; U; en) Presto/2.5.22 Version/10.51";
+
 
   @Test
   public void test() {
@@ -55,5 +56,13 @@ public class UserAgentUnitTest {
     Assert.assertEquals(UserAgent.GECKO, UserAgent.getInstance(FIREFOX_3_6_UBUNTU_10_4));
 
     Assert.assertEquals(UserAgent.OPERA, UserAgent.getInstance(OPERA_10_10_MACOSX));
+
+    Assert.assertEquals(UserAgent.OPERA, UserAgent.getInstance(OPERA_10_51_UBUNTU_9_10));
+
+    Assert.assertEquals(UserAgent.WEBKIT, UserAgent.getInstance(CHROME_5_0_360_UBUNTO_9_10));
+
+    Assert.assertEquals(UserAgent.WEBKIT, UserAgent.getInstance(CHROMIUM_5_0_365_UBUNTU_9_10));
+    
+    Assert.assertEquals(UserAgent.WEBKIT, UserAgent.getInstance(SAFARI_4_MACOSX));
   }
 }
