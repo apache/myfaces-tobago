52da51f250101e09325248ae2da316f797eb2b21
Udo Schnurpfeil <lofwyr@apache.org>
2010-06-21 07:10:53 +0000

    tree sample

---
+++ example/test/src/main/java/org/apache/myfaces/tobago/example/test/TreeController.java
@@ -0,0 +1,46 @@
+package org.apache.myfaces.tobago.example.test;
+
+import org.apache.myfaces.tobago.example.data.CategoryTree;
+import org.apache.myfaces.tobago.model.TreeState;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class TreeController {
+
+  private DefaultMutableTreeNode tree;
+
+  private TreeState state;
+
+  public TreeController() {
+    this.tree = CategoryTree.createSample();
+  }
+
+  public DefaultMutableTreeNode getTree() {
+    return tree;
+  }
+
+  public TreeState getState() {
+    return state;
+  }
+
+  public void setState(TreeState state) {
+    this.state = state;
+  }
+}
