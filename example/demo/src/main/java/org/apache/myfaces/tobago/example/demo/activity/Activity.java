b0307b0fcd0d780d5d6aca1dd1a5e5d0faaa22b2
Udo Schnurpfeil <lofwyr@apache.org>
2010-10-01 08:35:25 +0000

    checkstyle

--- example/demo/src/main/java/org/apache/myfaces/tobago/example/demo/activity/Activity.java
+++ example/demo/src/main/java/org/apache/myfaces/tobago/example/demo/activity/Activity.java
@@ -1,15 +1,25 @@
 package org.apache.myfaces.tobago.example.demo.activity;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import javax.servlet.http.HttpSession;
 import java.util.Date;
 
-/**
- * Created by IntelliJ IDEA.
- * User: lofwyr
- * Date: 30.09.2010
- * Time: 16:47:47
- * To change this template use File | Settings | File Templates.
- */
 public class Activity {
 
   private String sessionId;
