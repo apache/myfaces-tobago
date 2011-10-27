e628d3a40c1805b99c2ce1aecea4ccbc626f7b1f
Udo Schnurpfeil <lofwyr@apache.org>
2011-10-27 13:56:12 +0000

    prevent potential NullPointerException

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIGridLayout.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIGridLayout.java
@@ -350,7 +350,7 @@ public abstract class AbstractUIGridLayout extends AbstractUILayoutBase implemen
           position = position.add(LayoutUtils.getPaddingBegin(orientation, getLayoutContainer()));
           position = position.add(getMarginBegin(orientation));
           for (int k = 0; k < i; k++) {
-            if (heads[k] == null) {
+            if (heads[k] == null || heads[k].getCurrent() == null) {
               LOG.warn("Head is null, should be debugged... i=" + i + " k=" + k + " grid=\n" + grid,
                   new NullPointerException());
             } else {
