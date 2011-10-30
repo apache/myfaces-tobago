66bedc55fa813b1b2256c8e5d4d49c9bb295baf8
Bernd Bohmann <bommel@apache.org>
2011-10-30 09:56:22 +0000

    checkstyle

--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/SynchronizeNavigationPhaseListener.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/SynchronizeNavigationPhaseListener.java
@@ -34,7 +34,8 @@ public class SynchronizeNavigationPhaseListener implements PhaseListener {
     // synchronizing direct links with controller
     FacesContext facesContext = FacesContext.getCurrentInstance();
     UIViewRoot viewRoot = facesContext.getViewRoot();
-    if (viewRoot != null && viewRoot.getChildCount() == 0) { // in case of direct links the ViewRoot is empty after "restore view".
+    // in case of direct links the ViewRoot is empty after "restore view".
+    if (viewRoot != null && viewRoot.getChildCount() == 0) {
       String viewId = viewRoot.getViewId();
       Navigation navigation = (Navigation) VariableResolverUtils.resolveVariable(facesContext, "navigation");
       navigation.selectByViewId(viewId);
