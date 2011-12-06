d7aceeeeffec3ae531ae53e3653923fa65ae61b5
Udo Schnurpfeil <lofwyr@apache.org>
2011-12-06 09:54:28 +0000

    TOBAGO-1067: Ajax doesn't work, when there is a TAB in the content

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/util/HtmlWriterUtils.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/util/HtmlWriterUtils.java
@@ -38,6 +38,7 @@ public final class HtmlWriterUtils {
     CHARS_TO_ESCAPE['>'] = "&gt;".toCharArray();
     CHARS_TO_ESCAPE['\n'] = "&#13;".toCharArray();
     CHARS_TO_ESCAPE['\r'] = "&#10;".toCharArray();
+    CHARS_TO_ESCAPE['\t'] = "&#09;".toCharArray();
   }
 
   private final Writer out;
