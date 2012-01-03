333c5df1279f1466c3f85431b5d715f5b85c396f
Udo Schnurpfeil <lofwyr@apache.org>
2012-01-03 16:57:01 +0000

    TOBAGO-1049: Tree inside of the Sheet (aka TreeTable) - treeSelect

--- tobago-theme/tobago-theme-scarborough/src/main/java/org/apache/myfaces/tobago/renderkit/html/scarborough/standard/tag/TreeSelectRenderer.java
+++ tobago-theme/tobago-theme-scarborough/src/main/java/org/apache/myfaces/tobago/renderkit/html/scarborough/standard/tag/TreeSelectRenderer.java
@@ -43,6 +43,9 @@ public class TreeSelectRenderer extends SelectBooleanCheckboxRenderer {
 
     TobagoResponseWriter writer = HtmlRendererUtils.getTobagoResponseWriter(facesContext);
 
+    writer.startElement(HtmlElements.SPAN, null);
+    writer.writeClassAttribute(Classes.create(select));
+
     writer.startElement(HtmlElements.INPUT, null);
     writer.writeAttribute(HtmlAttributes.TYPE, HtmlInputTypes.CHECKBOX, false);
     writer.writeAttribute(HtmlAttributes.VALUE, "true", false);
@@ -61,6 +64,8 @@ public class TreeSelectRenderer extends SelectBooleanCheckboxRenderer {
       writer.writeText(label);
       writer.endElement(HtmlElements.LABEL);
     }
+
+    writer.endElement(HtmlElements.SPAN);
   }
 
   @Override
