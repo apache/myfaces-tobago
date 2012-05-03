39a6a041e9d46c4d81dce827357b6365f130e79c
Udo Schnurpfeil <lofwyr@apache.org>
2012-05-03 08:58:01 +0000

    TOBAGO-1112: Rerendering of a view with date picker after a locale switch doesn't update popup title and button labels

--- tobago-theme/tobago-theme-scarborough/src/main/java/org/apache/myfaces/tobago/renderkit/html/scarborough/standard/tag/DatePickerRenderer.java
+++ tobago-theme/tobago-theme-scarborough/src/main/java/org/apache/myfaces/tobago/renderkit/html/scarborough/standard/tag/DatePickerRenderer.java
@@ -140,8 +140,7 @@ public class DatePickerRenderer extends LinkRenderer {
     final UIButton cancelButton = (UIButton) CreateComponentUtils.createComponent(
         facesContext, UIButton.COMPONENT_TYPE, RendererTypes.BUTTON, "cancel");
     buttonPanel.getChildren().add(cancelButton);
-    FacesUtils.setBindingOrExpression(okButton, Attributes.LABEL, "#{tobagoContext.resourceBundle.datePickerCancel}");
-    cancelButton.setOnclick("writeIntoField2(this);");
+    FacesUtils.setBindingOrExpression(cancelButton, Attributes.LABEL, "#{tobagoContext.resourceBundle.datePickerCancel}");
     cancelButton.getAttributes().put(Attributes.POPUP_CLOSE, "immediate");
 
     buttonPanel.onComponentPopulated(facesContext, parent);
