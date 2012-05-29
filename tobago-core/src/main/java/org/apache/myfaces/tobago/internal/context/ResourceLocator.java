39420502e6c1e013d1a11991b123b7f35ad93957
Udo Schnurpfeil <lofwyr@apache.org>
2012-05-29 15:53:14 +0000

    Merged from trunk checkstyle [from revision 1343791]

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/context/ResourceLocator.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/context/ResourceLocator.java
@@ -148,7 +148,7 @@ class ResourceLocator {
         if (themeUrl.toString().endsWith(META_INF_TOBAGO_CONFIG_XML)) {
           TobagoConfigFragment tobagoConfig = new TobagoConfigParser().parse(themeUrl);
           for (ThemeImpl theme : tobagoConfig.getThemeDefinitions()) {
-            addThemeResources( resources, themeUrl, theme);
+            addThemeResources(resources, themeUrl, theme);
           }
         } else {
           // the old way
