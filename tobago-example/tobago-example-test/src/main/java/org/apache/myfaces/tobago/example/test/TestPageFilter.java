970c0f5d9ce8c2be0cbb1c22e2d8eae263ea41f6
Udo Schnurpfeil <lofwyr@apache.org>
2012-06-27 10:01:10 +0000

    activating tabGroup layout test

--- tobago-example/tobago-example-test/src/main/java/org/apache/myfaces/tobago/example/test/TestPageFilter.java
+++ tobago-example/tobago-example-test/src/main/java/org/apache/myfaces/tobago/example/test/TestPageFilter.java
@@ -74,7 +74,6 @@ public class TestPageFilter {
       "/tc/gridLayout/rendered-combinations.jspx", // uses facelets templating
       "/tc/gridLayout/scrolling-2-levels.*", // todo: ?
       "/tc/gridLayout/scrolling-tab.*", // todo: measurement problem with firefox?
-      "/tc/gridLayout/tabGroup-.*", // todo: measurement problem with firefox?
       "/tc/gridLayout/transparent-for-layout.jspx", // uses facelets templating
       "/tc/mediator/tomahawk-inputHtml.*", // todo
       "/tc/treeListbox/*", // todo
