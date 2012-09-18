cbec22fd4b8eee0abc3538532846aba5b90b2aae
Udo Schnurpfeil <lofwyr@apache.org>
2012-09-18 12:45:23 +0000

    TOBAGO-1201: Clicking into an empty cell of an Ajax-enabled tc:sheet leads to JavaScript error

--- tobago-theme/tobago-theme-standard/src/main/resources/org/apache/myfaces/tobago/renderkit/html/standard/standard/script/tobago.js
+++ tobago-theme/tobago-theme-standard/src/main/resources/org/apache/myfaces/tobago/renderkit/html/standard/standard/script/tobago.js
@@ -1185,7 +1185,11 @@ var Tobago = {
    */
   clearSelection: function() {
     if (document.selection) {  // IE
-      document.selection.empty();
+      try {
+        document.selection.empty();
+      } catch (error) {
+        // Ignore error: seems to be a browser bug (TOBAGO-1201)
+      }
     } else if (window.getSelection) {  // GECKO
       window.getSelection().removeAllRanges();
     }
