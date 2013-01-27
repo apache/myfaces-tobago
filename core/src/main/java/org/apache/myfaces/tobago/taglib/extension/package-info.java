5234e6682dac17dd841e46e72d90d6e3b6184eb3
Bernd Bohmann <bommel@apache.org>
2013-01-27 10:58:42 +0000

    prepare release

--- core/src/main/java/org/apache/myfaces/tobago/taglib/extension/package-info.java
+++ core/src/main/java/org/apache/myfaces/tobago/taglib/extension/package-info.java
@@ -1,6 +1,6 @@
 /**
  * <b>Tobago Framework Extension Tag Library -
- * (C) Copyright 2005-2012 The Apache Software Foundation</b>
+ * (C) Copyright 2005-2013 The Apache Software Foundation</b>
  */
 @org.apache.myfaces.tobago.apt.annotation.Taglib(
     shortName = "tx",
