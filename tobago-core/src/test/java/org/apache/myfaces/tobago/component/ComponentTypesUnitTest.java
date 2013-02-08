bb1cfe91194ed761d1d675b34358efd14998ff8d
Udo Schnurpfeil <lofwyr@apache.org>
2013-02-08 15:03:11 +0000

    check component types

--- tobago-core/src/test/java/org/apache/myfaces/tobago/component/ComponentTypesUnitTest.java
+++ tobago-core/src/test/java/org/apache/myfaces/tobago/component/ComponentTypesUnitTest.java
@@ -30,10 +30,10 @@ public class ComponentTypesUnitTest {
   @Test
   public void testNames() throws IllegalAccessException {
 
-    for (Field field : RendererTypes.class.getFields()) {
+    for (Field field : ComponentTypes.class.getFields()) {
       String value = (String) field.get(null);
-      Assert.assertTrue("value='" + value + "'", value.matches("[A-Z][a-zA-Z]*"));
-      Assert.assertEquals(StringUtils.constantToCamelCase(field.getName()), value);
+      Assert.assertTrue("value='" + value + "'", value.matches("org\\.apache\\.myfaces\\.tobago\\.[A-Z][a-zA-Z]*"));
+      Assert.assertEquals("org.apache.myfaces.tobago." + StringUtils.constantToCamelCase(field.getName()), value);
     }
   }
 }
