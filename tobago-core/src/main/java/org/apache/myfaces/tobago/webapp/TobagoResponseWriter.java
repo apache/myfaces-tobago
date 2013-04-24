0a48f6e89ca3bb226d614d565fd2e853c9519bfe
Udo Schnurpfeil <lofwyr@apache.org>
2013-04-24 11:19:46 +0000

    TOBAGO-1254: Improve logging - only replace contentType when it's needed: "text/html; charset=UTF-8" should be handle as same as "text/html;charset=UTF-8" (without space)

--- tobago-core/src/main/java/org/apache/myfaces/tobago/webapp/TobagoResponseWriter.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/webapp/TobagoResponseWriter.java
@@ -273,7 +273,7 @@ public abstract class TobagoResponseWriter extends ResponseWriter {
     }
 
     StringBuilder builder = new StringBuilder(contentType);
-    builder.append(";charset=");
+    builder.append("; charset=");
     builder.append(characterEncoding);
     return builder.toString();
 
