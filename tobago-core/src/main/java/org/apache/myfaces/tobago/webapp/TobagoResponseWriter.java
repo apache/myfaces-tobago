36ad061ff7c5e9e17f95d26153efdf9a9bcfac23
Udo Schnurpfeil <lofwyr@apache.org>
2013-04-29 15:18:44 +0000

    TOBAGO-1145: The tobago-deprecation.jar will be reduced in Tobago 2.0

--- tobago-core/src/main/java/org/apache/myfaces/tobago/webapp/TobagoResponseWriter.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/webapp/TobagoResponseWriter.java
@@ -134,19 +134,10 @@ public abstract class TobagoResponseWriter extends ResponseWriter {
 
   /**
    * Write the class attribute. The value will not escaped.
-   * <br/>
-   * <b>Note:</b> For backward compatibility the value of the Attribute
-   * <code>{@link org.apache.myfaces.tobago.component.Attributes#STYLE_CLASS}</code>
-   * will be inserted additionally. This function will be removed in later releases.
    * @param classes The abstract representation of the css class string, normally created by the renderer.
    */
   public void writeClassAttribute(Classes classes) throws IOException {
-    String styleClasses = getStyleClasses();
-    String stringValue = classes.getStringValue();
-    if (styleClasses != null) {
-      stringValue += " " + styleClasses;
-    }
-    writeAttribute(HtmlAttributes.CLASS, stringValue, false);
+    writeAttribute(HtmlAttributes.CLASS, classes.getStringValue(), false);
   }
 
   @Deprecated
