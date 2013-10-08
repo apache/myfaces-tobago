f16962ad1c3650e446e29c699b5c1b8335a37246
Udo Schnurpfeil <lofwyr@apache.org>
2013-10-08 11:37:58 +0000

    TOBAGO-1320: SessionSecret not compatible with portlets

--- tobago-core/src/main/java/org/apache/myfaces/tobago/portlet/PortletUtils.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/portlet/PortletUtils.java
@@ -19,10 +19,13 @@
 
 package org.apache.myfaces.tobago.portlet;
 
+import org.apache.myfaces.tobago.webapp.Secret;
+
 import javax.faces.context.FacesContext;
 import javax.portlet.ActionRequest;
 import javax.portlet.PortletContext;
 import javax.portlet.PortletRequest;
+import javax.portlet.PortletSession;
 import javax.portlet.PortletURL;
 import javax.portlet.RenderResponse;
 import java.io.UnsupportedEncodingException;
@@ -59,7 +62,7 @@ public final class PortletUtils {
    *
    * @param facesContext The current FacesContext.
    * @return <code>true</code> if we are processing a RenderResponse,
-   *         <code>false</code> otherwise.
+   * <code>false</code> otherwise.
    */
   public static boolean isRenderResponse(FacesContext facesContext) {
     return PORTLET_API_AVAILABLE && facesContext.getExternalContext().getResponse() instanceof RenderResponse;
@@ -70,7 +73,7 @@ public final class PortletUtils {
    *
    * @param facesContext The current FacesContext.
    * @return <code>true</code> if we are running as a portlet,
-   *         <code>false</code> otherwise.
+   * <code>false</code> otherwise.
    */
 //  public static boolean isPortletRequest(FacesContext facesContext) {
 //    return facesContext.getExternalContext().getSessionMap().get(PORTLET_REQUEST) != null;
@@ -98,6 +101,15 @@ public final class PortletUtils {
     ActionRequest request = (ActionRequest) facesContext.getExternalContext().getRequest();
     if (request.getCharacterEncoding() == null) {
       request.setCharacterEncoding("UTF-8");
+    }
+  }
+
+  public static Secret getAttributeFromSessionForApplication(Object session, String name) {
+
+    if (PORTLET_API_AVAILABLE && session instanceof PortletSession) {
+      return (Secret) ((PortletSession) session).getAttribute(name, PortletSession.APPLICATION_SCOPE);
+    } else {
+      throw new IllegalArgumentException("Unknown session type: " + session.getClass().getName());
     }
   }
 }
