8a8dc7299a19b46537183f8ec2cfee5ceb37f436
Volker Weber <weber@apache.org>
2013-10-29 15:38:19 +0000

    TOBAGO-1332 - SplitLayout: a bit of documentation

--- tobago-extension/tobago-sandbox/src/main/java/org/apache/myfaces/tobago/internal/taglib/sandbox/SplitLayoutTagDeclaration.java
+++ tobago-extension/tobago-sandbox/src/main/java/org/apache/myfaces/tobago/internal/taglib/sandbox/SplitLayoutTagDeclaration.java
@@ -14,8 +14,12 @@ import org.apache.myfaces.tobago.internal.taglib.declaration.HasMargin;
 import org.apache.myfaces.tobago.internal.taglib.declaration.HasMargins;
 import org.apache.myfaces.tobago.internal.taglib.declaration.HasMarkup;
 import org.apache.myfaces.tobago.internal.taglib.declaration.HasSpacing;
-import org.apache.myfaces.tobago.layout.Orientation;
 
+  /**
+   * Renders a SplitLayout.
+   * A area with two child components rendered horizontally or vertically and allows to change the
+   * layout relation of this two components on the client.
+   */
 @Tag(name = "splitLayout", bodyContent = BodyContent.EMPTY)
 @UIComponentTag(
     uiComponent = "org.apache.myfaces.tobago.component.UISplitLayout",
@@ -27,9 +31,19 @@ import org.apache.myfaces.tobago.layout.Orientation;
 public interface SplitLayoutTagDeclaration extends HasId, HasBorder, HasSpacing, HasMargin,
     HasMargins, HasBinding, HasMarkup, HasCurrentMarkup {
 
+  /**
+   * This value defines the layout constraints for the layout.
+   * It is two layout tokens separated by a semicolon. See GridLayout.
+   * Example: '2*;*'.
+   */
   @TagAttribute
   @UIComponentTagAttribute(defaultValue = "1*;1*")
   void setLayout(String rows);
+
+  /**
+   * This value defines the orientation of the split layout.
+   * Possible values are 'HORIZONTAL' and 'VERTICAL'.
+   */
 
   @TagAttribute
   @UIComponentTagAttribute(allowedValues = {"HORIZONTAL", "VERTICAL"})
