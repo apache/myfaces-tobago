32146dffed2e91c990507b5c817c76aff7ee88cd
Udo Schnurpfeil <lofwyr@apache.org>
2014-05-07 12:47:15 +0000

    demo: set context path correctly for "redirect"

--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/TobagoDemoController.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/TobagoDemoController.java
@@ -185,7 +185,7 @@ public class TobagoDemoController implements Serializable {
       session.invalidate();
     }
     final ExternalContext externalContext = facesContext.getExternalContext();
-    externalContext.redirect("/" + externalContext.getRequestContextPath());
+    externalContext.redirect(externalContext.getRequestContextPath() + "/");
     facesContext.responseComplete();
     return null;
   }
