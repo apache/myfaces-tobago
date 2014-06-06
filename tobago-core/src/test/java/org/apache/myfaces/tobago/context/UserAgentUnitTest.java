73744b3df2b8bc712698f4467bf4a3b16b82d930
Udo Schnurpfeil <lofwyr@apache.org>
2014-06-06 08:51:23 +0000

    TOBAGO-1402: IE 11 detection - checkstyle

--- tobago-core/src/test/java/org/apache/myfaces/tobago/context/UserAgentUnitTest.java
+++ tobago-core/src/test/java/org/apache/myfaces/tobago/context/UserAgentUnitTest.java
@@ -92,9 +92,9 @@ public class UserAgentUnitTest {
 
         {"Internet Explorer 11 - Windows 7",
             UserAgent.MSIE_11_0,
-            "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; " +
-                ".NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; " +
-                "rv:11.0) like Gecko"},
+            "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; "
+                + ".NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; "
+                + ".NET4.0C; .NET4.0E; rv:11.0) like Gecko"},
 
         {"Firefox 3.6 - Ubuntu 10.4",
             UserAgent.GECKO_1_9,
