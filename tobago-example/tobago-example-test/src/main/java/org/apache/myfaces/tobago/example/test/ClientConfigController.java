f78db8780ec396c381bdd99f72cd0f32fee6317c
Udo Schnurpfeil <lofwyr@apache.org>
2014-07-02 11:36:05 +0000

    example: no default locale may set

--- tobago-example/tobago-example-test/src/main/java/org/apache/myfaces/tobago/example/test/ClientConfigController.java
+++ tobago-example/tobago-example-test/src/main/java/org/apache/myfaces/tobago/example/test/ClientConfigController.java
@@ -86,11 +86,20 @@ public class ClientConfigController {
     final Locale defaultLocale = application.getDefaultLocale();
     final Iterator supportedLocales = application.getSupportedLocales();
 
+    boolean defaultInList = false;
     final List<SelectItem> localeItems = new ArrayList<SelectItem>();
-    localeItems.add(createLocaleItem(defaultLocale));
     while (supportedLocales.hasNext()) {
       final Locale locale = (Locale) supportedLocales.next();
       localeItems.add(createLocaleItem(locale));
+      if (locale.equals(defaultLocale)) {
+        defaultInList = true;
+      }
+    }
+    // If the default is already in the list, don't add it.
+    // Background: Must the default be in the supported list? Yes or No?
+    // This question is not specified explicit and different implemented in the RI and MyFaces
+    if (!defaultInList && defaultLocale != null) {
+      localeItems.add(0, createLocaleItem(defaultLocale));
     }
     return localeItems;
   }
