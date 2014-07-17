b355bf8b8093583c25071bbcb79ac641ac2a4c56
Udo Schnurpfeil <lofwyr@apache.org>
2014-07-17 07:03:43 +0000

    license/rat

--- tobago-theme/tobago-theme-standard/src/origin/external/prepare-jquery-ui-resources.sh
+++ tobago-theme/tobago-theme-standard/src/origin/external/prepare-jquery-ui-resources.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Please call this script in its directory
 
 # Click "Download" on this site http://jqueryui.com/download/#!version=1.10.4&components=1111100000001000100000000000000000
