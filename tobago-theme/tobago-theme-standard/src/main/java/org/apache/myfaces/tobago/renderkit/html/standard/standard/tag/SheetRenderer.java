239b09f1587dd868eeca3ea561e7100bee69ac26
Volker Weber <weber@apache.org>
2014-11-30 22:18:34 +0000

    TOBAGO-1423 - Sheet: forceVerticalScrollbar=true breaks column alignment: fix layout with fixed column sizes < sheet width

--- tobago-theme/tobago-theme-standard/src/main/java/org/apache/myfaces/tobago/renderkit/html/standard/standard/tag/SheetRenderer.java
+++ tobago-theme/tobago-theme-standard/src/main/java/org/apache/myfaces/tobago/renderkit/html/standard/standard/tag/SheetRenderer.java
@@ -808,7 +808,11 @@ public class SheetRenderer extends LayoutComponentRendererBase {
       writer.startElement(HtmlElements.COLGROUP, null);
       for (int i = 0; i < columnWidths.size(); i++) {
         writer.startElement(HtmlElements.COL, null);
-        writer.writeAttribute(HtmlAttributes.WIDTH, columnWidths.get(i));
+        if (needVerticalScrollbar && i == columnWidths.size() - 1) {
+          writer.writeAttribute(HtmlAttributes.WIDTH, columnWidths.get(i) + verticalScrollbarWidth);
+        } else {
+          writer.writeAttribute(HtmlAttributes.WIDTH, columnWidths.get(i));
+        }
         writer.endElement(HtmlElements.COL);
       }
       writer.endElement(HtmlElements.COLGROUP);
