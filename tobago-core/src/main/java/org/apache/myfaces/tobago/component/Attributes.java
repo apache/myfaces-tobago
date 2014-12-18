31456de3e065e587f9241b004a42d1fcccc3f450
Udo Schnurpfeil <lofwyr@apache.org>
2014-12-18 10:39:53 +0000

    cleanup

--- tobago-core/src/main/java/org/apache/myfaces/tobago/component/Attributes.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/component/Attributes.java
@@ -153,7 +153,6 @@ public final class Attributes {
   public static final String PADDING_RIGHT = "paddingRight";
   /** Used by a layout manager */
   public static final String PADDING_TOP = "paddingTop";
-  public static final String PAGE_MENU = "pageMenu";
   public static final String PASSWORD = "password";
   public static final String POPUP_CLOSE = "popupClose";
   public static final String POPUP_LIST = "popupList";
