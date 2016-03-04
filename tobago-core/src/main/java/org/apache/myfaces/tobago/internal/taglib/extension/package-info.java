246ef1aa8acee8a2fe7d3cee556f1595ab6ccf67
Udo Schnurpfeil <lofwyr@apache.org>
2016-03-04 14:49:51 +0000

    updated (c) year to 2016

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/extension/package-info.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/extension/package-info.java
@@ -19,7 +19,7 @@
 
 /**
  * <b>Tobago Framework Extension Tag Library 3.0.x (deprecated) -
- * (C) Copyright 2005-2015 The Apache Software Foundation</b>
+ * (C) Copyright 2005-2016 The Apache Software Foundation</b>
  */
 @org.apache.myfaces.tobago.apt.annotation.Taglib(
     shortName = "tx",
