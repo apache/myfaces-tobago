b634961f5328274922564e4d22fbc0bb62ca9950
Udo Schnurpfeil <lofwyr@apache.org>
2016-03-16 16:52:57 +0000

    TOBAGO-1541: Use of ResourceBundles should support parameters

--- tobago-core/src/main/java/org/apache/myfaces/tobago/util/MessageFormat.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/util/MessageFormat.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.myfaces.tobago.util;
 
 import javax.faces.context.FacesContext;
@@ -40,12 +59,14 @@ public class MessageFormat {
 
   public static String format(String pattern, Object param0, Object param1, Object param2, Object param3,
                                Object param4, Object param5, Object param6, Object param7) {
-    return createMessageFormat(pattern).format(new Object[] {param0, param1, param2, param3, param4, param5, param6, param7});
+    return createMessageFormat(pattern).format(
+        new Object[] {param0, param1, param2, param3, param4, param5, param6, param7});
   }
 
   public static String format(String pattern, Object param0, Object param1, Object param2, Object param3,
                                Object param4, Object param5, Object param6, Object param7, Object param8) {
-    return createMessageFormat(pattern).format(new Object[] {param0, param1, param2, param3, param4, param5, param6, param7, param8});
+    return createMessageFormat(pattern).format(
+        new Object[] {param0, param1, param2, param3, param4, param5, param6, param7, param8});
   }
 
   private static java.text.MessageFormat createMessageFormat(String pattern) {
