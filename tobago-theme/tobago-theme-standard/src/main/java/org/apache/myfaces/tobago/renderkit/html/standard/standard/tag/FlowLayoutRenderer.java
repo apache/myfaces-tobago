e1c5a5eb810e779c420a461527cf21e6045f18a1
Udo Schnurpfeil <lofwyr@apache.org>
2016-04-14 14:17:07 +0000

    TOBAGO-1543: Paradigm: The clientId shoud be rendered in the local root element in HTML

--- tobago-theme/tobago-theme-standard/src/main/java/org/apache/myfaces/tobago/renderkit/html/standard/standard/tag/FlowLayoutRenderer.java
+++ tobago-theme/tobago-theme-standard/src/main/java/org/apache/myfaces/tobago/renderkit/html/standard/standard/tag/FlowLayoutRenderer.java
@@ -38,6 +38,7 @@ public class FlowLayoutRenderer extends RendererBase {
     final TobagoResponseWriter writer = HtmlRendererUtils.getTobagoResponseWriter(facesContext);
     final UIFlowLayout layout = (UIFlowLayout) component;
     writer.startElement(HtmlElements.DIV);
+    writer.writeIdAttribute(layout.getClientId());
     writer.writeClassAttribute(Classes.create(layout), layout.getCustomClass());
     writer.writeStyleAttribute(layout.getStyle());
   }
