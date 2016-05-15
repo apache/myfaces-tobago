c00def30d26d697d183e2c004ff64fedee979b05
Dennis Kieselhorst <deki@apache.org>
2016-05-15 15:17:48 +0000

    TOBAGO-1473: fixed checkstyle violations

--- tobago-example/tobago-example-demo/src/test/java/org/apache/myfaces/tobago/example/demo/ArquillianTest.java
+++ tobago-example/tobago-example-demo/src/test/java/org/apache/myfaces/tobago/example/demo/ArquillianTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.myfaces.tobago.example.demo;
 
 import java.net.URL;
@@ -35,7 +54,8 @@ public class ArquillianTest {
 
     @Deployment
     public static WebArchive createDeployment() {
-        WebArchive webArchive = ShrinkWrap.create(MavenImporter.class).loadPomFromFile("pom.xml", "jsf-provided", "!myfaces-2.0").importBuildOutput()
+        WebArchive webArchive = ShrinkWrap.create(MavenImporter.class).
+                loadPomFromFile("pom.xml", "jsf-provided", "!myfaces-2.0").importBuildOutput()
                 .as(WebArchive.class);
         webArchive.addClass(ArquillianTest.class);
         // XXX there should be a proper profile in POM for that
@@ -48,7 +68,8 @@ public class ArquillianTest {
         browser.get(contextPath + "/faces/content/20-component/010-input/10-in/in.xhtml");
 
         Assert.assertEquals("On Server", outputField.getText());
-        // TODO results in "Refused to evaluate a string as JavaScript because 'unsafe-eval' is not an allowed source of script in the following Content Security Policy directive"
+        // TODO results in "Refused to evaluate a string as JavaScript because 'unsafe-eval'
+        // is not an allowed source of script in the following Content Security Policy directive"
 //        Graphene.guardAjax(inputField).sendKeys("Testtext");
 
     }
