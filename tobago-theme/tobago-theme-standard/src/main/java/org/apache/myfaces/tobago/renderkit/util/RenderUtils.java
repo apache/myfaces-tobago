53fe7d85621c83608cb8536d996c76fce448f20d
Udo Schnurpfeil <lofwyr@apache.org>
2016-08-15 13:35:49 +0000

    TOBAGO-1574: AJAX not working from change-facet-command * hack to make after and before facets runable with AJAX buttons -> invert logic

--- tobago-theme/tobago-theme-standard/src/main/java/org/apache/myfaces/tobago/renderkit/util/RenderUtils.java
+++ tobago-theme/tobago-theme-standard/src/main/java/org/apache/myfaces/tobago/renderkit/util/RenderUtils.java
@@ -272,7 +272,8 @@ public final class RenderUtils {
       for (Map.Entry<String, UIComponent> facetEntry : ((UIComponent) holder).getFacets().entrySet()) {
 
         // XXX hack to make after and before facets runable with AJAX buttons
-        if (facetEntry.getKey().equals(Facets.before.name()) || facetEntry.getKey().equals(Facets.after.name())) {
+        if (!facetEntry.getKey().equals(Facets.click.name())
+            && !facetEntry.getKey().equals(Facets.change.name())) {
           break;
         }
 
