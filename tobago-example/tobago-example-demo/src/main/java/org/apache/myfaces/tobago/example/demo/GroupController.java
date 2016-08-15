ddcb48552cdc28189fd4c5efb08ba9fbd8870dc0
Udo Schnurpfeil <lofwyr@apache.org>
2016-08-15 13:37:23 +0000

    TOBAGO-1574: AJAX not working from change-facet-command * test

--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/GroupController.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/GroupController.java
@@ -20,24 +20,43 @@
 package org.apache.myfaces.tobago.example.demo;
 
 import org.apache.myfaces.tobago.component.UICommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.Currency;
 
 @SessionScoped
 @Named
 public class GroupController implements Serializable {
 
+  private static final Logger LOG = LoggerFactory.getLogger(GroupController.class);
+
   private String chatlog;
   private String newMessage;
   private String sendTo;
+  private double value;
+  private double valueInEuro;
+  private Currency currency;
+  private static final Currency[] currencies;
+
+  static {
+    currencies = new Currency[]{
+        Currency.getInstance("JPY"),
+        Currency.getInstance("TTD"),
+        Currency.getInstance("USD"),
+        Currency.getInstance("EUR")};
+  }
 
   public GroupController() {
     chatlog = "Peter: Hi, how are you?";
     newMessage = "I'm fine.";
     sendTo = "";
+    value = 1000.0;
+    currency = Currency.getInstance("EUR");
   }
 
   public String getChatlog() {
@@ -65,10 +84,58 @@ public class GroupController implements Serializable {
     this.sendTo = sendTo;
   }
 
-  public void sendToListener(ActionEvent actionEvent) {
-    if (actionEvent != null && actionEvent.getComponent() instanceof UICommand) {
-      UICommand command = (UICommand) actionEvent.getComponent();
+  public void sendToListener(AjaxBehaviorEvent event) {
+
+    LOG.info("AjaxBehaviorEvent called.");
+
+    if (event != null && event.getComponent() instanceof UICommand) {
+      UICommand command = (UICommand) event.getComponent();
       sendTo = command.getLabel();
+      LOG.info("AjaxBehaviorEvent called. Current label: '{}'", sendTo);
     }
+  }
+
+  public void compute(AjaxBehaviorEvent event) {
+    LOG.info("AjaxBehaviorEvent called.");
+    compute();
+  }
+
+  private void compute() {
+    if (currency.getCurrencyCode().equals("JPY")) {
+      valueInEuro = value * 0.00884806667;
+    } else if (currency.getCurrencyCode().equals("TTD")) {
+      valueInEuro = value * 0.133748824;
+    } else if (currency.getCurrencyCode().equals("USD")) {
+      valueInEuro = value * 0.896097495;
+    } else if (currency.getCurrencyCode().equals("EUR")) {
+      valueInEuro = value;
+    } else {
+      valueInEuro = 0;
+    }
+    LOG.info("euro value: '{}', value: '{}', currency: '{}'", valueInEuro, value, currency.getCurrencyCode());
+  }
+
+  public double getValue() {
+    return value;
+  }
+
+  public void setValue(double value) {
+    this.value = value;
+  }
+
+  public double getValueInEuro() {
+    return valueInEuro;
+  }
+
+  public Currency getCurrency() {
+    return currency;
+  }
+
+  public void setCurrency(Currency currency) {
+    this.currency = currency;
+  }
+
+  public Currency[] getCurrencies() {
+    return currencies;
   }
 }
