8ea59ab1ce0489249b7965a262b514bca71a849d
Udo Schnurpfeil <lofwyr@apache.org>
2017-09-13 16:55:27 +0200

    TOBAGO-1786: Selector to address UIStyle * fix of id writing in selector ** checkstyle

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/util/StyleRenderUtils.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/util/StyleRenderUtils.java
@@ -55,12 +55,13 @@ public class StyleRenderUtils {
   }
 
   // not using writeText, because > must not be encoded!
-  public static void writeSelector(TobagoResponseWriter writer, String selector) throws IOException {
+  public static void writeSelector(final TobagoResponseWriter writer, final String selector) throws IOException {
     if (selector.contains("<")) {
       LOG.warn("Found invalid char < inside of style!");
-      selector = selector.replaceAll("<", "&lt;");
+      writer.write(selector.replaceAll("<", "&lt;"));
+    } else {
+      writer.write(selector);
     }
-    writer.write(selector);
   }
 
   public static String encodeSelector(String... selector) {
