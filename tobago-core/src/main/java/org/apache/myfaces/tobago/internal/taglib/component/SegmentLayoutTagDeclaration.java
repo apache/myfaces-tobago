fe4213390b65470033bc5c5a714c34b53b357df3
Udo Schnurpfeil <lofwyr@apache.org>
2017-11-30 13:03:36 +0100

    TOBAGO-1818: Progress in the Measure class (cleanup and CSS support) * replace "N" with "Nseg" and use spaces instead of ";"

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/SegmentLayoutTagDeclaration.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/SegmentLayoutTagDeclaration.java
@@ -32,7 +32,7 @@ import org.apache.myfaces.tobago.internal.taglib.declaration.IsVisual;
  * Renders a layout using a 12 columns grid.
  * Find more information on how the grid works in the Twitter Bootstrap documentation.
  *
- * If no attribute is defined, extraSmall="12" will be used as default.
+ * If no attribute is defined, extraSmall="12seg" will be used as default.
  * @since 3.0.0
  */
 @Tag(name = "segmentLayout")
