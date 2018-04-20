8f51b4009cc964a30729e7fb1cb49da26b9780ee
Udo Schnurpfeil <lofwyr@apache.org>
2018-04-20 13:35:54 +0200

    rat check

--- tobago-example/tobago-example-demo/src/main/webapp/script/login.js
+++ tobago-example/tobago-example-demo/src/main/webapp/script/login.js
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * Copies the values from the data-login attribute to the username/password fields.
  */
@@ -24,4 +41,4 @@ Demo.prepareLoginForm = function() {
   jQuery(Tobago.Utils.escapeClientId("page::form")).attr("action", contextPath + "/j_security_check");
 };
 
-// Tobago.registerListener(Demo.prepareLoginForm, Tobago.Phase.DOCUMENT_READY);
+// XXX turned off in the moment Tobago.registerListener(Demo.prepareLoginForm, Tobago.Phase.DOCUMENT_READY);
