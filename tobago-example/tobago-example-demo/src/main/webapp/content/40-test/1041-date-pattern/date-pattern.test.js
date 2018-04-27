9f8fc8cdc32abcb80e51034a2c499c79d0f92b04
Udo Schnurpfeil <lofwyr@apache.org>
2018-04-27 15:12:08 +0200

    TOBAGO-1843: Demo
    progress on date pattern...

---
+++ tobago-example/tobago-example-demo/src/main/webapp/content/40-test/1041-date-pattern/date-pattern.test.js
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+QUnit.test("test patterns", function (assert) {
+
+  assert.ok(false);
+  TestDateTime.testDate();
+  TestDateTime.testTime();
+  TestDateTime.testBoth();
+});
+
+
+TestDateTime.testDate = function () {
+
+  var date;
+  var format = "dd.mm.yy";
+  var initial = "13.05.2014";
+  var result;
+
+  date = moment(initial, format);
+  result = moment(date).format(format);
+
+  if (result !== initial) {
+    console.error("Error!");
+  } else {
+    console.info("Okay!");
+  }
+
+};
+
+TestDateTime.testTime = function () {
+
+  var date;
+  var format = "HH:mm:ss";
+  var initial = "14:06:55";
+  var result;
+
+  date = moment(initial, format);
+  result = moment(date).format(format);
+
+  if (result !== initial) {
+    console.error("Error!");
+  } else {
+    console.info("Okay!");
+  }
+
+};
+
+TestDateTime.testBoth = function () {
+
+  var date, dateT;
+  var formatD = "dd.mm.yy";
+  var formatT = "HH:mm:ss";
+  var initial = "13.05.2014 13:05:55";
+  var result;
+  var separator = " ";
+
+  date = moment(initial, formatD + ' ' +  formatT);
+  dateT = {
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds(),
+    millisec: date.getMilliseconds(),
+    microsec: date.getMicroseconds()
+  };
+  result = moment(date).format(formatD) + separator + jQuery.datepicker.formatTime(formatT, dateT);
+
+  if (result !== initial) {
+    console.error("Error!");
+  } else {
+    console.info("Okay!");
+  }
+
+};
