9df8571b0b471df56c3e598d0e67d799a64c4224
Udo Schnurpfeil <lofwyr@apache.org>
2018-09-21 12:23:22 +0200

    TOBAGO-1913: Enhance tobago-config.xsd: there should be an includes tag in resources tag
    * fix version
    * remove old tag position of <script> and <style>

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/config/TobagoConfigParser.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/config/TobagoConfigParser.java
@@ -451,8 +451,8 @@ public class TobagoConfigParser extends TobagoConfigEntityResolver {
 
     final SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
     final Schema schema;
-    if ("4.3".equals(version.getVersion())) {
-      schema = schemaFactory.newSchema(getClass().getResource(TOBAGO_CONFIG_XSD_4_3));
+    if ("5.0".equals(version.getVersion())) {
+      schema = schemaFactory.newSchema(getClass().getResource(TOBAGO_CONFIG_XSD_5_0));
     } else if ("4.0".equals(version.getVersion())) {
       schema = schemaFactory.newSchema(getClass().getResource(TOBAGO_CONFIG_XSD_4_0));
     } else if ("3.0".equals(version.getVersion())) {
