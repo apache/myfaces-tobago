cf57a14d5b61c2d9cc3454a49b58d6203a50fe3a
Udo Schnurpfeil <lofwyr@apache.org>
2018-11-29 10:48:35 +0100

    rat check

--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/DockerServer.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/DockerServer.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.myfaces.tobago.example.demo;
 
 import java.io.Serializable;
@@ -36,7 +55,7 @@ public enum DockerServer implements Serializable {
   private boolean ssl;
 
   DockerServer(
-      final String displayName, final String volume, final int port,final int sslPort,
+      final String displayName, final String volume, final int port, final int sslPort,
       final String image, final String[] tags, final boolean ssl) {
     this.displayName = displayName;
     this.volume = volume;
