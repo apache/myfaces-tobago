dadfbb2cea48b87ebfe4bf4651e19a6a4d313463
Udo Schnurpfeil <lofwyr@apache.org>
2019-01-08 09:45:14 +0100

    don't warn for empty sheets
    (cherry picked from commit 6270313cfde905052d0b95e08c8b99fc802b8a86)

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIData.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIData.java
@@ -135,10 +135,12 @@ public abstract class AbstractUIData extends javax.faces.component.UIData implem
     }
 
     if (getFirst() >= getRowCount()) {
-      LOG.warn("Illegal paging state detected, first='{}' >= rowCount='{}'. Setting first to 0. "
-          + "This might happen because the data model has changed. "
-          + "You may want to manipulate the sheet state in your application after manipulating the model "
-          + "(e. g. filtering) to avoid this warning.", getFirst(), getRowCount());
+      if (getRowCount() > 0) {
+        LOG.warn("Illegal paging state detected, first='{}' >= rowCount='{}'. Setting first to 0. "
+            + "This might happen because the data model has changed. "
+            + "You may want to manipulate the sheet state in your application after manipulating the model "
+            + "(e. g. filtering) to avoid this warning.", getFirst(), getRowCount());
+      }
       setFirst(0);
     }
 
