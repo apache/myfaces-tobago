20e25f22be279aaef17109f30215539b00b3e8ca
Henning Noeth <hnoeth@apache.org>
2019-03-04 16:17:35 +0100

    TOBAGO-1957 go to today button for tc:date
    * implement todayButton attribute
    * improve demo
    * add feature to new-in-*.xhtml files

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIDate.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIDate.java
@@ -49,4 +49,6 @@ public abstract class AbstractUIDate extends AbstractUIInput {
     }
     return DateFormatUtils.findPattern((DateTimeConverter) converter);
   }
+
+  public abstract boolean isTodayButton();
 }
