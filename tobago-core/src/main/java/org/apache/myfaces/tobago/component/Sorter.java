b243569b5d3ece9fe49224b37c99bfa626e1fffe
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2019-10-23 20:51:29 +0200

    TOBAGO-2019: NPE tobago-core-2.4.1 NPE Sorter.java

--- tobago-core/src/main/java/org/apache/myfaces/tobago/component/Sorter.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/component/Sorter.java
@@ -150,7 +150,7 @@ public class Sorter {
 
       // memorize selected rows
       List<Object> selectedDataRows = null;
-      if (sheetState.getSelectedRows().size() > 0) {
+      if (sheetState.getSelectedRows() != null && sheetState.getSelectedRows().size() > 0) {
         selectedDataRows = new ArrayList<Object>(sheetState.getSelectedRows().size());
         Object dataRow;
         for (final Integer index : sheetState.getSelectedRows()) {
