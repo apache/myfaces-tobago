189d4dbeb35c823b057046f9c170f230e717ec3d
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2020-07-02 11:58:51 +0200

    remove directory tobago-bootstrap in the resoures path
    issue: TOBAGO-1932 Refactor Bootstrap Theme Building

--- tobago-example/tobago-example-demo/src/main/webapp/script/demo.js
+++ tobago-example/tobago-example-demo/src/main/webapp/script/demo.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import {Listener, Phase} from "../tobago/standard/tobago-bootstrap/5.0.0-SNAPSHOT/js/tobago-listener";
+import {Listener, Phase} from "../tobago/standard/5.0.0-SNAPSHOT/js/tobago-listener";
 
 class Demo {
 
