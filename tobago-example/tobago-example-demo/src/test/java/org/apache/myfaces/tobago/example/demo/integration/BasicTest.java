385cfa62b742c3592f0fa4bc37ec26cd92470224
Henning Nöth <hnoeth@apache.org>
2020-12-15 20:59:28 +0100

    Integration tests with testcontainers
    * update junit5; add testcontainers dependencies
    * add TestRule fake class, which is needed for testcontainers
    * better naming package, class, tests
    * refactor integration tests to work with jasmine

---
+++ tobago-example/tobago-example-demo/src/test/java/org/apache/myfaces/tobago/example/demo/integration/BasicTest.java
@@ -0,0 +1,161 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.myfaces.tobago.example.demo.integration;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.invoke.MethodHandles;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalTime;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Testcontainers
+class BasicTest extends FrontendBase {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  static LocalTime startTime;
+
+  @BeforeAll
+  public static void setup() {
+    startTime = LocalTime.now();
+  }
+
+  @Test
+  void verifyExceptionTest() throws MalformedURLException, UnknownHostException, UnsupportedEncodingException {
+    final String host = InetAddress.getLocalHost().getHostAddress();
+    final int tomcatPort = tomcat.getFirstMappedPort();
+    final String path = "error/exception.xhtml";
+
+    final String base = path.substring(0, path.length() - 6);
+    final String url = "http://" + host + ":" + tomcatPort + "/test.xhtml?base="
+        + URLEncoder.encode(base, "UTF-8") + "&basicTest=true";
+
+    WebDriver webDriver = getWebDriver(host, seleniumChrome.getFirstMappedPort());
+    webDriver.get(url);
+
+    List<WebElement> results = getJasmineResults(webDriver);
+    Assertions.assertTrue(results.size() > 0, "no results detected");
+    for (WebElement result : results) {
+      if ("has no exception".equals(result.getAttribute("title"))) {
+        Assertions.assertEquals("jasmine-failed", result.getAttribute("class"), result.getAttribute("title"));
+      } else {
+        Assertions.assertEquals("jasmine-passed", result.getAttribute("class"), result.getAttribute("title"));
+      }
+    }
+  }
+
+  @Test
+  void verify404Test() throws MalformedURLException, UnknownHostException, UnsupportedEncodingException {
+    final String host = InetAddress.getLocalHost().getHostAddress();
+    final int tomcatPort = tomcat.getFirstMappedPort();
+    final String path = "error/404.xhtml";
+
+    final String base = path.substring(0, path.length() - 6);
+    final String url = "http://" + host + ":" + tomcatPort + "/test.xhtml?base="
+        + URLEncoder.encode(base, "UTF-8") + "&basicTest=true";
+
+    WebDriver webDriver = getWebDriver(host, seleniumChrome.getFirstMappedPort());
+    webDriver.get(url);
+
+    List<WebElement> results = getJasmineResults(webDriver);
+    Assertions.assertTrue(results.size() > 0, "no results detected");
+    for (WebElement result : results) {
+      if ("has no 404".equals(result.getAttribute("title"))) {
+        Assertions.assertEquals("jasmine-failed", result.getAttribute("class"), result.getAttribute("title"));
+      } else {
+        Assertions.assertEquals("jasmine-passed", result.getAttribute("class"), result.getAttribute("title"));
+      }
+    }
+  }
+
+  /**
+   * Call every page without a specific *.test.js and run general tests (duplicated Ids, 404, exception, ...).
+   */
+  @ParameterizedTest
+  @MethodSource("basicTestProvider")
+  void basicTest(String path, int testNumber, int testSize)
+      throws MalformedURLException, UnknownHostException, UnsupportedEncodingException {
+
+    final String timeLeft = getTimeLeft(startTime, testSize, testNumber);
+    final String host = InetAddress.getLocalHost().getHostAddress();
+    final int tomcatPort = tomcat.getFirstMappedPort();
+
+    LOG.info("(" + testNumber + "/" + testSize + " | time left: " + timeLeft + ")"
+        + " - url: http://" + host + ":" + tomcatPort + "/" + path);
+
+    final String base = path.substring(0, path.length() - 6);
+    final String url = "http://" + host + ":" + tomcatPort + "/test.xhtml?base="
+        + URLEncoder.encode(base, "UTF-8") + "&basicTest=true";
+
+    WebDriver webDriver = getWebDriver(host, seleniumChrome.getFirstMappedPort());
+    webDriver.get(url);
+
+    List<WebElement> results = getJasmineResults(webDriver);
+    parseJasmineResults(results);
+  }
+
+  private static Stream<Arguments> basicTestProvider() throws IOException {
+    final List<String> paths = Files.walk(Paths.get("src/main/webapp/content/"))
+        .filter(Files::isRegularFile)
+        .map(Path::toString)
+        .filter(s -> s.endsWith(".xhtml"))
+        .filter(s -> !s.contains("/x-"))
+        .map(s -> s.substring("src/main/webapp/".length()))
+        .sorted()
+        .collect(Collectors.toList());
+
+    for (String standardTestPath : getStandardTestPaths()) {
+      paths.remove(standardTestPath); // already tested by standard test
+    }
+
+    final int testSize = paths.size();
+
+    int testNumber = 1;
+    List<Arguments> arguments = new LinkedList<>();
+    for (String path : paths) {
+      arguments.add(Arguments.of(path, testNumber, testSize));
+      testNumber++;
+    }
+    return arguments.stream();
+  }
+}
---
+++ tobago-example/tobago-example-demo/src/test/java/org/apache/myfaces/tobago/example/demo/integration/FrontendBase.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.myfaces.tobago.example.demo.integration;
+
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+abstract class FrontendBase {
+
+  @SuppressWarnings("rawtypes") // this is how to use testcontainers
+  @Container
+  public static GenericContainer seleniumChrome =
+      new GenericContainer<>(DockerImageName.parse("selenium/standalone-chrome")).withExposedPorts(4444);
+
+  @SuppressWarnings("rawtypes") // this is how to use testcontainers
+  @Container
+  public static GenericContainer tomcat =
+      new GenericContainer(DockerImageName.parse("myfaces/tobago-example-demo")).withExposedPorts(8080);
+
+  private static WebDriver chromeDriver;
+
+  @AfterAll
+  static void tearDown() {
+    if (chromeDriver != null) {
+      chromeDriver.quit();
+    }
+  }
+
+  static List<String> getStandardTestPaths() throws IOException {
+    return Files.walk(Paths.get("src/main/webapp/content/"))
+        .filter(Files::isRegularFile)
+        .map(Path::toString)
+        .filter(s -> s.endsWith(".test.js"))
+        .map(s -> s.substring("src/main/webapp/".length()))
+        .sorted()
+        .map(s -> s.substring(0, s.length() - 8) + ".xhtml")
+        .collect(Collectors.toList());
+  }
+
+  WebDriver getWebDriver(final String host, final Integer port) throws MalformedURLException {
+    if (chromeDriver == null || ((RemoteWebDriver) chromeDriver).getSessionId() == null) {
+      chromeDriver = new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"), new ChromeOptions());
+    }
+    return chromeDriver;
+  }
+
+  List<WebElement> getJasmineResults(WebDriver webDriver) {
+    final FluentWait<WebDriver> fluentWait = new FluentWait<>(webDriver)
+        .withTimeout(Duration.ofSeconds(60))
+        .pollingEvery(Duration.ofSeconds(1))
+        .ignoring(NoSuchElementException.class);
+    fluentWait.until(driver -> driver.findElement(By.className("jasmine-overall-result")));
+
+    return webDriver.findElements(By.cssSelector(".jasmine-symbol-summary li"));
+  }
+
+  void parseJasmineResults(List<WebElement> results) {
+    Assertions.assertTrue(results.size() > 0, "no results detected");
+    for (WebElement result : results) {
+      Assertions.assertEquals("jasmine-passed", result.getAttribute("class"), result.getAttribute("title"));
+    }
+  }
+
+  String getTimeLeft(final LocalTime startTime, final int testSize, final int testNo) {
+    final LocalTime now = LocalTime.now();
+    final Duration completeWaitTime = Duration.between(startTime, now).dividedBy(testNo).multipliedBy(testSize);
+    final LocalTime endTime = LocalTime.from(startTime).plus(completeWaitTime);
+    final Duration timeLeft = Duration.between(LocalTime.now(), endTime);
+
+    if (timeLeft.toHours() > 0) {
+      return DurationFormatUtils.formatDuration(timeLeft.toMillis(), "H'h' m'm' s's'");
+    } else if (timeLeft.toMinutes() > 0) {
+      return DurationFormatUtils.formatDuration(timeLeft.toMillis(), "m'm' s's'");
+    } else if (timeLeft.toMillis() >= 0) {
+      return DurationFormatUtils.formatDuration(timeLeft.toMillis(), "s's'");
+    } else {
+      return "---";
+    }
+  }
+}
