60416fbf80830b23dfb71c5fd046d41593a28432
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2021-02-18 18:05:54 +0100

    TOBAGO-2036: Replace date-picker implementation with vanillajs-datepicker
    * clean up
    * pattern conversion on server instead of client

--- tobago-theme/tobago-theme-standard/npm/dist/js/tobago.js
+++ tobago-theme/tobago-theme-standard/npm/dist/js/tobago.js
@@ -2869,100 +2869,6 @@
      * See the License for the specific language governing permissions and
      * limitations under the License.
      */
-    // XXX it might be nice, if this util was in tobago-date.ts, but in that case there are problems
-    // XXX with Jest (UnitTesting)
-    class DateUtils {
-        /*
-        Get the pattern from the "Java world",
-        see https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/text/SimpleDateFormat.html
-        and convert it to 'vanillajs-datepicker', see https://mymth.github.io/vanillajs-datepicker/#/date-string+format
-        Attention: Not every pattern char is supported.
-        */
-        static convertPatternJava2Js(originalPattern) {
-            let pattern;
-            if (!originalPattern || originalPattern.length > 100) {
-                console.warn("Pattern not supported: " + originalPattern);
-                pattern = "";
-            }
-            else {
-                pattern = originalPattern;
-            }
-            let analyzedPattern = "";
-            let nextSegment = "";
-            let escMode = false;
-            for (let i = 0; i < pattern.length; i++) {
-                const currentChar = pattern.charAt(i);
-                if (currentChar == "'" && escMode == false) {
-                    escMode = true;
-                    analyzedPattern += DateUtils.convertPatternPart(nextSegment);
-                    nextSegment = "";
-                }
-                else if (currentChar == "'" && pattern.charAt(i + 1) == "'") {
-                    if (escMode) {
-                        nextSegment += "\\";
-                    }
-                    nextSegment += "'";
-                    i++;
-                }
-                else if (currentChar == "'" && escMode == true) {
-                    escMode = false;
-                    analyzedPattern += nextSegment;
-                    nextSegment = "";
-                }
-                else {
-                    if (escMode) {
-                        nextSegment += "\\";
-                    }
-                    nextSegment += currentChar;
-                }
-            }
-            if (nextSegment != "") {
-                if (escMode) {
-                    analyzedPattern += nextSegment;
-                }
-                else {
-                    analyzedPattern += this.convertPatternPart(nextSegment);
-                }
-            }
-            return analyzedPattern;
-        }
-        static convertPatternPart(originalPattern) {
-            let pattern = originalPattern;
-            if (pattern.search("G") > -1 || pattern.search("W") > -1 || pattern.search("F") > -1
-                || pattern.search("K") > -1 || pattern.search("z") > -1 || pattern.search("X") > -1) {
-                console.warn("Pattern chars 'G', 'W', 'F', 'K', 'z' and 'X' are not supported: " + pattern);
-                pattern = "";
-            }
-            if (pattern.search("y") > -1) {
-                pattern = pattern.replace(/y/g, "y");
-            }
-            if (pattern.search("M") > -1) {
-                pattern = pattern.replace(/M/g, "m");
-            }
-            if (pattern.search("d") > -1) {
-                pattern = pattern.replace(/dd+/g, "dd");
-                pattern = pattern.replace(/\bd\b/g, "d");
-            }
-            return pattern;
-        }
-    }
-
-    /*
-     * Licensed to the Apache Software Foundation (ASF) under one or more
-     * contributor license agreements.  See the NOTICE file distributed with
-     * this work for additional information regarding copyright ownership.
-     * The ASF licenses this file to You under the Apache License, Version 2.0
-     * (the "License"); you may not use this file except in compliance with
-     * the License.  You may obtain a copy of the License at
-     *
-     *      http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
     class Config {
         static set(key, value) {
             this.map.set(key, value);
@@ -3468,6 +3374,7 @@
       requiresIfExists: ['preventOverflow']
     };
 
+    var round = Math.round;
     var unsetSides = {
       top: 'auto',
       right: 'auto',
@@ -3483,8 +3390,8 @@
       var win = window;
       var dpr = win.devicePixelRatio || 1;
       return {
-        x: Math.round(x * dpr) / dpr || 0,
-        y: Math.round(y * dpr) / dpr || 0
+        x: round(round(x * dpr) / dpr) || 0,
+        y: round(round(y * dpr) / dpr) || 0
       };
     }
 
@@ -3500,7 +3407,7 @@
           adaptive = _ref2.adaptive,
           roundOffsets = _ref2.roundOffsets;
 
-      var _ref3 = roundOffsets ? roundOffsetsByDPR(offsets) : offsets,
+      var _ref3 = roundOffsets === true ? roundOffsetsByDPR(offsets) : typeof roundOffsets === 'function' ? roundOffsets(offsets) : offsets,
           _ref3$x = _ref3.x,
           x = _ref3$x === void 0 ? 0 : _ref3$x,
           _ref3$y = _ref3.y,
@@ -3799,7 +3706,7 @@
       }
 
       var scrollParent = getScrollParent(element);
-      var isBody = getNodeName(scrollParent) === 'body';
+      var isBody = scrollParent === element.ownerDocument.body;
       var win = getWindow(scrollParent);
       var target = isBody ? [win].concat(win.visualViewport || [], isScrollParent(scrollParent) ? scrollParent : []) : scrollParent;
       var updatedList = list.concat(target);
@@ -10453,8 +10360,7 @@
             }
         }
         get pattern() {
-            const pattern = this.getAttribute("pattern");
-            return DateUtils.convertPatternJava2Js(pattern); // todo: to the conversation in Java, not here
+            return this.getAttribute("pattern");
         }
         get i18n() {
             const i18n = this.getAttribute("i18n");
