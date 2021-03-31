cf66a23622a1c479b662c37db909f0893ecc1dbc
Henning Noeth <hnoeth@apache.org>
2021-03-31 13:54:23 +0200

    fix(sheet): columnNode
    fix tree in sheet

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIColumnNode.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIColumnNode.java
@@ -26,5 +26,5 @@ import org.apache.myfaces.tobago.component.Visual;
  *
  * @since Tobago 2.0.0
  */
-public abstract class AbstractUIColumnNode extends AbstractUITreeNodeBase implements Visual {
+public abstract class AbstractUIColumnNode extends AbstractUITreeNode implements Visual {
 }
