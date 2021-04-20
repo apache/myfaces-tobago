87e4154d5201fa6f208219185d2f6e80f6cac87f
Henning Noeth <hnoeth@apache.org>
2021-04-20 15:19:26 +0200

    fix(demo): OutController
    Add RequestScoped annotation

--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/OutController.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/OutController.java
@@ -19,15 +19,16 @@
 
 package org.apache.myfaces.tobago.example.demo;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 
 @Named
+@RequestScoped
 public class OutController implements Serializable {
 
   public String getHtml() {
     return "Text in <span style='color:#FF0000'>red</span>, "
         + "<span style='color:#00FF00'>green</span> and <span style='color:#0000FF'>blue</span>.";
   }
-
 }
