007f055c8f22a4a55e434f2675c95a72c97216ed
Henning Noeth <hnoeth@apache.org>
2021-06-02 10:04:50 +0200

    build(theme): rebuild after changes for suggest, tabGroup, package.json
    * feat(suggest): client sided filtering
    * docs: package.json version
    * feat: icon for label of tab
    
    Issue: TOBAGO-2085
    Issue: TOBAGO-2086

--- tobago-theme/tobago-theme-standard/src/main/js/tobago.js
+++ tobago-theme/tobago-theme-standard/src/main/js/tobago.js
@@ -11234,6 +11234,29 @@
    * See the License for the specific language governing permissions and
    * limitations under the License.
    */
+  var SuggestFilter;
+  (function (SuggestFilter) {
+      SuggestFilter[SuggestFilter["all"] = 0] = "all";
+      SuggestFilter[SuggestFilter["prefix"] = 1] = "prefix";
+      SuggestFilter[SuggestFilter["contains"] = 2] = "contains"; // (default) checks if the typed text is inside of the suggested string
+  })(SuggestFilter || (SuggestFilter = {}));
+
+  /*
+   * Licensed to the Apache Software Foundation (ASF) under one or more
+   * contributor license agreements.  See the NOTICE file distributed with
+   * this work for additional information regarding copyright ownership.
+   * The ASF licenses this file to You under the Apache License, Version 2.0
+   * (the "License"); you may not use this file except in compliance with
+   * the License.  You may obtain a copy of the License at
+   *
+   *      http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+   */
   class Suggest {
       constructor(tobagoIn) {
           this.tobagoIn = tobagoIn;
@@ -11271,7 +11294,14 @@
                           });
                       }
                       else {
-                          return resolve(this.filterItems());
+                          switch (this.filter) {
+                              case SuggestFilter.all:
+                                  return resolve(this.filterAll());
+                              case SuggestFilter.prefix:
+                                  return resolve(this.filterPrefix());
+                              default:
+                                  return resolve(this.filterContains());
+                          }
                       }
                   });
               },
@@ -11291,10 +11321,18 @@
       }
       resolvePromise(event) {
           if (event.source === this.suggest && event.status === "success") {
-              return this.resolve(this.filterItems());
+              return this.resolve(this.filterAll());
           }
       }
-      filterItems() {
+      filterAll() {
+          return this.items;
+      }
+      filterPrefix() {
+          return this.items.filter(item => {
+              return item.toLowerCase().startsWith(this.hiddenInput.value);
+          });
+      }
+      filterContains() {
           return this.items.filter(item => {
               return item.toLowerCase().indexOf(this.hiddenInput.value) > -1;
           });
@@ -11391,6 +11429,9 @@
       }
       get localMenu() {
           return this.suggest.getAttribute("local-menu") !== null;
+      }
+      get filter() {
+          return SuggestFilter[this.suggest.getAttribute("filter")];
       }
   }
 
