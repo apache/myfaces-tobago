8e6c9b880cf110792dd6cf92e2aa0d60b8c71b96
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2021-06-17 07:38:24 +0200

    fix(demo): Controller needs a scope

--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/ButtonLinkController.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/ButtonLinkController.java
@@ -22,11 +22,13 @@ package org.apache.myfaces.tobago.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
 
 @Named
+@RequestScoped
 public class ButtonLinkController implements Serializable {
 
   private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
