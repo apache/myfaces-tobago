02380f05ac44a111459ecdc4649c2473e19115fe
Henning Noeth <hnoeth@apache.org>
2021-10-06 15:43:30 +0200

    test(event): implement Jasmine test

--- tobago-example/tobago-example-demo/src/main/webapp/content/900-test/6000-event/Event.test.js
+++ tobago-example/tobago-example-demo/src/main/webapp/content/900-test/6000-event/Event.test.js
@@ -16,174 +16,141 @@
  */
 
 import {JasmineTestTool} from "/tobago/test/tobago-test-tool.js";
+import {elementByIdFn, querySelectorAllFn} from "/script/tobago-test.js";
 
-it("must be fixed first", function (done) {
-  let test = new JasmineTestTool(done);
-  test.do(() => fail("not implemented yet"));
+it("tc:button", function (done) {
+  const eventNames = ["click", "dblclick", "focus", "blur"];
+  const eventComponentFn = elementByIdFn("page:mainForm:buttonevent");
+  const ajaxComponentFn = elementByIdFn("page:mainForm:buttonajax");
+
+  const test = new JasmineTestTool(done);
+  createSteps(test, "button", eventNames, eventComponentFn, ajaxComponentFn, null);
   test.start();
 });
 
-/*
-import {querySelectorAllFn, querySelectorFn} from "/script/tobago-test.js";
-import {TobagoTestTool} from "/tobago/test/tobago-test-tool.js";
+it("tc:in", function (done) {
+  const eventNames = ["change", "click", "dblclick", "focus", "blur"];
+  const eventComponentFn = elementByIdFn("page:mainForm:inevent::field");
+  const ajaxComponentFn = elementByIdFn("page:mainForm:inajax::field");
 
-QUnit.test("tc:button", function (assert) {
-  let eventNames = ["click", "dblclick", "focus", "blur"];
-  let eventComponentFn = querySelectorFn("#page\\:mainForm\\:buttonevent");
-  let ajaxComponentFn = querySelectorFn("#page\\:mainForm\\:buttonajax");
-  testEvent(assert, "button", eventNames, eventComponentFn, ajaxComponentFn, null);
-});
-
-QUnit.test("tc:in", function (assert) {
-  let eventNames = ["change", "click", "dblclick", "focus", "blur"];
-  let eventComponentFn = querySelectorFn("#page\\:mainForm\\:inevent\\:\\:field");
-  let ajaxComponentFn = querySelectorFn("#page\\:mainForm\\:inajax\\:\\:field");
-  let changeValue = function (componentFn) {
+  const changeValueFn = function (componentFn) {
     let oldValue = componentFn().value;
-    let newValue = "hello";
-    if (oldValue === "hello") {
-      newValue = "hello there!"
+    let newValue = "ping";
+    if (oldValue === "ping") {
+      newValue = "pong"
     }
     componentFn().value = newValue;
   };
-  testEvent(assert, "in", eventNames, eventComponentFn, ajaxComponentFn, changeValue);
+
+  const test = new JasmineTestTool(done);
+  createSteps(test, "in", eventNames, eventComponentFn, ajaxComponentFn, changeValueFn);
+  test.start();
 });
 
-QUnit.test("tc:row", function (assert) {
-  let eventNames = ["click", "dblclick"];
-  let eventComponentFn = querySelectorFn("#page\\:mainForm\\:sheetevent\\:0\\:selectPlanet");
-  let ajaxComponentFn = querySelectorFn("#page\\:mainForm\\:sheetajax\\:0\\:selectPlanet");
-  testEvent(assert, "row", eventNames, eventComponentFn, ajaxComponentFn, null);
+it("tc:row", function (done) {
+  const eventNames = ["click", "dblclick"];
+  const eventComponentFn = elementByIdFn("page:mainForm:sheetevent:0:selectPlanet");
+  const ajaxComponentFn = elementByIdFn("page:mainForm:sheetajax:0:selectPlanet");
+
+  const test = new JasmineTestTool(done);
+  createSteps(test, "row", eventNames, eventComponentFn, ajaxComponentFn, null);
+  test.start();
 });
 
-QUnit.test("tc:selectBooleanCheckbox", function (assert) {
-  let eventNames = ["change", "click", "dblclick", "focus", "blur"];
-  let eventComponentFn = querySelectorFn("#page\\:mainForm\\:selectBooleanCheckboxevent\\:\\:field");
-  let ajaxComponentFn = querySelectorFn("#page\\:mainForm\\:selectBooleanCheckboxajax\\:\\:field");
-  let changeValue = function (componentFn) {
+it("tc:selectBooleanCheckbox", function (done) {
+  const eventNames = ["change", "click", "dblclick", "focus", "blur"];
+  const eventComponentFn = elementByIdFn("page:mainForm:selectBooleanCheckboxevent::field");
+  const ajaxComponentFn = elementByIdFn("page:mainForm:selectBooleanCheckboxajax::field");
+
+  const changeValueFn = function (componentFn) {
     componentFn().checked = !componentFn().checked;
   };
-  testEvent(assert, "selectBooleanCheckbox", eventNames, eventComponentFn, ajaxComponentFn, changeValue);
+
+  const test = new JasmineTestTool(done);
+  createSteps(test, "selectBooleanCheckbox", eventNames, eventComponentFn, ajaxComponentFn, changeValueFn);
+  test.start();
 });
 
-QUnit.test("tc:textarea", function (assert) {
-  let eventNames = ["change", "click", "dblclick", "focus", "blur"];
-  let eventComponentFn = querySelectorFn("#page\\:mainForm\\:textareaevent\\:\\:field");
-  let ajaxComponentFn = querySelectorFn("#page\\:mainForm\\:textareaajax\\:\\:field");
-  let changeValue = function (componentFn) {
+it("tc:textarea", function (done) {
+  const eventNames = ["change", "click", "dblclick", "focus", "blur"];
+  const eventComponentFn = elementByIdFn("page:mainForm:textareaevent::field");
+  const ajaxComponentFn = elementByIdFn("page:mainForm:textareaajax::field");
+
+  const changeValueFn = function (componentFn) {
     let oldValue = componentFn().value;
-    let newValue = "hello";
-    if (oldValue === "hello") {
-      newValue = "hello there!"
+    let newValue = "ping";
+    if (oldValue === "ping") {
+      newValue = "pong"
     }
     componentFn().value = newValue;
   };
-  testEvent(assert, "textarea", eventNames, eventComponentFn, ajaxComponentFn, changeValue);
+
+  const test = new JasmineTestTool(done);
+  createSteps(test, "textarea", eventNames, eventComponentFn, ajaxComponentFn, changeValueFn);
+  test.start();
 });
 
-function testEvent(assert, componentName, eventNames, eventComponentFn, ajaxComponentFn, changeValueFunc) {
+function createSteps(test, componentName, eventNames, eventComponentFn, ajaxComponentFn, changeValueFn) {
+  const actionCountFn = elementByIdFn("page:mainForm:inAction::field");
+  const actionListenerCountFn = elementByIdFn("page:mainForm:inActionListener::field");
+  const ajaxListenerCountFn = elementByIdFn("page:mainForm:inAjaxListener::field");
+  const valueChangeListenerCountFn = elementByIdFn("page:mainForm:inValueChangeListener::field");
+  const timestampFn = elementByIdFn("page:mainForm:inTimestamp::field");
+
+  const rows = querySelectorAllFn("#page\\:mainForm\\:componentTable .tobago-sheet-row");
   let oldActionCount;
   let oldActionListenerCount;
   let oldAjaxListenerCount;
   let oldValueChangeListenerCount;
   let oldTimestamp;
 
-  let TTT = new TobagoTestTool(assert);
   for (let i = 0; i < eventNames.length; i++) {
-    let eventName = eventNames[i];
+    const eventName = eventNames[i];
+    let rowIdPrefix;
 
-    TTT.action(function () {
-      activateComponent(componentName, eventName);
+    const tobagoSheetRow = rows();
+    Array.prototype.forEach.call(tobagoSheetRow, (element, i) => {
+      const tobagoOut = element.querySelector("tobago-out");
+      if (tobagoOut.querySelector(".form-control-plaintext").textContent === componentName) {
+        rowIdPrefix = tobagoOut.id.slice(0, tobagoOut.id.lastIndexOf(":"));
+      }
     });
-    TTT.waitForResponse();
-    TTT.action(function () {
-      oldActionCount = getActionCount();
-      oldActionListenerCount = getActionListenerCount();
-      oldAjaxListenerCount = getAjaxListenerCount();
-      oldValueChangeListenerCount = getValueChangeListenerCount();
-      oldTimestamp = getTimestamp();
 
-      if (changeValueFunc !== null) {
-        changeValueFunc(eventComponentFn);
-      }
-      eventComponentFn().dispatchEvent(new Event(eventName));
-    });
-    TTT.waitForResponse();
-    TTT.asserts(5, function () {
-      assert.equal(getActionCount(), oldActionCount + 1, eventName + " - tc:event - action");
-      assert.equal(getActionListenerCount(), oldActionListenerCount + 1, eventName + " - tc:event - actionListener");
-      assert.equal(getAjaxListenerCount(), oldAjaxListenerCount, eventName + " - tc:event - ajaxListener");
-      if (changeValueFunc !== null) {
-        assert.equal(getValueChangeListenerCount(), oldValueChangeListenerCount + 1,
-            eventName + " - tc:event - valueChangeListener");
-      } else {
-        assert.equal(getValueChangeListenerCount(), oldValueChangeListenerCount,
-            eventName + " - tc:event - valueChangeListener");
-      }
-      assert.ok(getTimestamp() > oldTimestamp, eventName + " - tc:event - timestamp");
-    });
-    TTT.action(function () {
-      oldActionCount = getActionCount();
-      oldActionListenerCount = getActionListenerCount();
-      oldAjaxListenerCount = getAjaxListenerCount();
-      oldValueChangeListenerCount = getValueChangeListenerCount();
-      oldTimestamp = getTimestamp();
+    const selectorButton = elementByIdFn(rowIdPrefix + ":" + eventName + "Behavior");
 
-      if (changeValueFunc !== null) {
-        changeValueFunc(ajaxComponentFn);
-      }
-      ajaxComponentFn().dispatchEvent(new Event(eventName));
-    });
-    TTT.waitForResponse();
-    TTT.asserts(5, function () {
-      assert.equal(getActionCount(), oldActionCount, eventName + " - f:ajax - action");
-      assert.equal(getActionListenerCount(), oldActionListenerCount, eventName + " - f:ajax - actionListener");
-      assert.equal(getAjaxListenerCount(), oldAjaxListenerCount + 1, eventName + " - f:ajax - ajaxListener");
-      if (changeValueFunc !== null) {
-        assert.equal(getValueChangeListenerCount(), oldValueChangeListenerCount + 1,
-            eventName + " - f:ajax - valueChangeListener");
-      } else {
-        assert.equal(getValueChangeListenerCount(), oldValueChangeListenerCount,
-            eventName + " - f:ajax - valueChangeListener");
-      }
-      assert.ok(getTimestamp() > oldTimestamp, eventName + " - f:ajax - timestamp");
-    });
-  }
-  TTT.startTest();
-}
+    test.do(() => oldTimestamp = parseInt(timestampFn().value));
+    test.event("click", selectorButton, () => parseInt(timestampFn().value) > oldTimestamp);
 
-function activateComponent(componentName, eventName) {
-  const rows = querySelectorAllFn("#page\\:mainForm\\:componentTable .tobago-sheet-row")();
-  Array.prototype.forEach.call(rows, function (element, i) {
-    if (element.querySelectorAll("td").item(0).querySelector(".tobago-out").textContent === componentName) {
-      const buttons = element.querySelectorAll("button");
-      Array.prototype.forEach.call(buttons, function (element, i) {
-        let id = element.getAttribute("id");
-        if (id !== undefined && id.indexOf(eventName + "Behavior") >= 0) {
-          element.dispatchEvent(new Event("click", {bubbles: true}));
-        }
-      });
+    test.do(() => oldActionCount = parseInt(actionCountFn().value));
+    test.do(() => oldActionListenerCount = parseInt(actionListenerCountFn().value));
+    test.do(() => oldAjaxListenerCount = parseInt(ajaxListenerCountFn().value));
+    test.do(() => oldValueChangeListenerCount = parseInt(valueChangeListenerCountFn().value));
+    test.do(() => oldTimestamp = parseInt(timestampFn().value));
+    if (changeValueFn !== null) {
+      test.do(() => changeValueFn(eventComponentFn));
     }
-  });
-}
+    test.event(eventName, eventComponentFn, () => parseInt(timestampFn().value) > oldTimestamp);
+    test.do(() => expect(parseInt(actionCountFn().value)).toBe(oldActionCount + 1));
+    test.do(() => expect(parseInt(actionListenerCountFn().value)).toBe(oldActionListenerCount + 1));
+    test.do(() => expect(parseInt(ajaxListenerCountFn().value)).toBe(oldAjaxListenerCount));
+    test.do(() => expect(parseInt(valueChangeListenerCountFn().value))
+        .toBe(changeValueFn !== null ? oldValueChangeListenerCount + 1 : oldValueChangeListenerCount));
+    test.do(() => expect(parseInt(timestampFn().value)).toBeGreaterThan(oldTimestamp));
 
-function getActionCount() {
-  return parseInt(querySelectorFn("#page\\:mainForm\\:inAction\\:\\:field")().value);
+    test.do(() => oldActionCount = parseInt(actionCountFn().value));
+    test.do(() => oldActionListenerCount = parseInt(actionListenerCountFn().value));
+    test.do(() => oldAjaxListenerCount = parseInt(ajaxListenerCountFn().value));
+    test.do(() => oldValueChangeListenerCount = parseInt(valueChangeListenerCountFn().value));
+    test.do(() => oldTimestamp = parseInt(timestampFn().value));
+    if (changeValueFn !== null) {
+      test.do(() => changeValueFn(ajaxComponentFn));
+    }
+    test.event(eventName, ajaxComponentFn, () => parseInt(timestampFn().value) > oldTimestamp);
+    test.do(() => expect(parseInt(actionCountFn().value)).toBe(oldActionCount));
+    test.do(() => expect(parseInt(actionListenerCountFn().value)).toBe(oldActionListenerCount));
+    test.do(() => expect(parseInt(ajaxListenerCountFn().value)).toBe(oldAjaxListenerCount + 1));
+    test.do(() => expect(parseInt(valueChangeListenerCountFn().value))
+        .toBe(changeValueFn !== null ? oldValueChangeListenerCount + 1 : oldValueChangeListenerCount));
+    test.do(() => expect(parseInt(timestampFn().value)).toBeGreaterThan(oldTimestamp));
+  }
 }
-
-function getActionListenerCount() {
-  return parseInt(querySelectorFn("#page\\:mainForm\\:inActionListener\\:\\:field")().value);
-}
-
-function getAjaxListenerCount() {
-  return parseInt(querySelectorFn("#page\\:mainForm\\:inAjaxListener\\:\\:field")().value);
-}
-
-function getValueChangeListenerCount() {
-  return parseInt(querySelectorFn("#page\\:mainForm\\:inValueChangeListener\\:\\:field")().value);
-}
-
-function getTimestamp() {
-  return parseInt(querySelectorFn("#page\\:mainForm\\:inTimestamp\\:\\:field")().value);
-}
-*/
