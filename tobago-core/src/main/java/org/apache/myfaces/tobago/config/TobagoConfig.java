7f3c25d239e84d3ba0945f5447b8a0b4ddbe1cca
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2021-11-18 12:35:59 +0100

    fix: Default position of label should be configurable
    * should be defined in the theme
    
    issue: TOBAGO-2100

--- tobago-core/src/main/java/org/apache/myfaces/tobago/config/TobagoConfig.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/config/TobagoConfig.java
@@ -19,7 +19,6 @@
 
 package org.apache.myfaces.tobago.config;
 
-import org.apache.myfaces.tobago.component.Tags;
 import org.apache.myfaces.tobago.context.Theme;
 import org.apache.myfaces.tobago.context.ThemeImpl;
 import org.apache.myfaces.tobago.exception.TobagoConfigurationException;
@@ -67,7 +66,6 @@ public class TobagoConfig {
   private boolean decodeLineFeed;
   private Map<String, String> mimeTypes;
   private boolean enableTobagoExceptionHandler;
-  private Map<String, String> tagAttributeDefaults;
 
   private boolean locked = false;
 
@@ -115,7 +113,6 @@ public class TobagoConfig {
     contentSecurityPolicy = new ContentSecurityPolicy(ContentSecurityPolicy.Mode.OFF.getValue());
     mimeTypes = new HashMap<>();
     enableTobagoExceptionHandler = true;
-    tagAttributeDefaults = new HashMap<>();
 
     // internal
     final List<TobagoConfigFragment> fragments = new ArrayList<>();
@@ -370,13 +367,5 @@ public class TobagoConfig {
     builder.append(enableTobagoExceptionHandler);
     builder.append('}');
     return builder.toString();
-  }
-
-  public Map<String, String> getTagAttributeDefaults() {
-    return tagAttributeDefaults;
-  }
-
-  public String getTagAttributeDefault(Tags tag, String attribute) {
-    return tagAttributeDefaults.get(tag.name() + ' ' + attribute);
   }
 }
