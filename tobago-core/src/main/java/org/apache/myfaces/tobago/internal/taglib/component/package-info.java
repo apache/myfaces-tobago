1a9ae622a75884299b6c56e7202392139cd7ccef
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2022-02-16 12:50:01 +0100

    build: prepare release

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
@@ -18,7 +18,7 @@
  */
 
 /**
- * <b>Tobago Framework Component Tag Library 2.4.x -
+ * <b>Tobago Framework Component Tag Library 2.4.5 -
  * (C) Copyright 2005-2021 The Apache Software Foundation</b>
  */
 @org.apache.myfaces.tobago.apt.annotation.Taglib(
@@ -28,6 +28,6 @@
         "org.apache.myfaces.tobago.webapp.TobagoServletContextListener",
         "org.apache.myfaces.tobago.webapp.SecretSessionListener"},
     name = "tobago",
-    displayName = "Tobago Components 2.4.x")
+    displayName = "Tobago Components 2.4.5")
 
 package org.apache.myfaces.tobago.internal.taglib.component;
