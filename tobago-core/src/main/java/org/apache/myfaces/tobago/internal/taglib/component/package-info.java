4f4faccc92a6876b63cefc86a2e01629ba58c01a
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2022-12-09 00:25:50 +0100

    chore(release): prepare

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
@@ -18,13 +18,13 @@
  */
 
 /**
- * <b>Tobago Framework Component Tag Library 5.3.1-SNAPSHOT -
+ * <b>Tobago Framework Component Tag Library 5.4.0 -
  * (C) Copyright 2005-2021 The Apache Software Foundation</b>
  */
 @org.apache.myfaces.tobago.apt.annotation.Taglib(
     shortName = "tc",
     uri = "http://myfaces.apache.org/tobago/component",
     name = "tobago",
-    displayName = "Tobago Components 5.3.1-SNAPSHOT")
+    displayName = "Tobago Components 5.4.0")
 
 package org.apache.myfaces.tobago.internal.taglib.component;
