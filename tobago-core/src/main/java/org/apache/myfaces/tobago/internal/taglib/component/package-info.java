c86f4ebb676ed4f9e893efe141a059edaa8b11d4
Udo Schnurpfeil <lofwyr@apache.org>
2023-01-12 14:27:43 +0100

    Tobago 5.x next version (#3658)
    * build(deps): update json5 version to 2.2.3
    
    * build(deps): force update glob-parent version to 6.0.2
    
    * build: set next version to 5.5.0-SNAPSHOT

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
@@ -18,13 +18,13 @@
  */
 
 /**
- * <b>Tobago Framework Component Tag Library 5.4.1-SNAPSHOT -
+ * <b>Tobago Framework Component Tag Library 5.5.0-SNAPSHOT -
  * (C) Copyright 2005-2022 The Apache Software Foundation</b>
  */
 @org.apache.myfaces.tobago.apt.annotation.Taglib(
     shortName = "tc",
     uri = "http://myfaces.apache.org/tobago/component",
     name = "tobago",
-    displayName = "Tobago Components 5.4.1-SNAPSHOT")
+    displayName = "Tobago Components 5.5.0-SNAPSHOT")
 
 package org.apache.myfaces.tobago.internal.taglib.component;
