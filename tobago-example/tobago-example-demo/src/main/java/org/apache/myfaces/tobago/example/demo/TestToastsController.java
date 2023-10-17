d3347dedd0fd5981d4cf3c416da251454491ec57
Henning Noeth <hnoeth@apache.org>
2023-10-17 13:23:23 +0200

    fix(toast): update toast content
    * fix content update of headless toasts
    * add test
    
    Issue: TOBAGO-2231

---
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/TestToastsController.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.myfaces.tobago.example.demo;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import org.apache.myfaces.tobago.application.Toast;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Named
+@SessionScoped
+public class TestToastsController implements Serializable {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  private final List<Toast> headerlessToasts = new ArrayList<>();
+
+  public List<Toast> getHeaderlessToasts() {
+    return headerlessToasts;
+  }
+
+  public void createHeaderlessToast() {
+    headerlessToasts.add(new Toast() {
+      private final String id = UUID.randomUUID().toString();
+
+      @Override
+      public String getId() {
+        return id;
+      }
+    });
+  }
+
+  public void resetHeaderlessToasts() {
+    headerlessToasts.clear();
+  }
+}
