35d53c8a8ffd99872fc2bd780966f1aeffa9a736
Henning Noeth <hnoeth@apache.org>
2023-11-14 10:53:23 +0100

    build(deps-dev): typescript-eslint 6.11.0
    * update typescript-eslint to 6.11.0
    * update NodeJs to 20.9.0
    * use recommended eslint config
    * adjust eslint rules to let current code compile

--- tobago-theme/.eslintrc.js
+++ tobago-theme/.eslintrc.js
@@ -19,9 +19,9 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/stylistic'
   ],
   parserOptions: {
     project: './tsconfig.json',
@@ -29,58 +29,24 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "prefer-const": "warn",
-    "prefer-rest-params": "warn",
-    "no-case-declarations": "warn",
-    "no-irregular-whitespace": "warn",
-    "no-constant-condition": "warn",
-    "no-inner-declarations": "warn",
-    "@typescript-eslint/ban-ts-comment": "error",
-    "@typescript-eslint/no-implied-eval": "error",
-    "@typescript-eslint/unbound-method": "warn",
-    "@typescript-eslint/no-unsafe-call": "warn",
-    "@typescript-eslint/no-unsafe-assignment": "warn",
-    "@typescript-eslint/no-unsafe-member-access": "warn",
-    "@typescript-eslint/restrict-template-expressions": "warn",
+    "@typescript-eslint/array-type": "warn",
+    "@typescript-eslint/consistent-indexed-object-style": "warn",
+    "@typescript-eslint/consistent-type-assertions": "warn",
+    "@typescript-eslint/consistent-type-definitions": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-unused-vars": "off",
-    "indent": "off",
-    "@typescript-eslint/indent": [
-      "off"
-    ],
-    "@typescript-eslint/member-delimiter-style": [
-      "warn",
-      {
-        "multiline": {
-          "delimiter": "semi",
-          "requireLast": true
-        },
-        "singleline": {
-          "delimiter": "semi",
-          "requireLast": false
-        }
-      }
-    ],
-    "@typescript-eslint/member-ordering": "warn",
-    "@typescript-eslint/no-unnecessary-type-assertion": ["warn"],
-    "@typescript-eslint/no-empty-function": "warn",
-    "@typescript-eslint/no-inferrable-types": "warn",
-    "@typescript-eslint/restrict-plus-operands": "error",
-    "@typescript-eslint/no-unsafe-return": "warn",
-    "@typescript-eslint/quotes": [
-      "error",
-      "double"
-    ],
-    "@typescript-eslint/semi": [
-      "error",
-      "always"
-    ],
+    "@typescript-eslint/prefer-for-of": "warn",
     "max-len": [
       "error",
       {
         "code": 120
       }
     ],
+    "no-constant-condition": "warn",
+    "no-inner-declarations": "warn",
+    "no-irregular-whitespace": "warn",
     "no-multiple-empty-lines": "error",
-    "no-var": "warn"
+    "prefer-const": "warn",
+    "prefer-rest-params": "warn"
   }
 }
