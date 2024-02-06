ef9b65a584f6037bb45190e4e52ff25b139b0168
Henning Noeth <hnoeth@apache.org>
2024-02-06 08:34:44 +0100

    chore: update to minor version
    Update to minor version because of:
    * https://issues.apache.org/jira/browse/TOBAGO-2284

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
@@ -25,6 +25,6 @@
     shortName = "tc",
     uri = "http://myfaces.apache.org/tobago/component",
     name = "tobago",
-    displayName = "Tobago Components 5.10.1-SNAPSHOT")
+    displayName = "Tobago Components 5.11.0-SNAPSHOT")
 
 package org.apache.myfaces.tobago.internal.taglib.component;
