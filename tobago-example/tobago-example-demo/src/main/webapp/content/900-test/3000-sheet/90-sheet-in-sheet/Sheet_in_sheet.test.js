8ece2f94b0d97602af98cc7efc43516f967f356c
Henning Noeth <hnoeth@apache.org>
2024-06-24 13:15:11 +0200

    fix(columnSelector): columnPanel/sheet-in-sheet
    * add HTML name attribute for column selector in the header for better identification
    * refactor TypeScript
    * add many tests
    
    Issue: TOBAGO-2327
    Issue: TOBAGO-2328
    Issue: TOBAGO-2329

--- tobago-example/tobago-example-demo/src/main/webapp/content/900-test/3000-sheet/90-sheet-in-sheet/Sheet_in_sheet.test.js
+++ tobago-example/tobago-example-demo/src/main/webapp/content/900-test/3000-sheet/90-sheet-in-sheet/Sheet_in_sheet.test.js
@@ -15,9 +15,71 @@
  * limitations under the License.
  */
 
-import {querySelectorAllFn} from "/script/tobago-test.js";
+import {elementByIdFn, querySelectorAllFn, querySelectorFn} from "/script/tobago-test.js";
+import {JasmineTestTool} from "/tobago/test/tobago-test-tool.js";
 
 it("there must be no 'tobago-auto-spacing'", function () {
   const sheetAutoSpacings = querySelectorAllFn("tobago-sheet .tobago-auto-spacing")
   expect(sheetAutoSpacings().length).toBe(0);
 });
+
+it("main sheet: select all; sun sub sheet: select Mercury; venus sub sheet: select earth", function (done) {
+  const reloadPage = querySelectorFn("page:mainForm:reloadPage");
+  const selectAllCheckbox = querySelectorFn("input[name='page:mainForm:mainSheet::columnSelector']");
+  const sunSheetMercuryRow = querySelectorFn("#page\\:mainForm\\:mainSheet\\:0\\:subSheet tr[row-index='1']");
+  const venusSheetEarthRow = querySelectorFn("#page\\:mainForm\\:mainSheet\\:2\\:subSheet tr[row-index='3']");
+
+  const test = new JasmineTestTool(done);
+  test.setup(() => getMainSheetSelection().size === 0
+          && getSunSubSheetSelection().size === 0
+          && getVenusSubSheetSelection().size === 0,
+      null, "click", reloadPage);
+
+  test.do(() => expect(getMainSheetSelection().size).toBe(0));
+  test.do(() => expect(getSunSubSheetSelection().size).toBe(0));
+  test.do(() => expect(getVenusSubSheetSelection().size).toBe(0));
+
+  test.event("click", selectAllCheckbox, () => getMainSheetSelection().size === 5);
+  test.do(() => expect(getMainSheetSelection().has(0)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(1)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(2)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(3)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(4)).toBe(true));
+  test.do(() => expect(getSunSubSheetSelection().size).toBe(0));
+  test.do(() => expect(getVenusSubSheetSelection().size).toBe(0));
+
+  test.event("click", sunSheetMercuryRow, () => getSunSubSheetSelection().has(1));
+  test.do(() => expect(getMainSheetSelection().has(0)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(1)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(2)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(3)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(4)).toBe(true));
+  test.do(() => expect(getSunSubSheetSelection().has(1)).toBe(true));
+  test.do(() => expect(getVenusSubSheetSelection().size).toBe(0));
+
+  test.event("click", venusSheetEarthRow, () => getVenusSubSheetSelection().has(3));
+  test.do(() => expect(getMainSheetSelection().has(0)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(1)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(2)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(3)).toBe(true));
+  test.do(() => expect(getMainSheetSelection().has(4)).toBe(true));
+  test.do(() => expect(getSunSubSheetSelection().has(1)).toBe(true));
+  test.do(() => expect(getVenusSubSheetSelection().has(3)).toBe(true));
+
+  test.start();
+});
+
+function getMainSheetSelection() {
+  const hiddenSelectedField = elementByIdFn("page:mainForm:mainSheet::selected");
+  return new Set(JSON.parse(hiddenSelectedField().value));
+}
+
+function getSunSubSheetSelection() {
+  const hiddenSelectedField = elementByIdFn("page:mainForm:mainSheet:0:subSheet::selected");
+  return new Set(JSON.parse(hiddenSelectedField().value));
+}
+
+function getVenusSubSheetSelection() {
+  const hiddenSelectedField = elementByIdFn("page:mainForm:mainSheet:2:subSheet::selected");
+  return new Set(JSON.parse(hiddenSelectedField().value));
+}
