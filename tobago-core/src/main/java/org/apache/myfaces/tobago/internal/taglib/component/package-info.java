95c45bd68884ed9cdb1ac16b8d8df95efdf87758
Henning Noeth <hnoeth@apache.org>
2024-09-24 13:26:47 +0200

    refactor: update to minor version

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
@@ -25,6 +25,6 @@
     shortName = "tc",
     uri = "http://myfaces.apache.org/tobago/component",
     name = "tobago",
-    displayName = "Tobago Components 5.13.1-SNAPSHOT")
+    displayName = "Tobago Components 5.14.0-SNAPSHOT")
 
 package org.apache.myfaces.tobago.internal.taglib.component;
