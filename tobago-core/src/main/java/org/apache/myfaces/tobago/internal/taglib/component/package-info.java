8b4ec2b29ed969f30fb82cd563a8c963bd99346a
Henning Noeth <hnoeth@apache.org>
2024-11-18 12:04:33 +0100

    chore: update to minor version

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
@@ -18,13 +18,13 @@
  */
 
 /**
- * <b>Tobago Framework Component Tag Library 6.5.0-SNAPSHOT -
+ * <b>Tobago Framework Component Tag Library 6.7.0-SNAPSHOT -
  * (C) Copyright 2005-2024 The Apache Software Foundation</b>
  */
 @org.apache.myfaces.tobago.apt.annotation.Taglib(
     shortName = "tc",
     uri = "http://myfaces.apache.org/tobago/component",
     name = "tobago",
-    displayName = "Tobago Components 6.6.1-SNAPSHOT")
+    displayName = "Tobago Components 6.7.0-SNAPSHOT")
 
 package org.apache.myfaces.tobago.internal.taglib.component;
