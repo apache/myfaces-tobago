6f1b67ff03a6eca9486949e476872591c48f0a6f
Henning Noeth <hnoeth@apache.org>
2025-04-14 10:31:13 +0200

    fix(selectOneList): deselection
    * add test
    * deselection is now possible like it is for the selectOneListbox component
    * adjust unit test
    
    Issue: TOBAGO-2403

---
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/test/onelist/DeselectController.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.myfaces.tobago.example.demo.test.onelist;
+
+import org.apache.myfaces.tobago.example.demo.AstroData;
+import org.apache.myfaces.tobago.example.demo.SolarObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
+import java.util.Date;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class DeselectController implements Serializable {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  @Inject
+  private AstroData astroData;
+
+  private List<SolarObject> planets;
+  private SolarObject selectedPlanet;
+  private boolean required = false;
+  private boolean expanded = true;
+
+  @PostConstruct
+  public void init() {
+    planets = astroData.getSatellites("Sun");
+  }
+
+  public void reset() {
+    selectedPlanet = null;
+  }
+
+  public List<SolarObject> getPlanets() {
+    return planets;
+  }
+
+  public SolarObject getSelectedPlanet() {
+    return selectedPlanet;
+  }
+
+  public void setSelectedPlanet(SolarObject selectedPlanet) {
+    this.selectedPlanet = selectedPlanet;
+  }
+
+  public boolean isRequired() {
+    return required;
+  }
+
+  public void setRequired(boolean required) {
+    this.required = required;
+  }
+
+  public boolean isExpanded() {
+    return expanded;
+  }
+
+  public void setExpanded(boolean expanded) {
+    this.expanded = expanded;
+  }
+
+  public long getCurrentTimestamp() {
+    return new Date().getTime();
+  }
+}
