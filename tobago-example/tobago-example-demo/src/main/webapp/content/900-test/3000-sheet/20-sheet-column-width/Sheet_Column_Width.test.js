19871e8cfff286876280bba0b4c7d0a8d9943177
Henning Noeth <hnoeth@apache.org>
2025-04-17 09:51:27 +0200

    fix(sheet): column width pixel or percent only
    * fix wrong column width if only pixel value are used
    * add tests
    * adjust "horizontal scrollbar" test
    * adjust SheetRendererUnitTest
    
    Issue: TOBAGO-2405

--- tobago-example/tobago-example-demo/src/main/webapp/content/900-test/3000-sheet/20-sheet-column-width/Sheet_Column_Width.test.js
+++ tobago-example/tobago-example-demo/src/main/webapp/content/900-test/3000-sheet/20-sheet-column-width/Sheet_Column_Width.test.js
@@ -15,11 +15,41 @@
  * limitations under the License.
  */
 
-import {querySelectorFn} from "/script/tobago-test.js";
+import {querySelectorAllFn, querySelectorFn} from "/script/tobago-test.js";
 import {JasmineTestTool} from "/tobago/test/tobago-test-tool.js";
 
+it("Only pixel values set", function (done) {
+  const columnHeads = querySelectorAllFn("#page\\:mainForm\\:pixelOnly header table thead tr th");
+
+  const test = new JasmineTestTool(done);
+  test.do(() => expect(getComputedStyle(columnHeads()[0]).width).toBe("10px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[1]).width).toBe("20px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[2]).width).toBe("30px"));
+  test.start();
+});
+
+it("Only percent values set for a 1000px-sheet", function (done) {
+  const columnHeads = querySelectorAllFn("#page\\:mainForm\\:percentOnly header table thead tr th");
+
+  const test = new JasmineTestTool(done);
+  test.do(() => expect(getComputedStyle(columnHeads()[0]).width).toBe("100px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[1]).width).toBe("200px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[2]).width).toBe("300px"));
+  test.start();
+});
+
+it("Only auto value set for a 900px-sheet", function (done) {
+  const columnHeads = querySelectorAllFn("#page\\:mainForm\\:autoOnly header table thead tr th");
+
+  const test = new JasmineTestTool(done);
+  test.do(() => expect(getComputedStyle(columnHeads()[0]).width).toBe("300px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[1]).width).toBe("300px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[2]).width).toBe("300px"));
+  test.start();
+});
+
 it("There must be no horizontal scrollbar", function (done) {
-  const sheetBody = querySelectorFn("#page\\:testSheetColumnWidth .tobago-body");
+  const sheetBody = querySelectorFn("#page\\:mainForm\\:testSheetColumnWidth .tobago-body");
 
   const test = new JasmineTestTool(done);
   test.do(() => expect(sheetBody().clientWidth).toBeGreaterThanOrEqual(sheetBody().scrollWidth));
