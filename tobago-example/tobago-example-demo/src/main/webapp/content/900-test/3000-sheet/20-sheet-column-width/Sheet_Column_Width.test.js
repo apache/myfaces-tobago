6bd38aa0475c716025ca5e44f9a0f0494902473d
Henning Noeth <hnoeth@apache.org>
2025-04-17 09:51:27 +0200

    fix(sheet): column width index
    * add test
    * replace "sheetController" with "columnWidthController"
    * refactor test controller java classes for sheet
    * fix wrong column width (because of wrong index) if a previous column has rendered=false set
    
    Issue: TOBAGO-2404

--- tobago-example/tobago-example-demo/src/main/webapp/content/900-test/3000-sheet/20-sheet-column-width/Sheet_Column_Width.test.js
+++ tobago-example/tobago-example-demo/src/main/webapp/content/900-test/3000-sheet/20-sheet-column-width/Sheet_Column_Width.test.js
@@ -15,8 +15,53 @@
  * limitations under the License.
  */
 
-import {querySelectorAllFn, querySelectorFn} from "/script/tobago-test.js";
+import {elementByIdFn, querySelectorAllFn, querySelectorFn} from "/script/tobago-test.js";
 import {JasmineTestTool} from "/tobago/test/tobago-test-tool.js";
+
+it("Column width index", function (done) {
+  const hideButton = elementByIdFn("page:mainForm:hideColumn");
+  const showColumn = elementByIdFn("page:mainForm:showColumn");
+  const columnHeads = querySelectorAllFn("#page\\:mainForm\\:columnRenderedFalse header table thead tr th");
+
+  const test = new JasmineTestTool(done);
+  test.setup(() => columnHeads().length === 5, null, "click", showColumn);
+  test.do(() => expect(columnHeads().length).toBe(5));
+  test.do(() => expect(getComputedStyle(columnHeads()[0]).width).toBe("100px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[1]).width).toBe("200px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[2]).width).toBe("300px"));
+
+  test.event("click", hideButton, () => columnHeads().length === 4);
+  test.do(() => expect(columnHeads().length).toBe(4));
+  test.do(() => expect(getComputedStyle(columnHeads()[0]).width).toBe("100px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[1]).width).toBe("300px"));
+
+  test.do(() => simulatingResize(columnHeads()[0], +5));
+  test.do(() => simulatingResize(columnHeads()[1], +5));
+  test.do(() => expect(columnHeads().length).toBe(4));
+  test.do(() => expect(getComputedStyle(columnHeads()[0]).width).toBe("105px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[1]).width).toBe("305px"));
+
+  test.event("click", showColumn, () => columnHeads().length === 5);
+  test.do(() => expect(columnHeads().length).toBe(5));
+  test.do(() => expect(getComputedStyle(columnHeads()[0]).width).toBe("105px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[1]).width).toBe("200px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[2]).width).toBe("305px"));
+
+  test.do(() => simulatingResize(columnHeads()[0], +10));
+  test.do(() => simulatingResize(columnHeads()[1], +10));
+  test.do(() => simulatingResize(columnHeads()[2], +10));
+  test.do(() => expect(columnHeads().length).toBe(5));
+  test.do(() => expect(getComputedStyle(columnHeads()[0]).width).toBe("115px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[1]).width).toBe("210px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[2]).width).toBe("315px"));
+
+  test.event("click", hideButton, () => columnHeads().length === 4);
+  test.do(() => expect(columnHeads().length).toBe(4));
+  test.do(() => expect(getComputedStyle(columnHeads()[0]).width).toBe("115px"));
+  test.do(() => expect(getComputedStyle(columnHeads()[1]).width).toBe("315px"));
+
+  test.start();
+});
 
 it("Only pixel values set", function (done) {
   const columnHeads = querySelectorAllFn("#page\\:mainForm\\:pixelOnly header table thead tr th");
@@ -55,3 +100,23 @@ it("There must be no horizontal scrollbar", function (done) {
   test.do(() => expect(sheetBody().clientWidth).toBeGreaterThanOrEqual(sheetBody().scrollWidth));
   test.start();
 });
+
+function simulatingResize(columnHead, movePx) {
+  const resizeElement = columnHead.querySelector(".tobago-resize");
+  const headerCols = columnHead.closest("table").querySelectorAll("col")
+  const columnIndex = parseInt(resizeElement.dataset.tobagoColumnIndex);
+  const headerColumn = headerCols.item(columnIndex);
+  const startClientX = resizeElement.getBoundingClientRect().x;
+  resizeElement.dispatchEvent(new MouseEvent("mousedown", {
+    columnIndex: columnIndex,
+    clientX: startClientX,
+    originalHeaderColumnWidth: parseInt(getComputedStyle(headerColumn).width),
+  }));
+
+  resizeElement.dispatchEvent(new MouseEvent("mousemove", {
+    bubbles: true,
+    clientX: startClientX + movePx
+  }));
+
+  resizeElement.dispatchEvent(new MouseEvent("mouseup", {bubbles: true}));
+}
