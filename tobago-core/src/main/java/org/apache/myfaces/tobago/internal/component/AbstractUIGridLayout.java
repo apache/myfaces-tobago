5bac9fa10396d82ed3abe363e8080427a9988c23
Henning Noeth <hnoeth@apache.org>
2025-05-30 10:54:33 +0200

    fix(gridLayout): rendered=false
    Components keep their space, even if rendered=false is set.
    
    * add a test
    * if style component is set for grid layout children, check if it is rendered
    
    Issue: TOBAGO-2423

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIGridLayout.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/component/AbstractUIGridLayout.java
@@ -19,6 +19,14 @@
 
 package org.apache.myfaces.tobago.internal.component;
 
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.component.UIPanel;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.AbortProcessingException;
+import jakarta.faces.event.ComponentSystemEvent;
+import jakarta.faces.event.ComponentSystemEventListener;
+import jakarta.faces.event.ListenerFor;
+import jakarta.faces.event.PreRenderComponentEvent;
 import org.apache.myfaces.tobago.apt.annotation.Preliminary;
 import org.apache.myfaces.tobago.component.Attributes;
 import org.apache.myfaces.tobago.component.LabelLayout;
@@ -30,15 +38,6 @@ import org.apache.myfaces.tobago.layout.MeasureList;
 import org.apache.myfaces.tobago.util.ComponentUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import jakarta.faces.component.UIComponent;
-import jakarta.faces.component.UIPanel;
-import jakarta.faces.context.FacesContext;
-import jakarta.faces.event.AbortProcessingException;
-import jakarta.faces.event.ComponentSystemEvent;
-import jakarta.faces.event.ComponentSystemEventListener;
-import jakarta.faces.event.ListenerFor;
-import jakarta.faces.event.PreRenderComponentEvent;
 
 import java.lang.invoke.MethodHandles;
 import java.util.List;
@@ -104,31 +103,33 @@ public abstract class AbstractUIGridLayout extends AbstractUILayoutBase implemen
 
     // #1 put all components with "gridRow" and "gridColumn" set into the grid cells
     for (final UIComponent component : components) {
-      final Map<String, Object> attributes = component.getAttributes();
-      final Integer gridColumn = (Integer) attributes.get(Attributes.gridColumn.getName());
-      final Integer gridRow = (Integer) attributes.get(Attributes.gridRow.getName());
-      if (gridColumn != null && gridRow != null) {
-        if (gridColumn > columnsCount) {
-          // ignore wrong columns
-          LOG.warn("gridColumn {} > columnsCount {} in component '{}'!", gridColumn, columnsCount,
-              component.getClientId(facesContext));
-        } else {
-          if (gridRow > cells.length) {
-            if (LOG.isDebugEnabled()) {
-              LOG.debug("expanding, because gridRow {} > rowCount {} in component '{}'!", gridRow, cells.length,
-                  component.getClientId(facesContext));
+      if (component.isRendered()) {
+        final Map<String, Object> attributes = component.getAttributes();
+        final Integer gridColumn = (Integer) attributes.get(Attributes.gridColumn.getName());
+        final Integer gridRow = (Integer) attributes.get(Attributes.gridRow.getName());
+        if (gridColumn != null && gridRow != null) {
+          if (gridColumn > columnsCount) {
+            // ignore wrong columns
+            LOG.warn("gridColumn {} > columnsCount {} in component '{}'!", gridColumn, columnsCount,
+                component.getClientId(facesContext));
+          } else {
+            if (gridRow > cells.length) {
+              if (LOG.isDebugEnabled()) {
+                LOG.debug("expanding, because gridRow {} > rowCount {} in component '{}'!", gridRow, cells.length,
+                    component.getClientId(facesContext));
+              }
+              // ensure enough rows
+              cells = expand(cells, gridRow);
             }
-            // ensure enough rows
-            cells = expand(cells, gridRow);
+            cells = set(cells, gridColumn - 1, gridRow - 1, component);
           }
-          cells = set(cells, gridColumn - 1, gridRow - 1, component);
+        } else if (gridColumn != null) {
+          LOG.warn("gridColumn is set to {}, but gridRow not in component '{}'!", gridColumn,
+              component.getClientId(facesContext));
+        } else if (gridRow != null) {
+          LOG.warn("gridRow is set to {}, but gridColumn not in component '{}'!", gridRow,
+              component.getClientId(facesContext));
         }
-      } else if (gridColumn != null) {
-        LOG.warn("gridColumn is set to {}, but gridRow not in component '{}'!", gridColumn,
-            component.getClientId(facesContext));
-      } else if (gridRow != null) {
-        LOG.warn("gridRow is set to {}, but gridColumn not in component '{}'!", gridRow,
-            component.getClientId(facesContext));
       }
     }
 
@@ -136,74 +137,78 @@ public abstract class AbstractUIGridLayout extends AbstractUILayoutBase implemen
     int j = 0;
     int i = 0;
     for (final UIComponent component : components) {
-      final Map<String, Object> attributes = component.getAttributes();
-      final Integer gridColumn = (Integer) attributes.get(Attributes.gridColumn.getName());
-      final Integer gridRow = (Integer) attributes.get(Attributes.gridRow.getName());
-      // find a component without a position
-      // if only one value is defined, treat as undefined
-      if (gridColumn == null || gridRow == null) {
-        // find next free cell
-        while (cells[j][i] != null) {
-          i++;
-          if (i >= columnsCount) {
-            i = 0;
-            j++;
+      if (component.isRendered()) {
+        final Map<String, Object> attributes = component.getAttributes();
+        final Integer gridColumn = (Integer) attributes.get(Attributes.gridColumn.getName());
+        final Integer gridRow = (Integer) attributes.get(Attributes.gridRow.getName());
+        // find a component without a position
+        // if only one value is defined, treat as undefined
+        if (gridColumn == null || gridRow == null) {
+          // find next free cell
+          while (cells[j][i] != null) {
+            i++;
+            if (i >= columnsCount) {
+              i = 0;
+              j++;
+            }
+            if (j >= cells.length) {
+              cells = expand(cells, j + STEP);
+            }
           }
-          if (j >= cells.length) {
-            cells = expand(cells, j + STEP);
-          }
+          cells = set(cells, i, j, component);
         }
-        cells = set(cells, i, j, component);
       }
     }
 
     // #3 create UIStyle children. TODO: There might be a better way...
     for (final UIComponent component : components) {
-      final Map<String, Object> attributes = component.getAttributes();
+      if (component.isRendered()) {
+        final Map<String, Object> attributes = component.getAttributes();
 
-      final int gridColumn = (Integer) attributes.get(Attributes.gridColumn.getName());
-      final Integer columnSpan = (Integer) attributes.get(Attributes.columnSpan.getName());
-      final int gridRow = (Integer) attributes.get(Attributes.gridRow.getName());
-      final Integer rowSpan = (Integer) attributes.get(Attributes.rowSpan.getName());
-      final boolean labeledLeft = LabelLayout.isGridLeft(component);
-      final boolean labeledRight = LabelLayout.isGridRight(component);
-      final boolean labeledHorizontal = labeledLeft || labeledRight;
-      final boolean labeledTop = LabelLayout.isGridTop(component);
-      final boolean labeledBottom = LabelLayout.isGridBottom(component);
-      final boolean labeledVertical = labeledTop || labeledBottom;
-      final boolean labeled = labeledHorizontal || labeledVertical;
+        final int gridColumn = (Integer) attributes.get(Attributes.gridColumn.getName());
+        final Integer columnSpan = (Integer) attributes.get(Attributes.columnSpan.getName());
+        final int gridRow = (Integer) attributes.get(Attributes.gridRow.getName());
+        final Integer rowSpan = (Integer) attributes.get(Attributes.rowSpan.getName());
+        final boolean labeledLeft = LabelLayout.isGridLeft(component);
+        final boolean labeledRight = LabelLayout.isGridRight(component);
+        final boolean labeledHorizontal = labeledLeft || labeledRight;
+        final boolean labeledTop = LabelLayout.isGridTop(component);
+        final boolean labeledBottom = LabelLayout.isGridBottom(component);
+        final boolean labeledVertical = labeledTop || labeledBottom;
+        final boolean labeled = labeledHorizontal || labeledVertical;
 
-      // field style
+        // field style
 
-      AbstractUIStyle fieldStyle = ComponentUtils.findChild(component, AbstractUIStyle.class);
-      if (fieldStyle == null) {
-        fieldStyle = (AbstractUIStyle) ComponentUtils.createComponent(
-            facesContext, Tags.style.componentType(), RendererTypes.Style, null);
-        component.getChildren().add(fieldStyle);
+        AbstractUIStyle fieldStyle = ComponentUtils.findChild(component, AbstractUIStyle.class);
+        if (fieldStyle == null) {
+          fieldStyle = (AbstractUIStyle) ComponentUtils.createComponent(
+              facesContext, Tags.style.componentType(), RendererTypes.Style, null);
+          component.getChildren().add(fieldStyle);
+        }
+        // Style must be transient to avoid creating a new instance of GridSpan while restore state
+        // https://issues.apache.org/jira/browse/TOBAGO-1909
+        fieldStyle.setTransient(true);
+
+        fieldStyle.setGridColumn(
+            GridSpan.valueOf(labeledLeft ? gridColumn + 1 : gridColumn,
+                labeledHorizontal ? columnSpan - 1 : columnSpan));
+        fieldStyle.setGridRow(
+            GridSpan.valueOf(labeledTop ? gridRow + 1 : gridRow, labeledVertical ? rowSpan - 1 : rowSpan));
+
+        // label style
+
+        if (labeled) {
+          final AbstractUIStyle labelStyle = (AbstractUIStyle) ComponentUtils.createComponent(
+              facesContext, Tags.style.componentType(), RendererTypes.Style, null);
+          component.getChildren().add(labelStyle);
+          labelStyle.setTransient(true);
+          labelStyle.setSelector(StyleRenderUtils.encodeIdSelector(
+              component.getClientId(facesContext) + ComponentUtils.SUB_SEPARATOR + "label"));
+
+          labelStyle.setGridColumn(GridSpan.valueOf(labeledRight ? gridColumn + columnSpan - 1 : gridColumn, null));
+          labelStyle.setGridRow(GridSpan.valueOf(labeledBottom ? gridRow + rowSpan - 1 : gridRow, null));
+        }
       }
-      // Style must be transient to avoid creating a new instance of GridSpan while restore state
-      // https://issues.apache.org/jira/browse/TOBAGO-1909
-      fieldStyle.setTransient(true);
-
-      fieldStyle.setGridColumn(
-          GridSpan.valueOf(labeledLeft ? gridColumn + 1 : gridColumn, labeledHorizontal ? columnSpan - 1 : columnSpan));
-      fieldStyle.setGridRow(
-          GridSpan.valueOf(labeledTop ? gridRow + 1 : gridRow, labeledVertical ? rowSpan - 1 : rowSpan));
-
-      // label style
-
-      if (labeled) {
-        final AbstractUIStyle labelStyle = (AbstractUIStyle) ComponentUtils.createComponent(
-            facesContext, Tags.style.componentType(), RendererTypes.Style, null);
-        component.getChildren().add(labelStyle);
-        labelStyle.setTransient(true);
-        labelStyle.setSelector(StyleRenderUtils.encodeIdSelector(
-            component.getClientId(facesContext) + ComponentUtils.SUB_SEPARATOR + "label"));
-
-        labelStyle.setGridColumn(GridSpan.valueOf(labeledRight ? gridColumn + columnSpan - 1 : gridColumn, null));
-        labelStyle.setGridRow(GridSpan.valueOf(labeledBottom ? gridRow + rowSpan - 1 : gridRow, null));
-      }
-
     }
 
     return cells;
