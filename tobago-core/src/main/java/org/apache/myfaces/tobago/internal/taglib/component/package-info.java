84d3bc42c3abe2ba319aabd60c9f4f6e2a8deb6b
Henning Noeth <hnoeth@apache.org>
2025-06-11 11:16:32 +0200

    chore(release): prepare

--- tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/internal/taglib/component/package-info.java
@@ -18,13 +18,13 @@
  */
 
 /**
- * <b>Tobago Framework Component Tag Library 6.8.0-SNAPSHOT -
+ * <b>Tobago Framework Component Tag Library 6.8.0 -
  * (C) Copyright 2005-2025 The Apache Software Foundation</b>
  */
 @org.apache.myfaces.tobago.apt.annotation.Taglib(
     shortName = "tc",
     uri = "http://myfaces.apache.org/tobago/component",
     name = "tobago",
-    displayName = "Tobago Components 6.8.0-SNAPSHOT")
+    displayName = "Tobago Components 6.8.0")
 
 package org.apache.myfaces.tobago.internal.taglib.component;
--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/Release.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/Release.java
@@ -184,8 +184,8 @@ public enum Release {
   v6_6_0("12355068"),
   v6_7_0("12355193"),
   v6_7_1("12355672"),
-  v6_7_2("12355753", true),
-  v6_8_0("12355944", false, true);
+  v6_7_2("12355753"),
+  v6_8_0("12355944", true);
 
   private final String jira;
   private final String version;
