4f98f8a7c7bf36423aec3796f7ebc560cbe888d0
Henning Noeth <hnoeth@apache.org>
2025-08-12 18:19:56 +0200

    fix(checkboxes): disabled
    Checkboxes of tc:columnSelector now look like the checkboxes of tc:selectBooleanCheckbox and tc:selectManyCheckbox.
    Radio buttons of tc:columnSelect now look like the radio buttons of tc:selectOneRadio.
    Improve positioning of tc:columnSelector checkboxes in table cell. The "middle" position is now set via CSS "vertical-align" instead of calculate the padding. This should be more reliable.
    To make disabled and unchecked checkboxes, radio button and toggles more readable, the background is greyed out.
    Now checkboxes are the same between different components.
    Add a test.
    
    Issue: TOBAGO-2433

---
+++ tobago-example/tobago-example-demo/src/main/webapp/content/900-test/2001-checkboxes/Checkboxes.test.js
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {elementByIdFn} from "/script/tobago-test.js";
+import {querySelectorFn} from "../../../script/tobago-test";
+
+it("Required fields", function () {
+  testFormChecks(
+      elementByIdFn("page:mainForm:selectBooleanCheckbox::field"),
+      elementByIdFn("page:mainForm:selectBooleanCheckboxChecked::field"),
+      elementByIdFn("page:mainForm:selectBooleanCheckboxDisabled::field"),
+      elementByIdFn("page:mainForm:selectBooleanCheckboxCheckedDisabled::field")
+  );
+
+  testFormChecks(
+      elementByIdFn("page:mainForm:selectBooleanToggle::field"),
+      elementByIdFn("page:mainForm:selectBooleanToggleChecked::field"),
+      elementByIdFn("page:mainForm:selectBooleanToggleDisabled::field"),
+      elementByIdFn("page:mainForm:selectBooleanToggleCheckedDisabled::field")
+  );
+
+  testFormChecks(
+      elementByIdFn("page:mainForm:selectOneRadio::0"),
+      elementByIdFn("page:mainForm:selectOneRadio::1"),
+      elementByIdFn("page:mainForm:selectOneRadioDisabled::0"),
+      elementByIdFn("page:mainForm:selectOneRadioDisabled::1")
+  );
+
+  testFormChecks(
+      elementByIdFn("page:mainForm:selectManyCheckbox::0"),
+      elementByIdFn("page:mainForm:selectManyCheckbox::1"),
+      elementByIdFn("page:mainForm:selectManyCheckbox::2"),
+      elementByIdFn("page:mainForm:selectManyCheckbox::3")
+  );
+
+  testFormChecks(
+      querySelectorFn("input[name='page:mainForm:sheetMulti_data_row_selector_0']"),
+      querySelectorFn("input[name='page:mainForm:sheetMulti_data_row_selector_1']"),
+      querySelectorFn("input[name='page:mainForm:sheetMulti_data_row_selector_2']"),
+      querySelectorFn("input[name='page:mainForm:sheetMulti_data_row_selector_3']"),
+  );
+
+  testFormChecks(
+      querySelectorFn("input[name='page:mainForm:sheetSingle_data_row_selector_0']"),
+      querySelectorFn("input[name='page:mainForm:sheetSingle_data_row_selector_1']"),
+      querySelectorFn("input[name='page:mainForm:sheetSingle_data_row_selector_2']"),
+      querySelectorFn("input[name='page:mainForm:sheetSingle_data_row_selector_3']"),
+  );
+});
+
+function testFormChecks(uncheckedEnabled, checkedEnabled, uncheckedDisabled, checkedDisabled) {
+  const white = "rgb(255, 255, 255)";
+  const blue = "rgb(13, 110, 253)";
+  const gray = "rgb(222, 226, 230)";
+  const grayTransparent = "rgba(222, 226, 230, 0.5)";
+
+  expect(window.getComputedStyle(uncheckedEnabled()).backgroundColor).toContain(white);
+  expect(window.getComputedStyle(uncheckedEnabled()).borderColor).toContain(gray);
+  expect(window.getComputedStyle(uncheckedEnabled()).opacity).toContain(1);
+
+  expect(window.getComputedStyle(checkedEnabled()).backgroundColor).toContain(blue);
+  expect(window.getComputedStyle(checkedEnabled()).borderColor).toContain(blue);
+  expect(window.getComputedStyle(checkedEnabled()).opacity).toContain(1);
+
+  expect(window.getComputedStyle(uncheckedDisabled()).backgroundColor).toContain(grayTransparent);
+  expect(window.getComputedStyle(uncheckedDisabled()).borderColor).toContain(gray);
+  expect(window.getComputedStyle(uncheckedDisabled()).opacity).toContain(0.5);
+
+  expect(window.getComputedStyle(checkedDisabled()).backgroundColor).toContain(blue);
+  expect(window.getComputedStyle(checkedDisabled()).borderColor).toContain(blue);
+  expect(window.getComputedStyle(checkedDisabled()).opacity).toContain(0.5);
+}
