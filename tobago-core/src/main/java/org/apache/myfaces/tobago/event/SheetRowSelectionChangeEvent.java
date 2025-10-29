4f19f7192219ea0566060a9ecfcefc6e176f9678
Henning Noeth <hnoeth@apache.org>
2025-10-29 09:53:37 +0100

    feature(columnSelector): f:ajax
    Reimplement feature using "immediate" on tc:columnSelector.
    
    <tc:columnSelector><f:ajax> is possible. Technically it creates an rowSelectionChange event on the tc:sheet.
    
    Issue: TOBAGO-2442

---
+++ tobago-core/src/main/java/org/apache/myfaces/tobago/event/SheetRowSelectionChangeEvent.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.myfaces.tobago.event;
+
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.event.ActionEvent;
+
+import java.io.Serial;
+import java.util.List;
+
+public class SheetRowSelectionChangeEvent extends ActionEvent {
+
+  @Serial
+  private static final long serialVersionUID = 1482246499642513120L;
+  private final List<Integer> oldSelectedRows;
+  private final List<Integer> newSelectedRows;
+
+  public SheetRowSelectionChangeEvent(
+      final UIComponent uiComponent, final List<Integer> oldSelectedRows, final List<Integer> newSelectedRows) {
+    super(uiComponent);
+    this.oldSelectedRows = oldSelectedRows;
+    this.newSelectedRows = newSelectedRows;
+  }
+
+  public List<Integer> getOldSelectedRows() {
+    return oldSelectedRows;
+  }
+
+  public List<Integer> getNewSelectedRows() {
+    return newSelectedRows;
+  }
+}
