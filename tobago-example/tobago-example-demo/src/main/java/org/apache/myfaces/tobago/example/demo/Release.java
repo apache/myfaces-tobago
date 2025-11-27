bae770657acb1ff08dbce4b4872fb5413abace4f
Henning Noeth <hnoeth@apache.org>
2025-11-27 12:09:27 +0100

    Revert "chore(release): prepare"
    This reverts commit 1d22110bf82e9c64612c7fdeb9c3df456ddc1031.

--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/Release.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/Release.java
@@ -186,8 +186,8 @@ public enum Release {
   v6_7_1("12355672"),
   v6_7_2("12355753"),
   v6_8_0("12355944"),
-  v6_8_1("12356010"),
-  v6_8_2("12356249", true);
+  v6_8_1("12356010", true),
+  v6_8_2("12356249", false, true);
 
   private final String jira;
   private final String version;
