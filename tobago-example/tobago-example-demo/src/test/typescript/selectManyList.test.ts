5e05a24b8b4a603eb91b7e56f8ebb9e0883d988d
Henning Noeth <hnoeth@apache.org>
2025-12-05 13:26:47 +0100

    test(playwright): multiple servers
    Add multiple servers, started by docker compose, to the config.
    Add a goto function which tries to run all tests on several servers. If a server is not available, the test is skipped and does not fail.

--- tobago-example/tobago-example-demo/src/test/typescript/selectManyList.test.ts
+++ tobago-example/tobago-example-demo/src/test/typescript/selectManyList.test.ts
@@ -18,11 +18,12 @@
  */
 
 import {expect, test} from "@playwright/test";
+import {goto} from "./base/Functions";
 
 test.describe("900-test/2100-selectManyList/deselect/deselect.xhtml", () => {
 
-  test.beforeEach(async ({page}) => {
-    await page.goto("http://localhost:8080/content/900-test/selectManyList/deselect/deselect.xhtml");
+  test.beforeEach(async ({page}, testInfo) => {
+    await goto(test, page, testInfo, "http://localhost:8080/content/900-test/selectManyList/deselect/deselect.xhtml");
   });
 
   test("tc:selectManyList: deselect", async ({page}) => {
