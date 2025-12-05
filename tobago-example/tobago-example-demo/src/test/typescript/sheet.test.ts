3252811b3f4674a2c67191e27bdc36d5be98512f
Henning Noeth <hnoeth@apache.org>
2025-12-05 14:08:25 +0100

    test(playwright): multiple servers
    Add multiple servers, started by docker compose, to the config.
    Add a goto function which tries to run all tests on several servers. If a server is not available, the test is skipped and does not fail.

--- tobago-example/tobago-example-demo/src/test/typescript/sheet.test.ts
+++ tobago-example/tobago-example-demo/src/test/typescript/sheet.test.ts
@@ -24,7 +24,7 @@ import {goto} from "./base/Functions";
 test.describe("sheet/columnNode/ColumnNode.xhtml", () => {
 
   test.beforeEach(async ({page}, testInfo) => {
-    await goto(test, page, testInfo, "http://localhost:8080/content/900-test/sheet/columnNode/ColumnNode.xhtml");
+    await goto(test, page, testInfo, "/content/900-test/sheet/columnNode/ColumnNode.xhtml");
   });
 
   test("Open the 'World' node to see 'Carib' and 'Africa'", async ({page}) => {
