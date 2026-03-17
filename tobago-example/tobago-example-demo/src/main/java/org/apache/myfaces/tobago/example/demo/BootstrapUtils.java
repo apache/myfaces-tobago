09131289ea5c2702446838cb1e3698ad6f9ce3e9
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2026-03-17 08:46:07 +0100

    spelling

--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/BootstrapUtils.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/BootstrapUtils.java
@@ -24,7 +24,7 @@ import org.apache.myfaces.tobago.renderkit.css.BootstrapClass;
 public class BootstrapUtils {
 
   /**
-   * This method ensures, the used Bootstrap class is really existing.
+   * This method ensures the used Bootstrap class is really existing.
    */
   public static String valueOf(final String name) {
     return BootstrapClass.valueOf(name).getName();
