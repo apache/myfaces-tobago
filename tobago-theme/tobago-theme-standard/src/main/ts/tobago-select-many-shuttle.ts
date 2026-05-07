17e38d1fb9031c4face781665a040f7b7d42c603
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2026-05-07 17:26:24 +0200

    feat: shuttle ordering
    SelectManyShuttle: selected list should be sortable by the user
    
    "remove" and "removeAll" should put the unselected option back in the list at the original place/order.
    
    issue: TOBAGO-2486

--- tobago-theme/tobago-theme-standard/src/main/ts/tobago-select-many-shuttle.ts
+++ tobago-theme/tobago-theme-standard/src/main/ts/tobago-select-many-shuttle.ts
@@ -32,17 +32,26 @@ class SelectManyShuttle extends HTMLElement {
 
   connectedCallback(): void {
 
+    const tobagoOrder = JSON.parse(this.selectedSelect.dataset.tobagoOrder) as number[];
+
     const options = this.hiddenSelect.options;
+
+    for (let i = 0; i < options.length; i++) {
+      options[i].dataset.tobagoIndex = "" + i;
+    }
+
+    for (const i of tobagoOrder.reverse()) {
+      const option= this.hiddenSelect.querySelector(`[data-tobago-index="${i}"]`);
+      this.hiddenSelect.insertBefore(option, this.hiddenSelect.firstChild);
+    }
+
     for (let i = 0; i < options.length; i++) {
       const option: HTMLOptionElement = options[i];
       const clone = option.cloneNode(true) as HTMLOptionElement;
-      clone.dataset.tobagoIndex = "" + i;
       if (clone.selected) {
-        // console.info("adding selected: " + i);
-        this.selectedSelect.add(clone);
         clone.selected = false; // unselect, because it's in the selected list
+        this.selectedSelect.add(clone);
       } else {
-        // console.info("adding unselected: " + i);
         this.unselectedSelect.add(clone);
       }
     }
@@ -80,6 +89,22 @@ class SelectManyShuttle extends HTMLElement {
     if (!this.removeAllButton.disabled) {
       this.removeAllButton.addEventListener("click", this.removeAllItems.bind(this));
     }
+
+    if (this.topButton && !this.topButton.disabled) {
+      this.topButton.addEventListener("click", this.top.bind(this));
+    }
+
+    if (this.upButton && !this.upButton.disabled) {
+      this.upButton.addEventListener("click", this.up.bind(this));
+    }
+
+    if (this.downButton && !this.downButton.disabled) {
+      this.downButton.addEventListener("click", this.down.bind(this));
+    }
+
+    if (this.bottomButton && !this.bottomButton.disabled) {
+      this.bottomButton.addEventListener("click", this.bottom.bind(this));
+    }
   }
 
   private addAllItems(event: MouseEvent): void {
@@ -98,39 +123,132 @@ class SelectManyShuttle extends HTMLElement {
     this.removeItems(this.selectedSelect.querySelectorAll("option:not(:disabled)"));
   }
 
+  private top(event: MouseEvent): void {
+
+    const select = this.selectedSelect as HTMLSelectElement;
+    const options = select.options;
+    const hidden = this.hiddenSelect as HTMLSelectElement;
+
+    const firstUnselected: HTMLOptionElement = select.querySelector("option:not(:checked)");
+    if (firstUnselected == null) {
+      return; // noting to do
+    }
+
+    Array.from(select.querySelectorAll("option:checked")).forEach((current: HTMLOptionElement) => {
+      this.syncedInsertBefore(select, hidden, current, firstUnselected);
+    });
+  }
+
+  private up(event: MouseEvent): void {
+
+    const select = this.selectedSelect as HTMLSelectElement;
+    const options = select.options;
+    const hidden = this.hiddenSelect as HTMLSelectElement;
+
+    for (let i = 1; i < options.length; i++) {
+      const previous = options.item(i - 1)!;
+      const current = options.item(i);
+
+      if (current.selected && !previous.selected) {
+        this.syncedInsertBefore(select, hidden, current, previous);
+      }
+    }
+  }
+
+  private down(event: MouseEvent): void {
+
+    const select = this.selectedSelect as HTMLSelectElement;
+    const options = select.options;
+    const hidden = this.hiddenSelect as HTMLSelectElement;
+
+    for (let i = options.length - 2; i >= 0 ; i--) {
+      const current = options.item(i);
+      const next = options.item(i + 1)!;
+
+      if (current.selected && !next.selected) {
+        this.syncedInsertBefore(select, hidden, next, current);
+      }
+    }
+  }
+
+  private bottom(event: MouseEvent): void {
+
+    const select = this.selectedSelect as HTMLSelectElement;
+    const options = select.options;
+    const hidden = this.hiddenSelect as HTMLSelectElement;
+
+    const allUnselected: NodeListOf<HTMLOptionElement> = select.querySelectorAll("option:not(:checked)");
+    const lastUnselected: HTMLOptionElement = allUnselected[allUnselected.length - 1];
+    if (lastUnselected == null) {
+      return; // noting to do
+    }
+
+    Array.from(select.querySelectorAll("option:checked")).reverse().forEach((current: HTMLOptionElement) => {
+      this.syncedInsertAfter(select, hidden, current, lastUnselected);
+    });
+  }
+
+  private syncedInsertBefore(
+      parent: HTMLSelectElement, hidden: HTMLSelectElement, a: HTMLOptionElement, b: HTMLOptionElement) {
+    parent.insertBefore(a, b);
+    const hiddenA = hidden.querySelector(`option[value='${a.value}']`);
+    const hiddenB = hidden.querySelector(`option[value='${b.value}']`);
+    hidden.insertBefore(hiddenA, hiddenB);
+  }
+
+  private syncedInsertAfter(
+      parent: HTMLSelectElement, hidden: HTMLSelectElement, a: HTMLOptionElement, b: HTMLOptionElement) {
+    b.after(a);
+    const hiddenA = hidden.querySelector(`option[value='${a.value}']`);
+    const hiddenB = hidden.querySelector(`option[value='${b.value}']`);
+    hiddenB.after(hiddenA);
+  }
+
   private addItems(options: NodeListOf<HTMLOptionElement>): void {
+    const hiddenSelect = this.hiddenSelect;
     for (const option of options) {
       this.selectedSelect.add(option);
-      this.changeHiddenOption(option, true);
+      this.changeHiddenOption(hiddenSelect, option, true);
     }
     this.fireChangeEvent(options, null);
   }
 
   private removeItems(options: NodeListOf<HTMLOptionElement>): void {
+    const hiddenSelect = this.hiddenSelect;
     for (const option of options) {
       const index = Number(option.dataset.tobagoIndex);
       let nextOption: HTMLOptionElement = null;
       for (const option of this.unselectedSelect.options) {
-        // console.info("checking: " + option.dataset.tobagoIndex + " > " + index);
         if (Number(option.dataset.tobagoIndex) > index) {
           nextOption = option;
           break;
         }
       }
-      console.info("removing: " + index + " -> " + nextOption?.dataset.tobagoIndex + " (" + nextOption?.value + ")");
       if (nextOption != null) {
         this.unselectedSelect.add(option, nextOption);
       } else {
         this.unselectedSelect.appendChild(option);
       }
-      this.changeHiddenOption(option, false);
+      this.changeHiddenOption(hiddenSelect, option, false);
     }
     this.fireChangeEvent(null, options);
   }
 
-  private changeHiddenOption(option: HTMLOptionElement, select: boolean): void {
-    const hiddenOption: HTMLOptionElement = this.hiddenSelect.querySelector(`option[value='${option.value}']`);
-    hiddenOption.selected = select;
+  private changeHiddenOption(hiddenSelect: HTMLSelectElement, option: HTMLOptionElement, selected: boolean) {
+    const hiddenOption: HTMLOptionElement = hiddenSelect.querySelector(`option[value='${option.value}']`);
+    const firstUnselectedHiddenOption: HTMLOptionElement = hiddenSelect.querySelector("option:not(:checked)");
+    if (selected) {
+      hiddenSelect.insertBefore(hiddenOption, firstUnselectedHiddenOption);
+    } else {
+      const index = Number(hiddenOption.dataset.tobagoIndex);
+      const after = hiddenSelect.querySelector(`option[data-tobago-index="${index + 1}"]`);
+      if (after) {
+        hiddenSelect.insertBefore(hiddenOption, after);
+      } else {
+        hiddenSelect.appendChild(hiddenOption);
+      }
+    }
+    hiddenOption.selected = selected;
   }
 
   private fireChangeEvent(added: NodeListOf<HTMLOptionElement>, removed: NodeListOf<HTMLOptionElement>): void {
@@ -157,7 +275,7 @@ class SelectManyShuttle extends HTMLElement {
   }
 
   get hiddenSelect(): HTMLSelectElement {
-    return this.querySelector("select.d-none");
+    return this.querySelector(".tobago-value");
   }
 
   get addAllButton(): HTMLButtonElement {
@@ -175,6 +293,23 @@ class SelectManyShuttle extends HTMLElement {
   get removeAllButton(): HTMLButtonElement {
     return this.querySelector(".btn-group-vertical button:nth-child(4)");
   }
+
+  get topButton(): HTMLButtonElement {
+    return this.querySelector("[data-tobago-action=top]");
+  }
+
+  get upButton(): HTMLButtonElement {
+    return this.querySelector("[data-tobago-action=up]");
+  }
+
+  get downButton(): HTMLButtonElement {
+    return this.querySelector("[data-tobago-action=down]");
+  }
+
+  get bottomButton(): HTMLButtonElement {
+    return this.querySelector("[data-tobago-action=bottom]");
+  }
+
 }
 
 document.addEventListener("tobago.init", function (event: Event): void {
