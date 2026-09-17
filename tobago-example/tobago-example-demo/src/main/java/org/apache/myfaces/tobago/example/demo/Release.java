9fb696825bde0f489ad97b3addc27d5fe3a5cb5e
Henning Noeth <hnoeth@apache.org>
2026-09-17 09:01:37 +0200

    chore(release): prepare

--- tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/Release.java
+++ tobago-example/tobago-example-demo/src/main/java/org/apache/myfaces/tobago/example/demo/Release.java
@@ -191,8 +191,8 @@ public enum Release {
   v6_9_1("12356478"),
   v6_10_0("12356699"),
   v6_11_0("12356805"),
-  v6_12_0("12357051",  true),
-  v6_12_1("12357518",  false, true);
+  v6_12_0("12357051"),
+  v6_12_1("12357518", true);
 
   private final String jira;
   private final String version;
